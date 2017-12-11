fa049a7a2383c6194a5e9bbb45ebecae1e13a676
Jennifer Smith <smithj15@email.unc.edu>
2017-12-11 15:59:50 -0500

    Change entity_id column in entity_specific_responsibilities table to integer type

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,4 +21,23 @@ class Ability
     #   can [:create], ActiveFedora::Base
     # end
   end
+
+  private
+
+    # TODO: remove once sipity entitiy_specific_responsibilities are incorporated into hyrax gem
+    def can_review_submissions?
+      # Short-circuit logic for admins, who should have the ability
+      # to review submissions whether or not they are explicitly
+      # granted the approving role in any workflows
+      return true if admin?
+
+      # Are there any workflows or entities where this user has the "approving" responsibility
+      approving_role = Sipity::Role.find_by(name: Hyrax::RoleRegistry::APPROVING)
+      return false unless approving_role
+      Hyrax::Workflow::PermissionQuery.scope_processing_agents_for(user: current_user).any? do |agent|
+        (agent.workflow_responsibilities.joins(:workflow_role)
+            .where('sipity_workflow_roles.role_id' => approving_role.id).any? ||
+            agent.entity_specific_responsibilities.joins(:workflow_role).where('sipity_workflow_roles.role_id' => approving_role.id).any?)
+      end
+    end
 end
