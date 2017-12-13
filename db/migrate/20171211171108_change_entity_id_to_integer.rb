e02818fb64072b0718e29254b0f4ee40d65b9b26
Jennifer Smith <smithj15@email.unc.edu>
2017-12-13 11:01:45 -0500

    Remove unnecessary migration

--- db/migrate/20171211171108_change_entity_id_to_integer.rb
+++
@@ -1,8 +0,0 @@
-class ChangeEntityIdToInteger < ActiveRecord::Migration[5.0]
-  def self.up
-    change_column :sipity_entity_specific_responsibilities, :entity_id, 'integer USING CAST(entity_id AS integer)'
-  end
-  def self.down
-    change_column :sipity_entity_specific_responsibilities, :entity_id, :string
-  end
-end
