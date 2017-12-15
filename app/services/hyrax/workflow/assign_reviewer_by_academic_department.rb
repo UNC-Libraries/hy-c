57b8d159732c4da61d661278bbe5276af061636f
Jennifer Smith <smithj15@email.unc.edu>
2017-12-15 16:14:24 -0500

    Fix workflow selection in assignment service

--- app/services/hyrax/workflow/assign_reviewer_by_academic_department.rb
+++ app/services/hyrax/workflow/assign_reviewer_by_academic_department.rb
@@ -2,14 +2,16 @@ module Hyrax::Workflow::AssignReviewerByAcademicDepartment
 
   def self.call(target:, **)
     reviewer = find_reviewer_for(department: target.academic_department)
+    permission_template_id = Hyrax::PermissionTemplate.find_by_admin_set_id(target.admin_set_id).id
+
     # This assigns database permissions, but does not grant permissions on the Fedora object.
     Hyrax::Workflow::PermissionGenerator.call(entity: target, agents: [reviewer],
                                               roles: ['approving'],
-                                              workflow: Sipity::Workflow.find_by_name('honors_thesis_one_step_mediated_deposit'))
+                                              workflow: Sipity::Workflow.where(permission_template_id: permission_template_id, active: true).first)
   end
 
   def self.find_reviewer_for(department:)
     Sipity::Agent.where(proxy_for_id: department.to_s.downcase+'_reviewer',
-                                proxy_for_type: 'Hyrax::Group').first_or_create
+                        proxy_for_type: 'Hyrax::Group').first_or_create
   end
 end
