22dc7ff08982aacfc4447d497e6cd6a392cbc11d
Jennifer Smith <smithj15@email.unc.edu>
2017-12-21 12:26:25 -0500

    Add tests for honors thesis work type

--- spec/forms/hyrax/honors_thesis_form_spec.rb
+++ spec/forms/hyrax/honors_thesis_form_spec.rb
@@ -3,7 +3,98 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::HonorsThesisForm do
-  it "has tests" do
-    skip "Add your tests here"
+  let(:work) { HonorsThesis.new }
+  let(:form) { described_class.new(work, nil, nil) }
+
+  describe "#required_fields" do
+    subject { form.required_fields }
+
+    it { is_expected.to match_array [:title, :creator, :rights_statement, :abstract, :academic_department,
+                                     :advisor, :degree_granting_institution, :resource_type] }
+  end
+
+  describe "#primary_terms" do
+    subject { form.primary_terms }
+
+    it { is_expected.to match_array [:title, :creator, :rights_statement, :abstract, :academic_department,
+                                     :advisor, :degree_granting_institution, :resource_type] }
+  end
+
+  describe "#secondary_terms" do
+    subject { form.secondary_terms }
+
+    it { is_expected.to match_array [:license, :subject, :language, :degree, :genre, :graduation_year,
+                                     :honors_level, :note, :academic_concentration, :keyword, :related_url] }
+  end
+
+  describe '.model_attributes' do
+    let(:params) do
+      ActionController::Parameters.new(
+          title: 'foo', # single-valued
+          abstract: [''],
+          creator: ['someone@example.com'],
+          visibility: 'open',
+          representative_id: '456',
+          thumbnail_id: '789',
+          keyword: ['test'],
+          license: ['http://creativecommons.org/licenses/by/3.0/us/'],
+          member_of_collection_ids: ['123456', 'abcdef']
+      )
+    end
+
+    subject { described_class.model_attributes(params) }
+
+    it 'permits parameters' do
+      expect(subject['title']).to eq ['foo']
+      expect(subject['abstract']).to be_empty
+      expect(subject['creator']).to eq ['someone@example.com']
+      expect(subject['visibility']).to eq 'open'
+      expect(subject['representative_id']).to eq '456'
+      expect(subject['thumbnail_id']).to eq '789'
+      expect(subject['license']).to eq ['http://creativecommons.org/licenses/by/3.0/us/']
+      expect(subject['keyword']).to eq ['test']
+      expect(subject['member_of_collection_ids']).to eq ['123456', 'abcdef']
+    end
+
+    context '.model_attributes' do
+      let(:params) do
+        ActionController::Parameters.new(
+            title: '',
+            abstract: [''],
+            keyword: [''],
+            license: [''],
+            member_of_collection_ids: [''],
+            on_behalf_of: 'Melissa'
+        )
+      end
+
+      it 'removes blank parameters' do
+        expect(subject['title']).to be_empty
+        expect(subject['abstract']).to be_empty
+        expect(subject['license']).to be_empty
+        expect(subject['keyword']).to be_empty
+        expect(subject['member_of_collection_ids']).to be_empty
+        expect(subject['on_behalf_of']).to eq 'Melissa'
+      end
+    end
+  end
+
+  describe "#visibility" do
+    subject { form.visibility }
+
+    it { is_expected.to eq 'restricted' }
+  end
+
+  describe "#agreement_accepted" do
+    subject { form.agreement_accepted }
+
+    it { is_expected.to eq false }
+  end
+
+  context "on a work already saved" do
+    before { allow(work).to receive(:new_record?).and_return(false) }
+    it "defaults deposit agreement to true" do
+      expect(form.agreement_accepted).to eq(true)
+    end
   end
 end
