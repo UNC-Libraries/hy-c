0f2a5994671361e5777ec0b5a22320407ea578d0
Jennifer Smith <smithj15@email.unc.edu>
2018-01-05 11:59:34 -0500

    Update oai-pmh tests to work with official gem

--- app/models/language_set.rb
+++ app/models/language_set.rb
@@ -1,7 +1,7 @@
 class LanguageSet < ::BlacklightOaiProvider::SolrSet
   def description
-    if @spec
-      'This set includes files in the '+@spec.split(':').last.capitalize+' language.'
+    if label && value
+      "This set includes files in the #{value.capitalize} language."
     else
       'No description available.'
     end
