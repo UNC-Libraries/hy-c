035c49808aa6a1bd48564c9f81b6f2745077f81d
Jennifer Smith <smithj15@email.unc.edu>
2018-01-23 11:29:30 -0500

    Update feature tests and fix affected OAI response tests

--- spec/features/create_honors_thesis_spec.rb
+++ spec/features/create_honors_thesis_spec.rb
@@ -6,28 +6,54 @@ include Warden::Test::Helpers
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a HonorsThesis', js: false do
   context 'a logged in user' do
-    let(:user_attributes) do
-      { email: 'test@example.com' }
-    end
     let(:user) do
-      User.new(user_attributes) { |u| u.save(validate: false) }
+      User.new(email: 'test@example.com',guest: false) { |u| u.save!(validate: false)}
+    end
+
+    let(:admin_set) do
+      AdminSet.create(title: ["default admin set"],
+                      description: ["some description"],
+                      edit_users: [user.user_key])
+    end
+
+    let(:permission_template) do
+      Hyrax::PermissionTemplate.create!(admin_set_id: admin_set.id)
+    end
+
+    let(:workflow) do
+      Sipity::Workflow.create(name: 'test', allows_access_grant: true, active: true, permission_template_id: permission_template.id)
     end
 
     before do
-      AdminSet.find_or_create_default_admin_set_id
+      Hyrax::PermissionTemplateAccess.create(permission_template: permission_template, agent_type: 'user', agent_id: user.user_key, access: 'deposit')
+      Sipity::WorkflowAction.create(id: 4, name: 'show', workflow_id: workflow.id)
       login_as user
     end
 
     scenario do
-      visit '/dashboard'
-      click_link "Works"
-      click_link "Add new work"
+      visit new_hyrax_honors_thesis_path
+      expect(page).to have_content "Add New Honors Thesis"
 
-      # If you generate more than one work uncomment these lines
-      # choose "payload_concern", option: "HonorsThesis"
-      # click_button "Create work"
+      fill_in 'Title', with: 'Test HonorsThesis work'
+      fill_in 'Creator', with: 'Test Default Creator'
+      fill_in 'Keyword', with: 'Test Default Keyword'
+      select "In Copyright", :from => "honors_thesis_rights_statement"
+      choose "honors_thesis_visibility_open"
+      check 'agreement'
 
-      expect(page).to have_content "Add New Honors thesis"
+      click_link "Files" # switch tab
+      within "//span[@id=addfiles]" do
+        attach_file('files[]', File.join(Rails.root, '/spec/fixtures/files/test.txt'))
+      end
+
+      click_button 'Save'
+      expect(page).to have_content 'Your files are being processed by Hyrax'
+
+      visit '/dashboard/my/works/'
+      expect(page).to have_content 'Test HonorsThesis work'
+
+      first('.document-title', text: 'Test HonorsThesis work').click
+      expect(page).to have_content 'Test Default Keyword'
     end
   end
 end
