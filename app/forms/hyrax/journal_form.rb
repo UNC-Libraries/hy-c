1c12fb94d3d4bcd16cda9d1b4b185f57d49a3cfa
lfarrell <lfarrell@email.unc.edu>
2018-01-30 10:23:11 -0500

    Remove description field

--- app/forms/hyrax/journal_form.rb
+++ app/forms/hyrax/journal_form.rb
@@ -2,12 +2,14 @@
 #  `rails generate hyrax:work Journal`
 module Hyrax
   class JournalForm < Hyrax::Forms::WorkForm
+    class_attribute :single_value_fields
+    
     self.model_class = ::Journal
 
     self.terms += [:resource_type, :abstract, :alternate_title, :extent, :genre,
                    :geographic_subject, :issn, :note, :place_of_publication, :table_of_contents
     ]
-    self.terms -= [:based_near, :related_url]
+    self.terms -= [:description, :based_near, :related_url]
     self.single_value_fields = [:title]
   end
 end
