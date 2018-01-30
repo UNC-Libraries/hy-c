9dc43ece426d3587f6cf6825088e8244aa65382a
lfarrell <lfarrell@email.unc.edu>
2018-01-30 14:42:32 -0500

    Add tests

--- spec/features/create_journal_spec.rb
+++ spec/features/create_journal_spec.rb
@@ -7,27 +7,57 @@ include Warden::Test::Helpers
 RSpec.feature 'Create a Journal', js: false do
   context 'a logged in user' do
     let(:user_attributes) do
-      { email: 'test@example.com' }
+      { email: 'test@example.com', guest: false }
     end
+    
     let(:user) do
       User.new(user_attributes) { |u| u.save(validate: false) }
     end
 
+    let(:admin_set) do
+      AdminSet.create(title: ["default admin set"],
+                      description: ["some description"],
+                      edit_users: [user.user_key])
+    end
+    
+    let(:permission_template) do
+      Hyrax::PermissionTemplate.create!(admin_set_id: admin_set.id)
+    end
+
+    let(:workflow) do
+      Sipity::Workflow.create(name: 'test', allows_access_grant: true, active: true, permission_template_id: permission_template.id)
+    end
+
     before do
-      AdminSet.find_or_create_default_admin_set_id
+      Hyrax::PermissionTemplateAccess.create(permission_template: permission_template, agent_type: 'user', agent_id: user.user_key, access: 'deposit')
+      Sipity::WorkflowAction.create(id: 4, name: 'show', workflow_id: workflow.id)
       login_as user
     end
 
     scenario do
-      visit '/dashboard'
-      click_link "Works"
-      click_link "Add new work"
-
-      # If you generate more than one work uncomment these lines
-      # choose "payload_concern", option: "Journal"
-      # click_button "Create work"
-
+      visit new_hyrax_journal_path
       expect(page).to have_content "Add New Journal"
+
+      fill_in 'Title', with: 'Test Journal'
+      fill_in 'Creator', with: 'Test Default Creator'
+      fill_in 'Keyword', with: 'Test Default Keyword'
+      select "In Copyright", :from => "article_rights_statement"
+      choose "article_visibility_open"
+      check 'agreement'
+
+      click_link "Files" # switch tab
+      within "//span[@id=addfiles]" do
+        attach_file('files[]', File.join(Rails.root, '/spec/fixtures/files/test.txt'))
+      end
+
+      click_button 'Save'
+      expect(page).to have_content 'Your files are being processed by Hyrax'
+
+      visit '/dashboard/my/works/'
+      expect(page).to have_content 'Test Journal'
+
+      first('.document-title', text: 'Test Journal').click
+      expect(page).to have_content 'Test Default Keyword'
     end
   end
 end
