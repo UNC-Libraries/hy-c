1cddc85d358384c376df7cd5db4b5cf2e1a76eb4
Jennifer Smith <smithj15@email.unc.edu>
2018-03-01 12:06:33 -0500

    Add scholarly work properties to model

--- app/models/scholarly_work.rb
+++ app/models/scholarly_work.rb
@@ -10,6 +10,42 @@ class ScholarlyWork < ActiveFedora::Base
 
   self.human_readable_type = 'Scholarly Work'
 
+  property :abstract, predicate: ::RDF::Vocab::DC.abstract do |index|
+    index.as :stored_searchable
+  end
+
+  property :advisor, predicate: ::RDF::URI('http://id.loc.gov/vocabulary/relators/ths') do |index|
+    index.as :stored_searchable
+  end
+
+  property :affiliation, predicate: ::RDF::URI('http://vivoweb.org/ontology/core#AcademicDepartment') do |index|
+    index.as :stored_searchable
+  end
+
+  property :conference_name, predicate: ::RDF::Vocab::BIBO.presentedAt do |index|
+    index.as :stored_searchable
+  end
+
+  property :date_issued, predicate: ::RDF::Vocab::DC.issued, multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :genre, predicate: ::RDF::Vocab::EDM.hasType do |index|
+    index.as :stored_searchable
+  end
+
+  property :geographic_subject, predicate: ::RDF::Vocab::DC.spatial do |index|
+    index.as :stored_searchable
+  end
+
+  property :orcid, predicate: ::RDF::Vocab::Identifiers.orcid do |index|
+    index.as :stored_searchable
+  end
+
+  property :other_affiliation, predicate: ::RDF::Vocab::SCHEMA.affiliation do |index|
+    index.as :stored_searchable
+  end
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
