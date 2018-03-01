47d0e78ebbcfee9d1ef11a6f6b0c9d30b4582e3a
Jennifer Smith <smithj15@email.unc.edu>
2018-03-01 13:56:22 -0500

    Add properties to scholarly work form and presenter

--- app/forms/hyrax/scholarly_work_form.rb
+++ app/forms/hyrax/scholarly_work_form.rb
@@ -1,8 +1,21 @@
 # Generated via
 #  `rails generate hyrax:work ScholarlyWork`
 module Hyrax
-  class ScholarlyWorkForm < Hyrax::Forms::WorkForm
+  class ScholarlyWorkForm < ::SingleValueForm
+    class_attribute :single_value_fields
+
     self.model_class = ::ScholarlyWork
-    self.terms += [:resource_type]
+
+    self.terms += [:resource_type, :abstract, :advisor, :affiliation, :conference_name, :date_issued, :genre,
+                   :geographic_subject, :orcid, :other_affiliation]
+    self.terms -= [:contributor, :publisher, :identifier, :based_near, :related_url]
+    self.required_fields -= [:keyword, :rights_statement]
+    self.single_value_fields = [:title]
+
+    # Add overrides for required properties which are becoming single-valued
+
+    def title
+      super.first || ""
+    end
   end
 end
