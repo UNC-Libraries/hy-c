dbbf3a4bcc704515f275cc62d9e760dbfb998830
Jennifer Smith <smithj15@email.unc.edu>
2018-03-23 14:18:59 -0400

    Change the date_attribute_renderer to display humanized versions of EDTF dates

---
+++ app/renderers/hyrax/renderers/date_attribute_renderer.rb
@@ -0,0 +1,13 @@
+# Overriding renderer from gem to display readable versions of EDTF dates
+module Hyrax
+  module Renderers
+    class DateAttributeRenderer < AttributeRenderer
+      private
+
+      def attribute_value_to_html(value)
+        Date.try(:edtf, value).try(:humanize) ||
+            Date.parse(value).to_formatted_s(:standard).try(:edtf).try(:humanize)
+      end
+    end
+  end
+end
