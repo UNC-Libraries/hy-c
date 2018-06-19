b7969ad91d412ab832dbdc6204b284254fd7beb1
smithjp <smithj15@email.unc.edu>
2018-06-19 08:11:36 -0400

    Configure sendmail (#37)
    * Configure application to send email notifications as well as in-app notifications
    
    * Update travis env variables
    
    * Add explaination of overridden file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "hyrax_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- spec/services/hyrax/workflow/deletion_approval_notification_spec.rb
+++ spec/services/hyrax/workflow/deletion_approval_notification_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Hyrax::Workflow::DeletionApprovalNotification do
     it 'sends a message to all users' do
       expect(approver).to receive(:send_message)
                               .with(anything,
-                                    "The deletion request for #{work.title[0]} (<a href=\"/concern/articles/#{work.id}\">#{work.id}</a>) "\
+                                    "The deletion request for #{work.title[0]} (<a href=\"#{ENV['HYRAX_HOST']}/concern/articles/#{work.id}\">#{work.id}</a>) "\
       "was approved by #{approver.user_key}. #{comment.comment}",
                                     anything).exactly(3).times.and_call_original
 
