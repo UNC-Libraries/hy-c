53020f0e58417500ee198fab71c02468da7fae23
lfarrell <lfarrell@email.unc.edu>
2018-06-27 09:24:18 -0400

    Add DOI to citations

---
+++ app/helpers/hyrax/citation_behavoirs/formatters/apa_formatter.rb
@@ -0,0 +1,85 @@
+module Hyrax
+  module CitationsBehaviors
+    module Formatters
+      class ApaFormatter < BaseFormatter
+        include Hyrax::CitationsBehaviors::PublicationBehavior
+        include Hyrax::CitationsBehaviors::TitleBehavior
+
+        def format(work)
+          text = ''
+          text << authors_text_for(work)
+          text << pub_date_text_for(work)
+          text << add_title_text_for(work)
+          text << add_publisher_text_for(work)
+          # UNC customization. Add DOI
+          text << work.doi[0] if !work.doi.nil? && work.doi.length > 0
+          text.html_safe
+        end
+
+        private
+
+        def authors_text_for(work)
+          # setup formatted author list
+          authors_list = author_list(work).reject(&:blank?)
+          author_text = format_authors(authors_list)
+          if author_text.blank?
+            author_text
+          else
+            "<span class=\"citation-author\">#{author_text}</span> "
+          end
+        end
+
+        public
+
+        def format_authors(authors_list = [])
+          authors_list = Array.wrap(authors_list).collect { |name| abbreviate_name(surname_first(name)).strip }
+          text = ''
+          text << authors_list.first if authors_list.first
+          authors_list[1..-1].each do |author|
+            if author == authors_list.last # last
+              text << ", &amp; " << author
+            else # all others
+              text << ", " << author
+            end
+          end
+          text << "." unless text =~ /\.$/
+          text
+        end
+
+        private
+
+        def pub_date_text_for(work)
+          # Get Pub Date
+          pub_date = setup_pub_date(work)
+          format_date(pub_date)
+        end
+
+        def add_title_text_for(work)
+          # setup title info
+          title_info = setup_title_info(work)
+          format_title(title_info)
+        end
+
+        def add_publisher_text_for(work)
+          # Publisher info
+          pub_info = clean_end_punctuation(setup_pub_info(work))
+          if pub_info.nil?
+            ''
+          else
+            pub_info + "."
+          end
+        end
+
+        public
+
+        def format_date(pub_date)
+          pub_date.blank? ? "" : "(" + pub_date + "). "
+        end
+
+        def format_title(title_info)
+          title_info.nil? ? "" : "<i class=\"citation-title\">#{title_info}</i> "
+        end
+      end
+    end
+  end
+end
---
+++ app/presenters/hyrax/work_show_presenter.rb
@@ -0,0 +1,168 @@
+module Hyrax
+  class WorkShowPresenter
+    include ModelProxy
+    include PresentsAttributes
+    attr_accessor :solr_document, :current_ability, :request
+
+    class_attribute :collection_presenter_class
+
+    # modify this attribute to use an alternate presenter class for the collections
+    self.collection_presenter_class = CollectionPresenter
+
+    # Methods used by blacklight helpers
+    delegate :has?, :first, :fetch, :export_formats, :export_as, to: :solr_document
+
+    # delegate fields from Hyrax::Works::Metadata to solr_document
+    delegate :based_near_label, :related_url, :depositor, :identifier, :resource_type,
+             :keyword, :itemtype, :admin_set, to: :solr_document
+
+    # @param [SolrDocument] solr_document
+    # @param [Ability] current_ability
+    # @param [ActionDispatch::Request] request the http request context. Used so
+    #                                  the GraphExporter knows what URLs to draw.
+    def initialize(solr_document, current_ability, request = nil)
+      @solr_document = solr_document
+      @current_ability = current_ability
+      @request = request
+    end
+
+    def page_title
+      title.first
+    end
+
+    # CurationConcern methods
+    delegate :stringify_keys, :human_readable_type, :collection?, :to_s,
+             to: :solr_document
+
+    # Metadata Methods
+    delegate :title, :date_created, :description, :doi,
+             :creator, :contributor, :subject, :publisher, :language, :embargo_release_date,
+             :lease_expiration_date, :license, :source, :rights_statement, :thumbnail_id, :representative_id,
+             :member_of_collection_ids, to: :solr_document
+
+    def workflow
+      @workflow ||= WorkflowPresenter.new(solr_document, current_ability)
+    end
+
+    def inspect_work
+      @inspect_workflow ||= InspectWorkPresenter.new(solr_document, current_ability)
+    end
+
+    # @return [String] a download URL, if work has representative media, or a blank string
+    def download_url
+      return '' if representative_presenter.nil?
+      Hyrax::Engine.routes.url_helpers.download_url(representative_presenter, host: request.host)
+    end
+
+    # @return FileSetPresenter presenter for the representative FileSets
+    def representative_presenter
+      return nil if representative_id.blank?
+      @representative_presenter ||=
+          begin
+            result = member_presenters([representative_id]).first
+            if result.respond_to?(:representative_presenter)
+              result.representative_presenter
+            else
+              result
+            end
+          end
+    end
+
+    # Get presenters for the collections this work is a member of via the member_of_collections association.
+    # @return [Array<CollectionPresenter>] presenters
+    def member_of_collection_presenters
+      PresenterFactory.build_for(ids: member_of_collection_ids,
+                                 presenter_class: collection_presenter_class,
+                                 presenter_args: presenter_factory_arguments)
+    end
+
+    def date_modified
+      solr_document.date_modified.try(:to_formatted_s, :standard)
+    end
+
+    def date_uploaded
+      solr_document.date_uploaded.try(:to_formatted_s, :standard)
+    end
+
+    def link_name
+      current_ability.can?(:read, id) ? to_s : 'File'
+    end
+
+    def export_as_nt
+      graph.dump(:ntriples)
+    end
+
+    def export_as_jsonld
+      graph.dump(:jsonld, standard_prefixes: true)
+    end
+
+    def export_as_ttl
+      graph.dump(:ttl)
+    end
+
+    def editor?
+      current_ability.can?(:edit, solr_document)
+    end
+
+    def tweeter
+      TwitterPresenter.twitter_handle_for(user_key: depositor)
+    end
+
+    def presenter_types
+      Hyrax.config.registered_curation_concern_types.map(&:underscore) + ["collection"]
+    end
+
+    # @return [Array] presenters grouped by model name, used to show the parents of this object
+    def grouped_presenters(filtered_by: nil, except: nil)
+      # TODO: we probably need to retain collection_presenters (as parent_presenters)
+      #       and join this with member_of_collection_presenters
+      grouped = member_of_collection_presenters.group_by(&:model_name).transform_keys { |key| key.to_s.underscore }
+      grouped.select! { |obj| obj.downcase == filtered_by } unless filtered_by.nil?
+      grouped.except!(*except) unless except.nil?
+      grouped
+    end
+
+    def work_featurable?
+      user_can_feature_works? && solr_document.public?
+    end
+
+    def display_feature_link?
+      work_featurable? && FeaturedWork.can_create_another? && !featured?
+    end
+
+    def display_unfeature_link?
+      work_featurable? && featured?
+    end
+
+    def stats_path
+      Hyrax::Engine.routes.url_helpers.stats_work_path(self)
+    end
+
+    delegate :member_presenters, :file_set_presenters, :work_presenters, to: :member_presenter_factory
+
+    private
+
+    def featured?
+      if @featured.nil?
+        @featured = FeaturedWork.where(work_id: solr_document.id).exists?
+      end
+      @featured
+    end
+
+    def user_can_feature_works?
+      current_ability.can?(:create, FeaturedWork)
+    end
+
+    def presenter_factory_arguments
+      [current_ability, request]
+    end
+
+    def member_presenter_factory
+      MemberPresenterFactory.new(solr_document, current_ability, request)
+    end
+
+    def graph
+      GraphExporter.new(solr_document, request).fetch
+    end
+  end
+end
