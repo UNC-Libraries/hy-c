a7856286dee34175f753b0c5f2984e2ca9a83c29
lfarrell <lfarrell@email.unc.edu>
2018-07-09 09:26:17 -0400

    Fix deposit links

--- app/assets/javascripts/unc_custom.js
+++ app/assets/javascripts/unc_custom.js
@@ -1,6 +1,22 @@
 $(function() {
+    // Add datepicker to date fields in forms
     $('input[id*="date"]').datepicker({
         maxDate: '+0D',
         dateFormat: 'yy-mm-dd'
+    });
+
+
+    // Only show student paper options in modal when clicking "Student Papers" link on homepage
+    var all_work_types = $('form.new-work-select .select-worktype');
+
+    $('#student-papers-work-types').on('click', function (d) {
+        all_work_types.filter(function(index) {
+            return index >= 4 || index === 1;
+        }).addClass('hidden');
+    });
+
+    // Make sure all forms ar visible when non student papers deposits links are clicked
+    $('.all-unc-work-types').on('click', function(d) {
+       all_work_types.removeClass('hidden');
     });
 });
