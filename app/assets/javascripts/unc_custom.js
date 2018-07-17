b9c85def5fec15f5009acf192e2a26a981772743
lfarrell <lfarrell@email.unc.edu>
2018-07-17 12:12:34 -0400

    Fix datepicker bugs

--- app/assets/javascripts/unc_custom.js
+++ app/assets/javascripts/unc_custom.js
@@ -1,23 +1,47 @@
+/**
+ * Grouped into functional units for easier grouping of code
+ * Each function is self executing
+ */
 $(function() {
     // Add datepicker to date fields in forms
-    $('input[id*="date"]').datepicker({
-        maxDate: '+0D',
-        dateFormat: 'yy-mm-dd'
-    });
+    (function datePicking() {
+        var datepicker_options = {
+            dateFormat: 'ISO_8601',
+            beforeShow:function(field) {
+                // Make sure datepicker is always top element
+                $(field).css({
+                    'position': 'relative',
+                    'z-index': 100
+                });
+            }
+        };
+
+        // Allow future dates for embargoes
+        if ($('#file_set_embargo_release_date').length > 0) {
+            datepicker_options['maxDate'] = null;
+        } else {
+            datepicker_options['maxDate'] = '+0D';
+        }
+
+        // Check for leading _ plus date, otherwise selects things like "update" too
+        $('input[id*="_date"]').datepicker(datepicker_options);
+    })();
 
 
     // Only show student paper options in modal when clicking "Student Papers" link on homepage
-    var all_work_types = $('form.new-work-select .select-worktype');
+    (function visibleForms() {
+        var all_work_types = $('form.new-work-select .select-worktype');
 
-    $('#student-papers-work-types').on('click', function (d) {
-        all_work_types.filter(function(index, element) {
-            var work_type = $(this).find('input[type=radio]').attr('value');
-            return !/MastersPaper|HonorsThesis/.test(work_type);
-        }).addClass('hidden');
-    });
+        $('#student-papers-work-types').on('click', function () {
+            all_work_types.filter(function(index, element) {
+                var work_type = $(this).find('input[type=radio]').attr('value');
+                return !/MastersPaper|HonorsThesis/.test(work_type);
+            }).addClass('hidden');
+        });
 
-    // Make sure all forms ar visible when non student papers deposits links are clicked
-    $('.all-unc-work-types').on('click', function(d) {
-       all_work_types.removeClass('hidden');
-    });
+        // Make sure all forms ar visible when non student papers deposits links are clicked
+        $('.all-unc-work-types').on('click', function() {
+            all_work_types.removeClass('hidden');
+        });
+    })();
 });
