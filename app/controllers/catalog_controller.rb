4aafd54cac785efa5fb73335c34c5e8313107623
lfarrell <lfarrell@email.unc.edu>
2018-07-25 10:22:21 -0400

    Show affiliations lable as "Departments"

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -55,7 +55,7 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collections'
 
     # UNC Custom
-    config.add_facet_field solr_name("affiliation", :facetable), label: "Affiliation", limit: 5
+    config.add_facet_field solr_name("affiliation", :facetable), label: "Departments", limit: 5
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
@@ -113,7 +113,7 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("academic_concentration", :stored_searchable), label: "Academic Concentration"
     config.add_index_field solr_name("access", :stored_searchable), label: "Access"
     config.add_index_field solr_name("advisor", :stored_searchable), label: "Advisor"
-    config.add_index_field solr_name("affiliation", :stored_searchable), label: "Affiliation", link_to_search: solr_name("affiliation", :facetable)
+    config.add_index_field solr_name("affiliation", :stored_searchable), label: "Departments", link_to_search: solr_name("affiliation", :facetable)
     config.add_index_field solr_name("alternative_title", :stored_searchable), label: "Alternative Title"
     config.add_index_field solr_name("arranger", :stored_searchable), label: "Arranger"
     config.add_index_field solr_name("award", :stored_searchable), label: "Award"
