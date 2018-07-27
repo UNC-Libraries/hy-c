b361cd4214b23fe090dc1c3c64e9663377a8a7d7
bbpennel <bbpennel@email.unc.edu>
2018-07-27 12:48:09 -0400

    Added query for retrieving list of all work types for use in deposit modals

---
+++ app/presenters/hyrax/unrestricted_select_type_list_presenter.rb
@@ -0,0 +1,8 @@
+module Hyrax
+  class UnrestrictedSelectTypeListPresenter < Hyrax::SelectTypeListPresenter
+    def authorized_models
+      Rails.logger.info "Getting unrestricted models"
+      @authorized_models ||= Hyrax::UnrestrictedClassificationQuery.new(@current_user).authorized_models
+    end
+  end
+end
