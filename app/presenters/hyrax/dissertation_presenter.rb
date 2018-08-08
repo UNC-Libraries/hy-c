852a888173b7c38d28edb6724e630b084cabd44d
bbpennel <bbpennel@email.unc.edu>
2018-08-08 11:31:16 -0400

    Display dcmi type parameter for works

--- app/presenters/hyrax/dissertation_presenter.rb
+++ app/presenters/hyrax/dissertation_presenter.rb
@@ -3,7 +3,7 @@
 module Hyrax
   class DissertationPresenter < Hyrax::WorkShowPresenter
     delegate :abstract, :academic_concentration, :access, :advisor, :affiliation, :alternative_title, :date_issued,
-             :degree, :degree_granting_institution, :deposit_record, :discipline, :doi, :geographic_subject,
+             :dcmi_type, :degree, :degree_granting_institution, :deposit_record, :discipline, :doi, :geographic_subject,
              :graduation_year, :note, :place_of_publication, :resource_type, :reviewer, :use, to: :solr_document
   end
 end
