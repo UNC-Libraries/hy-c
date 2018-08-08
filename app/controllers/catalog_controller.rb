aeec0d54ed0b32b91f80b0775f3289860054a686
lfarrell <lfarrell@email.unc.edu>
2018-08-08 10:36:54 -0400

    Add contributor facet back in

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -43,10 +43,10 @@ class CatalogController < ApplicationController
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
-    config.add_facet_field solr_name("advisor", :facetable),  limit: 5
-    config.add_facet_field solr_name("affiliation", :facetable), label: "Affliation", limit: 5
+    config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5
+    config.add_facet_field solr_name("affiliation", :facetable), label: "Affiliation", limit: 5
     config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collection'
-    # config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
+    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("creator", :facetable), limit: 5
     config.add_facet_field solr_name("date_issued", :facetable), label: "Date", limit: 5
     config.add_facet_field solr_name("keyword", :facetable), limit: 5
@@ -56,7 +56,7 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name("subject", :facetable), limit: 5
     config.add_facet_field solr_name("edition", :facetable), label: "Version", limit: 5
     # config.add_facet_field solr_name("based_near_label", :facetable), limit: 5
-    config.add_facet_field solr_name("file_format", :facetable), limit: 5
+    # config.add_facet_field solr_name("file_format", :facetable), limit: 5
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
