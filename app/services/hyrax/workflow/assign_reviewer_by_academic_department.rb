a91080ef12a271eb58999aee8ed8c3f1e2ce93bb
Jennifer Smith <smithj15@email.unc.edu>
2018-08-13 11:59:26 -0400

    Select first affiliation in array for reviewing; add new reviewer role if not present

--- app/services/hyrax/workflow/assign_reviewer_by_academic_department.rb
+++ app/services/hyrax/workflow/assign_reviewer_by_academic_department.rb
@@ -1,16 +1,8 @@
 module Hyrax::Workflow::AssignReviewerByAcademicDepartment
 
   def self.call(target:, **)
-    Rails.logger.info "\n\n##########\n#{target.affiliation}\n###########\n\n"
-    Rails.logger.info "\n\n##########\n#{target.affiliation.inspect}\n###########\n\n"
-    Rails.logger.info "\n\n##########\n#{target.affiliation.to_a.first}\n###########\n\n"
-    Rails.logger.info "\n\n##########\n#{target.affiliation.first}\n###########\n\n"
-    Rails.logger.info "\n\n##########\n#{target.inspect}\n###########\n\n"
     reviewer = find_reviewer_for(department: target.affiliation.first.split(',')[-1])
     permission_template_id = Hyrax::PermissionTemplate.find_by_source_id(target.admin_set_id).id
-
-    Rails.logger.info "\n\n##########\n#{target.affiliation.first.strip}\n###########\n\n"
-    Rails.logger.info "\n\n##########\n#{reviewer}\n###########\n\n"
 
     # This assigns database permissions, but does not grant permissions on the Fedora object.
     Hyrax::Workflow::PermissionGenerator.call(entity: target, agents: [reviewer],
@@ -26,8 +18,7 @@ module Hyrax::Workflow::AssignReviewerByAcademicDepartment
   end
 
   def self.find_reviewer_for(department:)
-    Rails.logger.info "\n\n##########\n#{department}\n###########\n\n"
-
+    Role.where(name: department.strip.to_s.downcase.gsub(' ', '_')+'_reviewer').first_or_create
     Sipity::Agent.where(proxy_for_id: department.strip.to_s.downcase.gsub(' ', '_')+'_reviewer',
                         proxy_for_type: 'Hyrax::Group').first_or_create
   end
