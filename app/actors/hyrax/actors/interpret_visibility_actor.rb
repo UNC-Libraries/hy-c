2ba23a25ea9f32fdba2b816e6574600f64df71f3
lfarrell <lfarrell@email.unc.edu>
2018-08-14 13:21:11 -0400

    Allow any valid user to pass visibility test, otherwise visibility status won't save for non-admins

--- app/actors/hyrax/actors/interpret_visibility_actor.rb
+++ app/actors/hyrax/actors/interpret_visibility_actor.rb
@@ -149,7 +149,7 @@ module Hyrax
       # Validate visibility complies with AdminSet template requirements
       def validate_visibility(env, attributes, template)
         # Added this to allow admins to override visibility settings in admin set
-        return true if env.current_ability.admin?
+        return true if env.current_ability.can?(:edit, env.curation_concern)
         # NOTE: For embargo/lease, attributes[:visibility] will be nil (see sanitize_params), so visibility will be validated as part of embargo/lease
         return true if attributes[:visibility].blank?
 
