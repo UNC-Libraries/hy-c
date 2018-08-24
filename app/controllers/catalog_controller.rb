7f471a681115265fef6e1f4faa351a2ffc3ea4e7
lfarrell <lfarrell@email.unc.edu>
2018-08-24 16:10:26 -0400

    Remove human_readable_type facet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -42,7 +42,7 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
+    # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5
     config.add_facet_field solr_name("affiliation", :facetable), label: "Affiliation", limit: 5
     config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collection'
