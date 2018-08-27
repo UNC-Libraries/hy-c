2107081331820ff50d9568722bda61f9301779aa
lfarrell <lfarrell@email.unc.edu>
2018-08-27 08:41:02 -0400

    Add helper for language facet text

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -7,9 +7,17 @@ module HyraxHelper
     unless /iso639-2/.match(options.to_s).nil?
       begin
         to_sentence(options[:value].map { |lang| link_to LanguagesService.label(lang), main_app.search_catalog_path(f: { language_sim: [lang] })})
-      rescue
+      rescue KeyError
         nil
       end
+    end
+  end
+
+  def language_links_facets(options)
+    begin
+      link_to LanguagesService.label(options), main_app.search_catalog_path(f: { language_sim: [options] })
+    rescue KeyError
+      options
     end
   end
 end
