196ed22d53c550595c916f5bb430aafbf7062911
lfarrell <lfarrell@email.unc.edu>
2018-09-24 15:11:04 -0400

    Update degrees and service name

---
+++ spec/services/degrees_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Hyrax::DegreesService do
+  before do
+    # Configure QA to use fixtures
+    qa_fixtures = { local_path: File.expand_path('spec/fixtures/authorities') }
+    allow(Qa::Authorities::Local).to receive(:config).and_return(qa_fixtures)
+  end
+  let(:service) { described_class }
+
+  describe "#select_all_options" do
+    it "returns all terms" do
+      expect(service.select_all_options).to include(
+                                                ['Master of Science in Information Science', 'Master of Science in Information Science'],
+                                                ['Master of Science in Library Science', 'Master of Science in Library Science'])
+    end
+  end
+
+  describe "#label" do
+    it "resolves for ids of active terms" do
+      expect(service.label('Master of Science in Information Science')).to eq("Master of Science in Information Science")
+    end
+  end
+end
+
