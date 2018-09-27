d3bc66f1c29a9075958595739e703fdcce3954b2
lfarrell <lfarrell@email.unc.edu>
2018-09-27 16:19:13 -0400

    Add tests

---
+++ spec/controllers/hyrax/art_works_controller_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate hyrax:work ArtWork`
+require 'rails_helper'
+
+RSpec.describe Hyrax::ArtWorksController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
