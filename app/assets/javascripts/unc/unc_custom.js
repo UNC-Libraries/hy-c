fef6fd236e75132d513535b8cdbf966be771b4e7
lfarrell <lfarrell@email.unc.edu>
2018-10-01 11:52:53 -0400

    Get rid of jquery datepicker and use native HTML 5 date elements

--- app/assets/javascripts/unc/unc_custom.js
+++ app/assets/javascripts/unc/unc_custom.js
@@ -2,43 +2,6 @@
  * Grouped into functional units for easier grouping of code
  */
 $(function() {
-    // Check for leading _ plus date, otherwise selects things like "update" too
-    var date_field = 'div[class*="_date"] input, input[class*="_date"]';
-
-    // Add datepicker to date fields in forms
-    function datePicking() {
-        // Remove embargo date field
-        var date_inputs = $(date_field).not('input[type=date]');
-        var datepicker_options = {
-            dateFormat: 'yy-mm-dd',
-            beforeShow: function(field) {
-                // Make sure datepicker is always top element
-                $(field).css({
-                    'position': 'relative',
-                    'z-index': 100
-                });
-            }
-        };
-
-        // Allow future dates for embargoes
-        if ($('#file_set_embargo_release_date').length > 0) {
-            datepicker_options['maxDate'] = null;
-        } else {
-            datepicker_options['maxDate'] = '+0D';
-        }
-
-        // Ensure each date field has a unique id so cloned date fields select the right input
-        date_inputs.each(function(index) {
-            var self = $(this);
-            var current_id = self.attr('id').split(/-\d{1,}$/);
-            var updated_id = current_id[0] + '-' + index;
-
-            self.attr('id', updated_id);
-            self.removeClass('hasDatepicker');
-            $('#' + updated_id).datepicker(datepicker_options);
-        });
-    }
-
     // Only show student paper options in modal when clicking "Student Papers" link on homepage
     function visibleForms() {
         var all_work_types = $('form.new-work-select .select-worktype');
@@ -58,14 +21,8 @@ $(function() {
 
     visibleForms();
 
-    // Make sure that datepicker works with cloned date fields
-    $(document).on('focus', date_field, function() {
-        datePicking();
-    });
-
     // Make sure that form visibility and datepicker work with turbolinks
     $(document).on('turbolinks:load', function() {
-        datePicking();
         visibleForms();
     });
 
@@ -77,4 +34,5 @@ $(function() {
             new Editor(element).init();
         }
     };
+
 });
