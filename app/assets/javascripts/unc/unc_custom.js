a69123984b174ceedada433477e88d8115f1dc2e
lfarrell <lfarrell@email.unc.edu>
2018-10-02 08:42:49 -0400

    Hide general and art mfa worktypes from non-admins

--- app/assets/javascripts/unc/unc_custom.js
+++ app/assets/javascripts/unc/unc_custom.js
@@ -50,10 +50,24 @@ $(function() {
             }).addClass('hidden');
         });
 
-        // Make sure all forms are visible when non student papers deposits links are clicked
+
+        // Make sure all non-admin forms are visible when non student papers deposits links are clicked from homepage
         $('.all-unc-work-types').on('click', function() {
-            all_work_types.removeClass('hidden');
+            worktypesFilter();
         });
+
+        function worktypesFilter() {
+            all_work_types.removeClass('hidden'); // Remove any currently hidden options
+
+            if (/Group.Management/.test($('span.fa-user-circle + span').text())) {
+                return; // Show all options. They're an admin
+            }
+
+            all_work_types.filter(function(index, element) {
+                var work_type = $(this).find('input[type=radio]').attr('value');
+                return /Dissertation|General|ArtMfa/.test(work_type);
+            }).addClass('hidden');
+        }
     }
 
     visibleForms();
