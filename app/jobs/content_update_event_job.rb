d1f87d8ebe8b00a5196f36d87c8499ddffe0d003
lfarrell <lfarrell@email.unc.edu>
2018-10-30 09:22:39 -0400

    Get rid of links to user profiles for depositor search

---
+++ app/jobs/content_update_event_job.rb
@@ -0,0 +1,7 @@
+# [hyc-override] Overriding to make depositor a facet search and not link to user profile
+# Log content update to activity streams
+class ContentUpdateEventJob < ContentEventJob
+  def action
+    "User #{link_to depositor, main_app.search_catalog_path(f: { depositor_sim: [depositor]})} has updated #{link_to repo_object.title.first, polymorphic_path(repo_object)}"
+  end
+end
