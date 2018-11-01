c12fee009bfbb6986149897d66807acb53fadf23
lfarrell <lfarrell@email.unc.edu>
2018-11-01 08:05:46 -0400

    Remove main_app prefix

--- app/jobs/content_new_version_job.rb
+++ app/jobs/content_new_version_job.rb
@@ -2,6 +2,6 @@
 # Log new version of a file to activity streams
 class ContentNewVersionEventJob < ContentEventJob
   def action
-    @action ||= "User #{link_to depositor, main_app.search_catalog_path(f: { depositor_tesim: [depositor]})} has added a new version of #{link_to repo_object.title.first, Rails.application.routes.url_helpers.hyrax_file_set_path(repo_object)}"
+    @action ||= "User #{link_to depositor, search_catalog_path(f: { depositor_tesim: [depositor]})} has added a new version of #{link_to repo_object.title.first, Rails.application.routes.url_helpers.hyrax_file_set_path(repo_object)}"
   end
 end
