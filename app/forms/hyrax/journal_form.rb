fa1a8ff266a17b97f4e2f728c5eb1f0b16a734b1
lfarrell <lfarrell@email.unc.edu>
2018-11-07 15:19:16 -0500

    Remove custom help text and set default language

--- app/forms/hyrax/journal_form.rb
+++ app/forms/hyrax/journal_form.rb
@@ -16,7 +16,7 @@ module Hyrax
     self.single_value_fields = [:title, :license, :rights_statement]
     
     self.admin_only_terms = [:dcmi_type, :access, :date_created, :use]
-    self.default_term_values = { :dcmi_type => ["http://purl.org/dc/dcmitype/Text"] }
+    self.default_term_values = { :dcmi_type => ["http://purl.org/dc/dcmitype/Text"], :language => ["http://id.loc.gov/vocabulary/iso639-2/eng"] }
 
     # Add overrides for required properties which are becoming single-valued
 
