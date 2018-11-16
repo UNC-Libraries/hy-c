abaed1448d9bc5b15572264a37e7c396e355efd9
Jennifer Smith <smithj15@email.unc.edu>
2018-11-16 15:22:14 -0500

    Add person class to scholarly work people

--- app/presenters/hyrax/scholarly_work_presenter.rb
+++ app/presenters/hyrax/scholarly_work_presenter.rb
@@ -2,8 +2,7 @@
 #  `rails generate hyrax:work ScholarlyWork`
 module Hyrax
   class ScholarlyWorkPresenter < Hyrax::WorkShowPresenter
-    delegate :abstract, :advisor, :affiliation, :affiliation_label, :conference_name, :date_issued, :dcmi_type,
-             :deposit_record, :doi, :geographic_subject, :language_label, :license_label, :orcid, :other_affiliation,
-             :rights_statement_label, to: :solr_document
+    delegate :abstract, :advisor_display, :conference_name, :creator_display, :date_issued, :dcmi_type, :deposit_record,
+             :doi, :geographic_subject, :language_label, :license_label, :rights_statement_label, to: :solr_document
   end
 end
