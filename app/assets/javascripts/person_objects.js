32dfd789a1b9368b21020fc764cf125ff7b99d5a
Jennifer Smith <smithj15@email.unc.edu>
2018-11-21 14:11:38 -0500

    Update person js

--- app/assets/javascripts/person_objects.js
+++ app/assets/javascripts/person_objects.js
@@ -1,13 +1,9 @@
 $(document).on('turbolinks:load', function () {
-    attach_add_person_listeners('advisor');
-    attach_add_person_listeners('arranger');
-    attach_add_person_listeners('composer');
-    attach_add_person_listeners('contributor');
-    attach_add_person_listeners('creator');
-    attach_add_person_listeners('project_director');
-    attach_add_person_listeners('researcher');
-    attach_add_person_listeners('reviewer');
-    attach_add_person_listeners('translator');
+    var people = ['advisor', 'arranger', 'composer', 'contributor', 'creator', 'project_director', 'researcher',
+        'reviewer', 'translator'];
+    $(people).each(function(index, person) {
+        attach_add_person_listeners(person);
+    });
 });
 
 
@@ -17,8 +13,9 @@ function attach_add_person_listeners(selector){
     var cloning_row = '#'+selector+'-cloning_row';
 
     $(cloning_row).find('input').each(function() {
-        if ($(this).prop('id').match('_id')) {
-            $(this).remove();
+        var $self = $(this);
+        if ($self.prop('id').match('_id')) {
+            $self.remove();
         }
     });
 
@@ -34,41 +31,11 @@ function attach_add_person_listeners(selector){
 
         var $new_row = $(cloning_row+' > .row').clone();
 
-        var $name_input = $new_row.find('div.'+selector+'-name input');
-        var $orcid_input = $new_row.find('div.'+selector+'-orcid input');
-        var $affiliation_input = $new_row.find('div.'+selector+'-affiliation select');
-        var $other_affiliation_input = $new_row.find('div.'+selector+'-other-affiliation input');
+        updateNewRow($new_row, selector, 'name', current_index);
+        updateNewRow($new_row, selector, 'orcid', current_index);
+        updateNewRow($new_row, selector, 'affiliation', current_index);
+        updateNewRow($new_row, selector, 'other-affiliation', current_index);
 
-        var old_name = $name_input.prop('name');
-        var old_orcid = $orcid_input.prop('name');
-        var old_affiliation = $affiliation_input.prop('name');
-        var old_other_affiliation = $other_affiliation_input.prop('name');
-
-        var new_name = old_name.replace(/\d/, current_index);
-        var new_orcid = old_orcid.replace(/\d/, current_index);
-        var new_affiliation = old_affiliation.replace(/\d/, current_index);
-        var new_other_affiliation = old_other_affiliation.replace(/\d/, current_index);
-
-        $name_input.prop('name', new_name);
-        $orcid_input.prop('name', new_orcid);
-        $affiliation_input.prop('name', new_affiliation);
-        $other_affiliation_input.prop('name', new_other_affiliation);
-
-        var old_name_id = $name_input.attr('id');
-        var old_orcid_id = $orcid_input.attr('id');
-        var old_affiliation_id = $affiliation_input.attr('id');
-        var old_other_affiliation_id = $other_affiliation_input.attr('id');
-
-        var new_name_id = old_name_id.replace(/\d/, current_index);
-        var new_orcid_id = old_orcid_id.replace(/\d/, current_index);
-        var new_affiliation_id = old_affiliation_id.replace(/\d/, current_index);
-        var new_other_affiliation_id = old_other_affiliation_id.replace(/\d/, current_index);
-
-        $name_input.attr('id', new_name_id);
-        $orcid_input.attr('id', new_orcid_id);
-        $affiliation_input.attr('id', new_affiliation_id);
-        $other_affiliation_input.attr('id', new_other_affiliation_id);
-        
         //change $new_row's id so we don't find it again when looking for blank row to clone
         $new_row.prop('id', 'cloned_'+selector+'_row');
 
@@ -91,22 +58,23 @@ function remove_row(selector) {
     var cloning_row = '#'+selector+'-cloning_row';
 
     $(remove_selector).on('click', function (event) {
-        // get current_index again and store it in local variable, decrement it and store it
-        var current_index = $('#index-'+selector).val();
-        current_index--;
+        var $self = $(this);
 
-        if (current_index == 1) {
-            $('.remove-'+selector).addClass('hidden');
+        // get current count and store it in local variable
+        var count = $(row_selector+':visible').length;
+        count--;
+
+        if (count == 1) {
+            $(remove_selector).addClass('hidden');
         } else {
-            $('.remove-'+selector).removeClass('hidden');
+            $(remove_selector).removeClass('hidden');
         }
 
         var name_input = $(cloning_row+' > .row').find('div.'+selector+'-name input').prop('name');
         var model = name_input.split('[')[0];
-        var index = $(this).data('index');
+        var index = $self.data('index');
 
-        $('#index-'+selector).val(current_index);
-        $(this).parents(row_selector).remove();
+        $self.parents(row_selector).remove();
         if ($('#'+model+ '_'+selector+'s_attributes_'+index+'_id').length) {
             delete_record(selector, model, index);
         }
@@ -117,4 +85,10 @@ function delete_record(selector, model, index) {
     var $new_row = '<input type="hidden" name="'+model+'['+selector+'s_attributes]['+index+'][_destroy]" id="'+model+
         '_'+selector+'s_attributes_'+index+'__destroy" value="1">';
     $('div#'+selector).append($new_row);
+}
+
+function updateNewRow(new_row, selector, attr, index) {
+    var $input = $(new_row).find('div.'+selector+'-'+attr+' input,select');
+    $input.prop('name', $input.prop('name').replace(/\d/, index));
+    $input.attr('id', $input.attr('id').replace(/\d/, index));
 }
