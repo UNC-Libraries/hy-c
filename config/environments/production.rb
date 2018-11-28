3b3531932c744c8b5b9900549e447cc53bbae3ff
Jennifer Smith <smithj15@email.unc.edu>
2018-11-28 08:33:27 -0500

    change passenger version to 5.3.5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.logger = ActiveSupport::Logger.new(ENV["LOGS_PATH"])
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
