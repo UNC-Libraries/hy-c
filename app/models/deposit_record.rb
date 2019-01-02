9d5d67a5722f8a2f80fe0659aa27601d0c238762
Jennifer Smith <smithj15@email.unc.edu>
2019-01-02 10:11:34 -0500

    Remove audit metadata from deposit record

--- app/models/deposit_record.rb
+++ app/models/deposit_record.rb
@@ -4,13 +4,6 @@ class DepositRecord < ActiveFedora::Base
   # OAI_DC info
   property :title, predicate: ::RDF::Vocab::DC.title, multiple: false
   property :identifier, predicate: ::RDF::Vocab::DC.identifier, multiple: false
-  # audit trail info
-  property :audit_process, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditProcess'), multiple: false
-  property :audit_action, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditAction'), multiple: false
-  property :audit_component_id, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditComponentID'), multiple: false
-  property :audit_responsibility, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditResponsibility'), multiple: false
-  property :audit_date, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditDate'), multiple: false
-  property :audit_justification, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#auditJustification'), multiple: false
   # RDF description
   property :deposit_method, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#depositMethod'), multiple: false
   property :deposit_package_subtype, predicate: ::RDF::URI('http://cdr.unc.edu/definitions/model#depositPackageSubtype'), multiple: false
