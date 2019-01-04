b8c7af57183484aebd4734b7988fa42dbf4d39d3
bbpennel <bbpennel@email.unc.edu>
2019-01-04 14:48:22 -0500

    Allow admins to attach children owned by other users

--- app/search_builders/hyrax/my/search_builder.rb
+++ app/search_builders/hyrax/my/search_builder.rb
@@ -1,3 +1,4 @@
+# [hyc-override] allow admins to attach child owned by another user
 module Hyrax
   module My
     # Search builder for things that the current user has deposited and has edit access to
@@ -7,14 +8,19 @@ module Hyrax
       include Hyrax::My::SearchBuilderBehavior
       self.default_processor_chain += [:show_only_resources_deposited_by_current_user]
 
+      # [hyc-override] filter by depositor if not admin
       # adds a filter to the solr_parameters that filters the documents the current user
       # has deposited
       # @param [Hash] solr_parameters
       def show_only_resources_deposited_by_current_user(solr_parameters)
+        user_id = ::User.where(uid: current_user_key).first.id
+        usergroup = Role.select('name').joins(:roles_users).where('roles_users.user_id = ?', user_id)
         solr_parameters[:fq] ||= []
-        solr_parameters[:fq] += [
-          ActiveFedora::SolrQueryBuilder.construct_query_for_rel(depositor: current_user_key)
-        ]
+        if !(usergroup.map{|role| role.name}.include? 'admin')
+          solr_parameters[:fq] += [
+              ActiveFedora::SolrQueryBuilder.construct_query_for_rel(depositor: current_user_key)
+          ]
+        end
       end
     end
   end
