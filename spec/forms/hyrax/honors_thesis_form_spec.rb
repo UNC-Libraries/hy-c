5f32eda2cb22d53ed065ddc175dd453d18f9dbd0
lfarrell <lfarrell@email.unc.edu>
2019-01-17 14:44:06 -0500

    Honors form updates

--- spec/forms/hyrax/honors_thesis_form_spec.rb
+++ spec/forms/hyrax/honors_thesis_form_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Hyrax::HonorsThesisForm do
   describe "#required_fields" do
     subject { form.required_fields }
 
-    it do  is_expected.to match_array [:title, :abstract, :advisor, :affiliation, :award, :creator, :date_issued, :degree,
+    it do  is_expected.to match_array [:title, :abstract, :advisor, :affiliation, :creator, :date_issued, :degree,
                                        :graduation_year, :degree_granting_institution]
     end
   end
@@ -17,7 +17,7 @@ RSpec.describe Hyrax::HonorsThesisForm do
   describe "#primary_terms" do
     subject { form.primary_terms }
 
-    it do  is_expected.to match_array [:title, :abstract, :advisor, :award, :creator, :date_issued, :degree,
+    it do  is_expected.to match_array [:title, :abstract, :advisor, :creator, :date_issued, :degree,
                                        :graduation_year, :degree_granting_institution]
     end
   end
@@ -25,7 +25,7 @@ RSpec.describe Hyrax::HonorsThesisForm do
   describe "#secondary_terms" do
     subject { form.secondary_terms }
 
-    it { is_expected.to match_array [:access, :academic_concentration, :date_created, :dcmi_type, :doi, :extent,
+    it { is_expected.to match_array [:access, :academic_concentration, :award, :date_created, :dcmi_type, :doi, :extent,
                                      :geographic_subject, :note, :use, :language, :license, :resource_type,
                                      :rights_statement, :subject, :keyword, :related_url, :language_label,
                                      :license_label, :rights_statement_label] }
@@ -34,7 +34,7 @@ RSpec.describe Hyrax::HonorsThesisForm do
   describe "#admin_only_terms" do
     subject { form.admin_only_terms }
 
-    it { is_expected.to match_array [:dcmi_type, :access, :doi, :use] }
+    it { is_expected.to match_array [:dcmi_type, :access, :academic_concentration, :award, :doi, :extent, :use] }
   end
   
   describe 'default value set' do
