5b4c15c8d4535c7ddb65fa7f7a9c890408903c81
lfarrell <lfarrell@email.unc.edu>
2019-01-24 11:24:47 -0500

    Allow deletion of people objects on edit

--- app/assets/javascripts/person_objects.js
+++ app/assets/javascripts/person_objects.js
@@ -3,11 +3,11 @@ $(document).on('turbolinks:load', function () {
         'reviewer', 'translator'];
     people.forEach(function(person) {
         attach_add_person_listeners(person);
-        updateAllRows('.'+ person + '.row');
+        updateAllRows('.'+ person + '.row', false, $('#index-label-' + person).val());
     });
 });
 
-function attach_add_person_listeners(selector){
+function attach_add_person_listeners(selector) {
     var add_selector = '#add-another-'+selector;
     var remove_selector = '.remove-'+selector;
     var cloning_row = '#'+selector+'-cloning_row';
@@ -19,20 +19,22 @@ function attach_add_person_listeners(selector){
         }
     });
 
-    remove_row(selector, parseInt($('#index-'+selector).val()));
+    remove_row(selector);
 
-    $(add_selector).on('click', function(event){
+    $(add_selector).on('click', function(event) {
         // stop page from reloading
         event.preventDefault();
 
         $(remove_selector).removeClass('hidden');
 
         var index_selector = $('#index-'+selector);
+        var label_index_selector = $('#index-label-'+selector);
         var current_index = index_selector.val();
+        var current_label_index = label_index_selector.val();
         var $new_row = $(cloning_row+' > .row').clone();
 
         // Update inputs and labels for new row fields
-        updateFields($new_row , current_index);
+        updateFields($new_row , false, current_index, parseInt(current_label_index));
 
         //change $new_row's id so we don't find it again when looking for blank row to clone
         $new_row.prop('id', 'cloned_'+selector+'_row');
@@ -44,13 +46,15 @@ function attach_add_person_listeners(selector){
         $('div#'+selector).append($new_row);
 
         current_index++;
+        current_label_index++;
         index_selector.val(current_index);
+        label_index_selector.val(current_label_index);
 
-        remove_row(selector, parseInt(current_index));
+        remove_row(selector);
     });
 }
 
-function remove_row(selector, current_index) {
+function remove_row(selector) {
     var row_selector = '.'+selector+'.row';
     var remove_selector = '.remove-'+selector;
     var cloning_row = '#'+selector+'-cloning_row';
@@ -62,18 +66,19 @@ function remove_row(selector, current_index) {
         var name_input = $(cloning_row+' > .row').find('div.'+selector+'-name input').prop('name');
         var model = name_input.split('[')[0];
         var index = $self.data('index');
+        var label_index = $('#index-label-'+selector);
+        var label_index_val = label_index.val();
 
         $self.parents(row_selector).remove();
 
         // Trigger a removal event for the overall person object field since the specific instance clicked no longer exists.
         $('#'+selector).trigger("managed_field:remove");
 
-        current_index--;
-        $('#index-'+selector).val(current_index);
-
+        label_index_val--;
+        label_index.val(label_index_val);
 
         // Update row ordering
-        updateAllRows(row_selector);
+        updateAllRows(row_selector, true, label_index_val);
 
         if ($('#'+model+ '_'+selector+'s_attributes_'+index+'_id').length) {
             delete_record(selector, model, index);
@@ -82,42 +87,43 @@ function remove_row(selector, current_index) {
 }
 
 function delete_record(selector, model, index) {
-    var $new_row = '<input type="hidden" name="'+model+'['+selector+'s_attributes]['+index+'][_destroy]" id="'+model+
-        '_'+selector+'s_attributes_'+index+'__destroy" value="1">';
-    $('div#'+selector).append($new_row);
+    if ($('#' + model+'_'+selector+'s_attributes_'+index+'__destroy').length === 0) {
+        var $new_row = '<input type="hidden" name="'+model+'['+selector+'s_attributes]['+index+'][_destroy]" id="'+model+
+            '_'+selector+'s_attributes_'+index+'__destroy" value="1">';
+        $('div#'+selector).append($new_row);
+    }
 }
 
-function updateAllRows(row_selector) {
+function updateAllRows(row_selector, is_deletion, label_index) {
     $(row_selector).not(':hidden').each(function(row_index) {
         var self = $(this);
-        updateFields(self, row_index);
+        var offset = label_index - row_index;
+        updateFields(self, is_deletion, row_index, label_index - offset);
     });
 }
 
-function updateFields(row, current_index) {
-    ['name', 'orcid', 'affiliation', 'other_affiliation'].forEach(function(field) {
-        updateRow(row, field, current_index);
-    });
-}
-
-function updateRow(new_row, attr, index) {
+function updateFields(row, is_deletion, index, label_index) {
     var regex = /\d+/;
 
-    // Update input
-    var $input = new_row.find("[id$='" + attr + "']");
-    
-    $input.each(function() {
-        var self = $(this);
-        self.prop('name', self.prop('name').replace(regex, index))
-            .attr('id', self.attr('id').replace(regex, index));
-    });
-    
-    // Update label
-    var $label = new_row.find("label[for$='" + attr + "']");
+    ['name', 'orcid', 'affiliation', 'other_affiliation'].forEach(function(attr) {
+        // Update input
+        if (!is_deletion) {
+            var $input = row.find("[id$='" + attr + "']");
 
-    $label.each(function() {
-        var self = $(this);
-        self.attr('for', self.attr('for').replace(regex, index))
-            .html(self.html().replace(regex, parseInt(index) + 1));
+            $input.each(function() {
+                var self = $(this);
+                self.prop('name', self.prop('name').replace(regex, index))
+                    .attr('id', self.attr('id').replace(regex, index));
+            });
+        }
+
+        // Update label
+        var $label = row.find("label[for$='" + attr + "']");
+
+        $label.each(function() {
+            var self = $(this);
+            self.attr('for', self.attr('for').replace(regex, label_index))
+                .html(self.html().replace(regex, label_index + 1));
+        });
     });
 }
