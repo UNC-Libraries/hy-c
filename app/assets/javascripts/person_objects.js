25a779c67e588f90e589916c50b1d8bbba747e31
lfarrell <lfarrell@email.unc.edu>
2019-01-25 16:30:00 -0500

    switch to data attributes

--- app/assets/javascripts/person_objects.js
+++ app/assets/javascripts/person_objects.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function () {
     ['advisor', 'arranger', 'composer', 'contributor', 'creator', 'project_director', 'researcher',
         'reviewer', 'translator'].forEach(function(person) {
+        setPersonIndex(person);
         attachAddPersonListeners(person);
         updateAllRows('.'+ person + '.row', getLabelNumber(person));
     });
@@ -26,9 +27,9 @@ function attachAddPersonListeners(selector) {
 
         $(remove_selector).removeClass('hidden');
 
-        var index_selector = $('#index-'+selector);
-        var current_index = index_selector.val();
-        var $new_row = $(cloning_row+' > .row').clone();
+        var index_selector = $('#' + selector);
+        var current_index = index_selector.attr('data-' + selector);
+        var $new_row = $(cloning_row + ' > .row').clone();
 
         // Update inputs and labels for new row fields
         var regex = /\d+/;
@@ -46,43 +47,42 @@ function attachAddPersonListeners(selector) {
         updateLabels($new_row , current_index, getLabelNumber(selector));
 
         //change $new_row's id so we don't find it again when looking for blank row to clone
-        $new_row.prop('id', 'cloned_'+selector+'_row');
+        $new_row.prop('id', 'cloned_' + selector + '_row');
 
         var $removeMember = $new_row.find(remove_selector);
         $($removeMember).data('index', current_index);
 
         $new_row.removeClass('hidden');
-        $('div#'+selector).append($new_row);
+        $('div#' + selector).append($new_row);
 
-        current_index++;
-        index_selector.val(current_index);
+        setPersonIndex(selector);
 
         removeRow(selector);
     });
 }
 
 function removeRow(selector) {
-    var row_selector = '.'+selector+'.row';
-    var remove_selector = '.remove-'+selector;
-    var cloning_row = '#'+selector+'-cloning_row';
+    var row_selector = '.' + selector + '.row';
+    var remove_selector = '.remove-' + selector;
+    var cloning_row = '#' + selector + '-cloning_row';
 
     $(remove_selector).on('click', function (event) {
         event.preventDefault();
 
         var $self = $(this);
-        var name_input = $(cloning_row+' > .row').find('div.'+selector+'-name input').prop('name');
+        var name_input = $(cloning_row + ' > .row').find('div.' + selector + '-name input').prop('name');
         var model = name_input.split('[')[0];
         var index = $self.data('index');
 
         $self.parents(row_selector).remove();
 
         // Trigger a removal event for the overall person object field since the specific instance clicked no longer exists.
-        $('#'+selector).trigger("managed_field:remove");
+        $('#' + selector).trigger("managed_field:remove");
 
         // Update row ordering
         updateAllRows(row_selector, getLabelNumber(selector));
 
-        if ($('#'+model+ '_'+selector+'s_attributes_'+index+'_id').length) {
+        if ($('#' + model + '_' + selector + 's_attributes_' + index + '_id').length) {
             deleteRecord(selector, model, index);
         }
     });
@@ -92,11 +92,24 @@ function getLabelNumber(selector) {
     return $('#' + selector + ' div.' + selector).not(':hidden').length;
 }
 
+function setPersonIndex(selector) {
+    var data_index = $('#' + selector);
+
+    if (data_index.length > 0) {
+        var current_index = data_index.data(selector);
+        var set_value = (current_index > 0) ? current_index : getLabelNumber(selector);
+
+        data_index.attr('data-' + selector, set_value);
+    }
+}
+
 function deleteRecord(selector, model, index) {
-    if ($('#' + model+'_'+selector+'s_attributes_'+index+'__destroy').length === 0) {
-        var $new_row = '<input type="hidden" name="'+model+'['+selector+'s_attributes]['+index+'][_destroy]" id="'+model+
-            '_'+selector+'s_attributes_'+index+'__destroy" value="1">';
-        $('div#'+selector).append($new_row);
+    var select_string = model + '_' + selector + 's_attributes_'+ index +'__destroy';
+
+    if ($('#' + select_string).length === 0) {
+        var $new_row = '<input type="hidden" name="' + model + '[' + selector + 's_attributes][' + index +' ' +
+            '][_destroy]" id="' + select_string + '" value="1">';
+        $('div#' + selector).append($new_row);
     }
 }
 
