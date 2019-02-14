469a77ed828ae9a2cebc03603e32e7fdae6e747a
lfarrell <lfarrell@email.unc.edu>
2019-02-14 12:02:30 -0500

    add hostname to available fields

---
+++ app/lib/staccato/event.rb
@@ -0,0 +1,22 @@
+# [hyc-override] Overriding staccato gem to allow hostname as a field.
+module Staccato
+  # Event Hit type field definitions
+  # @author Tony Pitale
+  class Event
+    # Event field definitions
+    FIELDS = {
+        category: 'ec',
+        action: 'ea',
+        label: 'el',
+        value: 'ev',
+        hostname: 'dh'
+    }
+
+    include Hit
+
+    # event hit type
+    def type
+      :event
+    end
+  end
+end
