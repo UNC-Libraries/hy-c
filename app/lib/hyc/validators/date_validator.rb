d4b7e442f0f7243807934e48de8ea38f1410874d
lfarrell <lfarrell@email.unc.edu>
2019-02-19 11:01:04 -0500

    make case insensitive and allow 2000-2010

--- app/lib/hyc/validators/date_validator.rb
+++ app/lib/hyc/validators/date_validator.rb
@@ -20,13 +20,14 @@ module Hyc::Validators
     private
 
     def valid_value(record, field)
-      # ([Jj]an(uary)?|[Ff]eb(ruary)?|[Mm]ar(ch)?|[Aa]pr(il)?|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ug(ust)?|[Ss]ept(ember)?|[Oo]ct(ober)?|[Nn]ov(ember)?|[Dd]ec(ember)?|[Cc]irca|[Ss]pring|[Ss]ummer|[Ff]all|[Ww]inter|[Aa]utumn)?\s{0,}\d{4}s? matches Spring 2000, July 2000, 2000, 2000s, circa 2000
+      # (jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?|circa|spring|summer|fall|winter|autumn)?\s{0,}\d{4}s? matches Spring 2000, Aug 2000, July 2000, 2000, 2000s, circa 2000
       # \d{4}(\\|\/|-)\d{2}(\\|\/|-)\d{2} matches 2000-01-01, 2000/01/01, 2000\01\01
       # \d{2}(\\|\/|-)\d{2}(\\|\/|-)\d{4} matches 01-01-2000, 01/01/2000, 01\01\2000
-      # \d{4}\s{0,}to\s{0,}\d{4} matches 2000 to 2010
-      # ([Jj]an(uary)?|[Ff]eb(ruary)?|[Mm]ar(ch)?|[Aa]pr(il)?|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ug(ust)?|[Ss]ept(ember)?|[Oo]ct(ober)?|[Nn]ov(ember)?|[Dd]ec(ember))\s+\d{1,2}[a-zA-Z]{0,2},?\s{0,}\d{4} matches July 1st 2000, July 1st, 2000, July 1 2000, July 1, 2000
-      # [Uu]nknown) matches Unknown, unknown
-      date_regex = /^(([Jj]an(uary)?|[Ff]eb(ruary)?|[Mm]ar(ch)?|[Aa]pr(il)?|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ug(ust)?|[Ss]ept(ember)?|[Oo]ct(ober)?|[Nn]ov(ember)?|[Dd]ec(ember)?|[Cc]irca|[Ss]pring|[Ss]ummer|[Ff]all|[Ww]inter|[Aa]utumn)?\s{0,}\d{4}s?|\d{4}(\\|\/|-)\d{2}(\\|\/|-)\d{2}|\d{2}(\\|\/|-)\d{2}(\\|\/|-)\d{4}|\d{4}\s{0,}to\s{0,}\d{4}|([Jj]an(uary)?|[Ff]eb(ruary)?|[Mm]ar(ch)?|[Aa]pr(il)?|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ug(ust)?|[Ss]ept(ember)?|[Oo]ct(ober)?|[Nn]ov(ember)?|[Dd]ec(ember))\s+\d{1,2}[a-zA-Z]{0,2},?\s{0,}\d{4}|[Uu]nknown|^$)\Z/
+      # \d{4}(\s{0,}to\s{0,}|-)\d{4} matches 2000 to 2010, 2000-2010
+      # (jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?)\s+\d{1,2}[a-zA-Z]{0,2},?\s{0,}\d{4} matches July 1st 2000, July 1st, 2000, July 1 2000, July 1, 2000
+      # unknown matches Unknown, unknown
+      # ^$ matches empty string
+      date_regex = /^((jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?|circa|spring|summer|fall|winter|autumn)?\s{0,}\d{4}s?|\d{4}(\\|\/|-)\d{2}(\\|\/|-)\d{2}|\d{2}(\\|\/|-)\d{2}(\\|\/|-)\d{4}|\d{4}(\s{0,}to\s{0,}|-)\d{4}|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|june|july|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?)\s+\d{1,2}[a-zA-Z]{0,2},?\s{0,}\d{4}|unknown|^$)\Z/i
       !record[field].to_s.match(date_regex).nil?
     end
 
