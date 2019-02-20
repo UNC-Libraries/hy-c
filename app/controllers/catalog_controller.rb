b2e54cf2116ce08272a9237d4720d61be761fe4e
smithjp <smithj15@email.unc.edu>
2019-02-20 16:01:41 -0500

    Missing data (#290)
    * Allow honors thesis people objects to be saved without an affiliation
    
    * Update project director role term in migration script
    
    * Add missing metadata properties to models
    
    * Display date captured
    
    * Add date captured into form, presenter and solr.
    
    * Replace https with http in license urls during migration
    
    * Updated tests
    
    * Add url field back to honors theses. Make date_captured admin only in article form

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -74,6 +74,7 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false
     config.add_index_field solr_name("creator_label", :stored_searchable), label: "Creator", itemprop: 'creator', link_to_search: solr_name("creator", :facetable)
     config.add_index_field solr_name("date_created", :stored_searchable), itemprop: 'dateCreated', label: "Date created"
+    config.add_index_field solr_name("date_captured", :stored_searchable), itemprop: 'dateCaptured', label: "Date captured"
     config.add_index_field solr_name("date_issued", :stored_searchable), label: "Date of publication"
     config.add_index_field solr_name("abstract", :stored_searchable), label: "Abstract"
     config.add_index_field solr_name("resource_type", :stored_searchable), label: "Resource type", link_to_search: solr_name("resource_type", :facetable)
--- app/forms/hyrax/article_form.rb
+++ app/forms/hyrax/article_form.rb
@@ -6,7 +6,7 @@ module Hyrax
     class_attribute :single_value_fields
 
     self.model_class = ::Article
-    self.terms += [:resource_type, :abstract, :access, :alternative_title, :bibliographic_citation, :copyright_date, :date_issued,
+    self.terms += [:resource_type, :abstract, :access, :alternative_title, :bibliographic_citation, :copyright_date, :date_captured, :date_issued,
                    :date_other, :dcmi_type, :digital_collection, :doi, :edition, :extent, :funder, :geographic_subject, :issn,
                    :journal_title, :journal_volume, :journal_issue, :note, :page_start, :page_end, :peer_review_status,
                    :place_of_publication, :rights_holder, :translator, :use]
@@ -17,7 +17,7 @@ module Hyrax
     
     self.single_value_fields = [:title, :license]
     
-    self.admin_only_terms = [:dcmi_type, :date_created, :access, :bibliographic_citation, :copyright_date, :date_other,
+    self.admin_only_terms = [:dcmi_type, :date_created, :access, :bibliographic_citation, :copyright_date, :date_captured, :date_other,
                              :digital_collection, :doi, :extent, :identifier, :rights_holder, :translator, :use]
     self.default_term_values = { :dcmi_type => ["http://purl.org/dc/dcmitype/Text"], :language => ["http://id.loc.gov/vocabulary/iso639-2/eng"],
                                  :rights_statement => "http://rightsstatements.org/vocab/InC/1.0/" }
