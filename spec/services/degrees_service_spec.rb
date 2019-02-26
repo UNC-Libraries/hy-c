c5a5e3875589c9e5b4df37404bc6aa718a2987a0
lfarrell <lfarrell@email.unc.edu>
2019-02-26 15:18:01 -0500

    Add backwards compatible degree mappings

--- spec/services/degrees_service_spec.rb
+++ spec/services/degrees_service_spec.rb
@@ -12,13 +12,30 @@ RSpec.describe Hyrax::DegreesService do
     it "returns all terms" do
       expect(service.select_all_options).to include(
                                                 ['Master of Science in Information Science', 'Master of Science in Information Science'],
+                                                ['Master of Science in Library Science', 'Master of Science in Library Science'],
+                                                ['Master of Science in Information Science', 'MSIS'])
+    end
+  end
+
+  describe "#select_active_options" do
+    it "returns all active terms" do
+      expect(service.select_active_options).to include(
+                                                ['Master of Science in Information Science', 'Master of Science in Information Science'],
                                                 ['Master of Science in Library Science', 'Master of Science in Library Science'])
+    end
+
+    it "does not return inactive terms" do
+      expect(service.select_active_options).not_to include(['Master of Science in Information Science', 'MSIS'])
     end
   end
 
   describe "#label" do
     it "resolves for ids of active terms" do
       expect(service.label('Master of Science in Information Science')).to eq("Master of Science in Information Science")
+    end
+
+    it "resolves for ids of inactive terms" do
+      expect(service.label('MSIS')).to eq("Master of Science in Information Science")
     end
   end
 end
