1cdd58c55aa0188548a9fb41a7d0030ca94b39bc
lfarrell <lfarrell@email.unc.edu>
2019-03-12 11:22:00 -0400

    Show custom label for geonames display

--- app/indexers/hyrax/deep_indexing_service.rb
+++ app/indexers/hyrax/deep_indexing_service.rb
@@ -1,3 +1,5 @@
+# [hyc-override] Overriding to add method to return custom label and save it to Solr
+# Note: Customizations are very specific to the Location controlled vocabulary
 require 'linkeddata' # we need all the linked data types, because we don't know what types a service might return.
 module Hyrax
   class DeepIndexingService < BasicMetadataIndexer
@@ -9,6 +11,7 @@ module Hyrax
     # @param [ActiveTriples::Resource, String] val
     def append_to_solr_doc(solr_doc, solr_field_key, field_info, val)
       return super unless object.controlled_properties.include?(solr_field_key.to_sym)
+
       case val
       when ActiveTriples::Resource
         append_label_and_uri(solr_doc, solr_field_key, field_info, val)
@@ -25,6 +28,14 @@ module Hyrax
     end
 
     private
+
+    # [hyc-override] Method to return custom label
+    def parse_geo_request(location)
+      geo_id = location.match(/\d+/)[0]
+      request = HTTParty.get("http://api.geonames.org/getJSON?geonameId=#{geo_id}&username=#{ENV['GEONAMES_USER']}")
+      response = JSON.parse(request.body)
+      "#{response["asciiName"]}, #{response["adminName1"]}, #{response["countryName"]}"
+    end
 
     # Grab the labels for controlled properties from the remote sources
     def fetch_external
@@ -66,7 +77,8 @@ module Hyrax
     # @param [Hash] field_info
     # @param [Array] val an array of two elements, first is a string (the uri) and the second is a hash with one key: `:label`
     def append_label_and_uri(solr_doc, solr_field_key, field_info, val)
-      val = val.solrize
+      full_label = parse_geo_request(val.to_uri.to_s)
+      val = val.solrize(full_label)
       self.class.create_and_insert_terms(solr_field_key,
                                          val.first,
                                          field_info.behaviors, solr_doc)
@@ -83,11 +95,13 @@ module Hyrax
     # @param [Hash] field_info
     # @param [String] val
     def append_label(solr_doc, solr_field_key, field_info, val)
+      full_label = parse_geo_request(val.to_uri.to_s)
+
       self.class.create_and_insert_terms(solr_field_key,
-                                         val,
+                                         full_label,
                                          field_info.behaviors, solr_doc)
       self.class.create_and_insert_terms("#{solr_field_key}_label",
-                                         val,
+                                         full_label,
                                          field_info.behaviors, solr_doc)
     end
 
--- lib/hyrax/controlled_vocabularies/location.rb
+++ lib/hyrax/controlled_vocabularies/location.rb
@@ -1,12 +1,13 @@
+# [hyc-override] Overriding to add full label parameter
 module Hyrax
   module ControlledVocabularies
     class Location < ActiveTriples::Resource
       configure rdf_label: ::RDF::Vocab::GEONAMES.name
 
       # Return a tuple of url & label
-      def solrize
+      def solrize(full_label)
         return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label.first.to_s == rdf_subject.to_s
-        [rdf_subject.to_s, { label: "#{rdf_label.first}$#{rdf_subject}" }]
+        [rdf_subject.to_s, { label: "#{full_label}$#{rdf_subject}" }]
       end
     end
   end
