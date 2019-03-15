fd5cd8af85cd6a97ccf04cef4082f57b3615c86f
lfarrell <lfarrell@email.unc.edu>
2019-03-15 15:10:50 -0400

    Add custom indexing to humanize dates and have all indexers inherit from a base class

---
+++ app/indexers/hyc_indexer.rb
@@ -0,0 +1,17 @@
+class HycIndexer < Hyrax::WorkIndexer
+  # This indexes the default metadata. You can remove it if you want to
+  # provide your own metadata and indexing.
+  include Hyrax::IndexesBasicMetadata
+
+  # Fetch remote labels for based_near. You can remove this if you don't want
+  # this behavior
+  include Hyrax::IndexesLinkedMetadata
+
+  # Uncomment this block if you want to add custom indexing behavior:
+  def generate_solr_document
+    super.tap do |solr_doc|
+      solr_doc['date_created_tesim'] = Hyc::EdtfConvert.convert_from_edtf(object.date_created)
+      solr_doc['date_issued_tesim'] = Hyc::EdtfConvert.convert_from_edtf(object.date_issued)
+    end
+  end
+end
