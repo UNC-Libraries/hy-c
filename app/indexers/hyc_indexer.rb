a4f34949b240d392f4e686c0b7cfb81304eab611
Jennifer Smith <smithj15@email.unc.edu>
2019-03-19 10:59:38 -0400

    Allow multiple publication dates to be indexed

--- app/indexers/hyc_indexer.rb
+++ app/indexers/hyc_indexer.rb
@@ -11,7 +11,7 @@ class HycIndexer < Hyrax::WorkIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc['date_created_tesim'] = Hyc::EdtfConvert.convert_from_edtf(object.date_created)
-      solr_doc['date_issued_tesim'] = Hyc::EdtfConvert.convert_from_edtf(object.date_issued)
+      solr_doc['date_issued_tesim'] = Array(object.date_issued).map {|date| Hyc::EdtfConvert.convert_from_edtf(date)}
     end
   end
 end
