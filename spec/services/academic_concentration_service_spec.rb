89ab7ee1c952e5600e03ee647259ff3f1b9d3b36
lfarrell <lfarrell@email.unc.edu>
2019-03-25 16:09:42 -0400

    Update concentration service tests

--- spec/services/academic_concentration_service_spec.rb
+++ spec/services/academic_concentration_service_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe Hyrax::AcademicConcentrationService do
   end
   let(:service) { described_class }
 
-  describe "#select_all_options" do
-    it "returns only active terms" do
-      expect(service.select_all_options).to include(['Aquatic and Atmospheric Sciences', 'Aquatic and Atmospheric Sciences'],
+  describe "#select_options" do
+    it "returns only active  masters papers terms for masters form" do
+      expect(service.select('masters')).to include(['Aquatic and Atmospheric Sciences', 'Aquatic and Atmospheric Sciences'],
                                                     ['Clinical Nutrition', 'Clinical Nutrition'],
                                                     ['Economic Development', 'Economic Development'],
                                                     ['Environmental Chemistry and Biology', 'Environmental Chemistry and Biology'],
@@ -24,6 +24,40 @@ RSpec.describe Hyrax::AcademicConcentrationService do
                                                     ['Sustainable Water Resources', 'Sustainable Water Resources'],
                                                     ['Transportation Planning', 'Transportation Planning'],
                                                     ['Water Resources Engineering', 'Water Resources Engineering'])
+    end
+
+    it "returns only honors thesis active terms for honors form" do
+      expect(service.select('honors')).to include(['Anthropology', 'Anthropology'],
+                                                    ['Chemistry', 'Chemistry'],
+                                                    ['Biostatistics', 'Biostatistics'],
+                                                    ['Applied Science', 'Applied Science'],
+                                                    ['Business Administration', 'Business Administration'],
+                                                    ['Communication Studies', 'Communication Studies'],
+                                                    ['Comparative Literature', 'Comparative Literature'])
+    end
+
+    it "returns all terms for general form" do
+      expect(service.select('all')).to include(['Aquatic and Atmospheric Sciences', 'Aquatic and Atmospheric Sciences'],
+                                                   ['Clinical Nutrition', 'Clinical Nutrition'],
+                                                   ['Economic Development', 'Economic Development'],
+                                                   ['Environmental Chemistry and Biology', 'Environmental Chemistry and Biology'],
+                                                   ['Environmental Engineering', 'Environmental Engineering'],
+                                                   ['Environmental Health Sciences', 'Environmental Health Sciences'],
+                                                   ['Environmental Management and Policy', 'Environmental Management and Policy'],
+                                                   ['Housing and Community Development', 'Housing and Community Development'],
+                                                   ['Industrial Hygiene', 'Industrial Hygiene'],
+                                                   ['Land Use and Environmental Planning', 'Land Use and Environmental Planning'],
+                                                   ['Public Health Nutrition', 'Public Health Nutrition'],
+                                                   ['Sustainable Water Resources', 'Sustainable Water Resources'],
+                                                   ['Transportation Planning', 'Transportation Planning'],
+                                                   ['Water Resources Engineering', 'Water Resources Engineering'],
+                                                   ['Anthropology', 'Anthropology'],
+                                                   ['Chemistry', 'Chemistry'],
+                                                   ['Biostatistics', 'Biostatistics'],
+                                                   ['Applied Science', 'Applied Science'],
+                                                   ['Business Administration', 'Business Administration'],
+                                                   ['Communication Studies', 'Communication Studies'],
+                                                   ['Comparative Literature', 'Comparative Literature'])
     end
   end
 
