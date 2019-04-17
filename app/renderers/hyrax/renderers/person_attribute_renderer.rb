472d316f584bc7486ddcbdf0f1e3132007cf014f
Jennifer Smith <smithj15@email.unc.edu>
2019-04-17 16:06:02 -0400

    Add microdata to people object displays

--- app/renderers/hyrax/renderers/person_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/person_attribute_renderer.rb
@@ -4,10 +4,11 @@ module Hyrax
       private
 
       def attribute_value_to_html(value)
+
         begin
           person = value.split('||')
           display_text = ''
-          display_text << "<li>#{person[0]}"
+          display_text << "<li><span#{html_attributes(microdata_value_attributes(field.to_s.chomp('_display')))}>#{person[0]}</span>"
           if person.length > 1
             display_text << '<ul>'
             person[1..-1].each do |attr|
