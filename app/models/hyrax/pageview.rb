fe30fa0bf957c7282a1114b9cc713622c269a10f
Jennifer Smith <smithj15@email.unc.edu>
2019-06-10 11:44:23 -0400

    GA filter method should not be private

--- app/models/hyrax/pageview.rb
+++ app/models/hyrax/pageview.rb
@@ -6,23 +6,21 @@ module Hyrax
     dimensions :date
     filter :for_path, &->(path) { contains(:pagePath, redirect(path)) }
 
-    private
-
-      def redirect(path)
-        if ENV.has_key?('REDIRECT_FILE_PATH') && File.exist?(ENV['REDIRECT_FILE_PATH'])
-          redirect_uuids = File.read(ENV['REDIRECT_FILE_PATH'])
-        else
-          redirect_uuids = File.read(Rails.root.join('lib', 'redirects', 'redirect_uuids.csv'))
-        end
-
-        csv = CSV.parse(redirect_uuids, headers: true)
-        redirect_path = csv.find { |row| row['new_path'].match(path) }
-
-        if redirect_path
-          "#{path}|/record/uuid:#{redirect_path['uuid']}"
-        else
-          path
-        end
+    def redirect(path)
+      if ENV.has_key?('REDIRECT_FILE_PATH') && File.exist?(ENV['REDIRECT_FILE_PATH'])
+        redirect_uuids = File.read(ENV['REDIRECT_FILE_PATH'])
+      else
+        redirect_uuids = File.read(Rails.root.join('lib', 'redirects', 'redirect_uuids.csv'))
       end
+
+      csv = CSV.parse(redirect_uuids, headers: true)
+      redirect_path = csv.find { |row| row['new_path'].match(path) }
+
+      if redirect_path
+        "#{path}|/record/uuid:#{redirect_path['uuid']}"
+      else
+        path
+      end
+    end
   end
 end
