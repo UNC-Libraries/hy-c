3aea17c42e15918727c8f3d2cc7efde6720d7214
lfarrell <lfarrell@email.unc.edu>
2019-07-11 08:30:19 -0400

    Reorder facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -50,23 +50,16 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-
-    config.add_facet_field solr_name("advisor_label", :facetable), label: "Advisor", limit: 5
-    config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collection'
+    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
     config.add_facet_field solr_name("creator_label", :facetable), label: "Creator", limit: 5
+    config.add_facet_field solr_name("affiliation_label", :facetable), label: "Departments", limit: 5
     config.add_facet_field "date_issued_isim", label: "Date", limit: 5, range: true
     config.add_facet_field solr_name("keyword", :facetable), limit: 5
-    config.add_facet_field solr_name("language", :facetable), helper_method: :language_links_facets, limit: 5
-    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
     config.add_facet_field solr_name("subject", :facetable), limit: 5
-    config.add_facet_field solr_name("file_format", :facetable), limit: 5
-    config.add_facet_field solr_name("depositor", :facetable), limit: 5
-
-    # UNC Custom
-    config.add_facet_field solr_name("affiliation_label", :facetable), label: "Departments", limit: 5
+    config.add_facet_field solr_name("advisor_label", :facetable), label: "Advisor", limit: 5
     config.add_facet_field solr_name("edition", :facetable), label: "Version", limit: 5
-    config.add_facet_field solr_name("language_label", :facetable), label: "Language", limit: 5
-
+    config.add_facet_field solr_name("language", :facetable), helper_method: :language_links_facets, limit: 5
+    config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collection'
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
