60b22a695e3f784e94d3b06357c032f460cd7f23
lfarrell <lfarrell@email.unc.edu>
2019-07-12 08:49:53 -0400

    Fix language field for adv search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -271,7 +271,7 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('language') do |field|
-      solr_name = solr_name("language", :stored_searchable)
+      solr_name = solr_name("language_label", :stored_searchable)
       field.solr_local_parameters = {
         qf: solr_name,
         pf: solr_name
