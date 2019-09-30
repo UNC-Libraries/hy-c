7d68f56b91c2c9900ba0c1e599a7b2d1e1d319ed
Jennifer Smith <smithj15@email.unc.edu>
2019-09-30 14:32:37 -0400

    Add test for masters paper controller

--- spec/controllers/masters_papers_controller_spec.rb
+++ spec/controllers/masters_papers_controller_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe MastersPapersController, type: :controller do
     end
   end
 
+  describe 'POST #select_department' do
+    let(:user) do
+      User.new(email: 'test@example.com', guest: false, uid: 'test') { |u| u.save!(validate: false)}
+    end
+
+    let(:masters_paper_params) { {masters_paper: {affiliation: 'Physician Assistant Program'}} }
+
+    before do
+      sign_in user
+    end
+
+    it 'redirects to the form for creating a new masters paper' do
+      post :select_department, params: masters_paper_params
+      expect(response).to redirect_to '/concern/masters_papers/new?affiliation=Physician+Assistant+Program&locale=en'
+    end
+  end
 end
