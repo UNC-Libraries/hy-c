8b67b7164d745c3012acb1169046298e4b2498d3
Jennifer Smith <smithj15@email.unc.edu>
2019-10-03 13:46:30 -0400

    Do not show console output during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,6 +57,19 @@ RSpec.configure do |config|
     # Noid minting causes extra LDP requests which slow the test suite.
     Hyrax.config.enable_noids = false
   end
+
+  # disable writing to console during tests
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
 
 # The settings below are suggested to provide a good initial experience
