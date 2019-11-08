16c52c92a234abef5462cff139438767e22c99e0
lfarrell <lfarrell@email.unc.edu>
2019-11-08 09:18:10 -0500

    Turn ORCID urls into links

--- app/renderers/hyrax/renderers/person_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/person_attribute_renderer.rb
@@ -1,6 +1,16 @@
 module Hyrax
   module Renderers
     class PersonAttributeRenderer < AttributeRenderer
+      def format_attribute(text)
+        if text =~ /ORCID:.*?http/
+          text_pieces = text.split(' ')
+          url = text_pieces[1].strip
+          text = "#{text_pieces[0]} <a href='#{url}'>#{url}</a>"
+        end
+
+        text
+      end
+
       private
 
       def attribute_value_to_html(value)
@@ -12,7 +22,7 @@ module Hyrax
           if person.length > 1
             display_text << '<ul>'
             person[1..-1].each do |attr|
-              display_text << "<li>#{attr}</li>"
+              display_text << "<li>#{format_attribute(attr)}</li>"
             end
             display_text << '</ul>'
           end
