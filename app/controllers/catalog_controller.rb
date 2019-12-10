88c0d07d0c79bdd46f665b7f5cf41a0a02ca750a
lfarrell <lfarrell@email.unc.edu>
2019-12-10 10:40:51 -0500

    Update sort labels

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -360,8 +360,8 @@ class CatalogController < ApplicationController
     config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance"
     config.add_sort_field "#{title_field} asc", label: 'title [A-Z]'
     config.add_sort_field "#{title_field} desc", label: 'title [Z-A]'
-    config.add_sort_field "#{date_issued_field} desc", label: "date issued \u25BC"
-    config.add_sort_field "#{date_issued_field} asc", label: "date issued \u25B2"
+    config.add_sort_field "#{date_issued_field} desc", label: "date of publication \u25BC"
+    config.add_sort_field "#{date_issued_field} asc", label: "date of publication \u25B2"
     config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u25BC"
     config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u25B2"
     config.add_sort_field "#{modified_field} desc", label: "date modified \u25BC"
