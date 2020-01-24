ca36d8669efb763fdd172b3b4d74f8b0a4b1a76e
Jennifer Smith <smithj15@email.unc.edu>
2020-01-24 14:21:33 -0500

    Override bulkrax files to allow custom metadata to be imported

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -1,3 +1,4 @@
+# [hyc-override] updating `transform_attributes` and `permitted_attributes` methods
 # frozen_string_literal: true
 
 # TODO: require 'importer/log_subscriber'
@@ -209,9 +210,28 @@ module Bulkrax
 
     # Override if we need to map the attributes from the parser in
     # a way that is compatible with how the factory needs them.
+    # override to fix enumeration and formatting of people objects
     def transform_attributes
-      attributes.slice(*permitted_attributes)
-          .merge(file_attributes)
+      attrs = attributes.slice(*permitted_attributes)
+                  .merge(file_attributes)
+      resource = @klass.new
+      attrs.each do |k,v|
+        if resource.attributes.keys.member?(k.to_s) && !resource.attributes[k.to_s].respond_to?(:each) && attrs[k].respond_to?(:each)
+          attrs[k] = v.first
+        elsif resource.attributes.keys.member?(k.to_s) && resource.attributes[k.to_s].respond_to?(:each) && !attrs[k].respond_to?(:each)
+          attrs[k] = Array(v)
+        else
+          attrs[k] = v
+        end
+      end
+
+      attrs.each do |k,v|
+        if k.ends_with? '_attributes'
+          attrs[k] = JSON.parse(v.gsub('=>',':').gsub("'",'"'))
+        end
+      end
+
+      attrs
     end
 
     # Find existing files or upload new files. This assumes a Work will have unique file titles;
@@ -307,8 +327,13 @@ module Bulkrax
     end
 
     # Regardless of what the Parser gives us, these are the properties we are prepared to accept.
+    # override to allow '_attributes' properties for people obejcts
     def permitted_attributes
-      klass.properties.keys.map(&:to_sym) + %i[id edit_users edit_groups read_groups visibility work_members_attributes]
+      people_types = [:advisors, :arrangers, :composers, :contributors, :creators, :project_directors, :researchers,
+                      :reviewers, :translators]
+      properties = klass.properties.keys.map(&:to_sym)
+      people = properties.map{|p| people_types.include?(p) ? p : nil}.compact
+      properties + people.map{|p| "#{p}_attributes".to_sym} + %i[id edit_users edit_groups read_groups visibility work_members_attributes]
     end
   end
 end
