f634fb2f87fe639df3d1b4ca00963f9d53b5da8d
Jennifer Smith <smithj15@email.unc.edu>
2020-01-29 12:35:39 -0500

    Add bulkrax files for overrides

---
+++ app/models/bulkrax/csv_entry.rb
@@ -0,0 +1,130 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Bulkrax
+  class CsvEntry < Entry
+    serialize :raw_metadata, JSON
+
+    def self.fields_from_data(data)
+      data.headers.flatten.compact.uniq
+    end
+
+    # there's a risk that this reads the whole file into memory and could cause a memory leak
+    def self.read_data(path)
+      CSV.read(path,
+               headers: true,
+               header_converters: :symbol,
+               encoding: 'utf-8')
+    end
+
+    def self.data_for_entry(data, _path = nil)
+      # If the whole CSV data is passed, grab the first row
+      data = data.first if data.is_a?(CSV::Table)
+      raw_data = data.to_h
+      # If the collection field mapping is not 'collection', add 'collection' - the parser needs it
+      raw_data[:collection] = raw_data[collection_field.to_sym] if raw_data.keys.include?(collection_field.to_sym) && collection_field != 'collection'
+      # If the children field mapping is not 'children', add 'children' - the parser needs it
+      raw_data[:children] = raw_data[collection_field.to_sym] if raw_data.keys.include?(children_field.to_sym) && children_field != 'children'
+      return raw_data
+    end
+
+    def self.collection_field
+      Bulkrax.collection_field_mapping[self.class.to_s] || 'collection'
+    end
+
+    def self.children_field
+      Bulkrax.parent_child_field_mapping[self.to_s] || 'children'
+    end
+
+    def build_metadata
+      raise StandardError, 'Record not found' if record.nil?
+
+      # rubocop:disable Style/IfUnlessModifier
+      if importerexporter.parser.required_elements?(record.keys) == false
+        raise StandardError, "Missing required elements, required elements are: #{importerexporter.parser.required_elements.join(', ')}"
+      end
+      # rubocop:enable Style/IfUnlessModifier
+
+      self.parsed_metadata = {}
+      self.parsed_metadata[Bulkrax.system_identifier_field] = [record['source_identifier']]
+
+      record.each do |key, value|
+        next if key == 'collection'
+        add_metadata(key, value)
+      end
+
+      # construct full file path
+      self.parsed_metadata['file'] = record['file'].split(/\s*[:;|]\s*/).map { |f| file_path(f.tr(' ', '_')) } if record['file'].present?
+
+      add_visibility
+      add_rights_statement
+      add_collections
+      add_local
+
+      self.parsed_metadata
+    end
+
+    def build_export_metadata
+      self.parsed_metadata = {}
+      self.parsed_metadata['source_identifier'] = work.id
+      self.parsed_metadata['model'] = work.has_model.first
+      mapping.each do |key, value|
+        next if Bulkrax.reserved_properties.include?(key) && !field_supported?(key)
+        next unless work.respond_to?(key)
+        data = work.send(key)
+        if data.is_a?(ActiveTriples::Relation)
+          self.parsed_metadata[key] = data.join('; ').to_s unless value[:excluded]
+        else
+          self.parsed_metadata[key] = data
+        end
+      end
+      unless work.is_a?(Collection)
+        self.parsed_metadata['file'] = work.file_sets.map { |fs| "#{work.id}/#{filename(fs)}" unless filename(fs).blank? }.compact.join('; ')
+      end
+      self.parsed_metadata
+    end
+
+    def record
+      @record ||= raw_metadata
+    end
+
+    def self.matcher_class
+      Bulkrax::CsvMatcher
+    end
+
+    def collections_created?
+      return true if record[self.class.collection_field].blank?
+      record[self.class.collection_field].split(/\s*[:;|]\s*/).length == self.collection_ids.length
+    end
+
+    def find_or_create_collection_ids
+      return self.collection_ids if collections_created?
+      valid_system_id(Collection)
+      unless record[self.class.collection_field].blank?
+        record[self.class.collection_field].split(/\s*[:;|]\s*/).each do |collection|
+          c = find_collection(collection)
+          self.collection_ids << c.id unless c.blank? || self.collection_ids.include?(c.id)
+        end
+      end
+      return self.collection_ids
+    end
+
+    def required_elements?(keys)
+      !required_elements.map { |el| keys.include?(el) }.include?(false)
+    end
+
+    def required_elements
+      %w[title source_identifier]
+    end
+
+    def file_path(file)
+      # return if we already have the full file path
+      return file if File.exist?(file)
+      path = importerexporter.parser.files_path
+      f = File.join(path, file)
+      return f if File.exist?(f)
+      raise "File #{f} does not exist"
+    end
+  end
+end
---
+++ app/parsers/bulkrax/csv_parser.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+require 'csv'
+module Bulkrax
+  class CsvParser < ApplicationParser
+    def self.export_supported?
+      true
+    end
+
+    def initialize(importerexporter)
+      @importerexporter = importerexporter
+    end
+
+    def collections
+      # does the CSV contain a collection column?
+      return [] unless import_fields.include?(:collection)
+      # retrieve a list of unique collections
+      records.map { |r| r[:collection].split(/\s*[;|]\s*/) unless r[:collection].blank? }.flatten.compact.uniq
+    end
+
+    def collections_total
+      collections.size
+    end
+
+    def records(_opts = {})
+      @records ||= entry_class.read_data(parser_fields['import_file_path']).map { |record_data| entry_class.data_for_entry(record_data) }
+    end
+
+    # We could use CsvEntry#fields_from_data(data) but that would mean re-reading the data
+    def import_fields
+      @import_fields ||= records.inject(:merge).keys.compact.uniq
+    end
+
+    def required_elements?(keys)
+      return if keys.blank?
+      !required_elements.map { |el| keys.map(&:to_s).include?(el) }.include?(false)
+    end
+
+    def required_elements
+      %w[title source_identifier]
+    end
+
+    def valid_import?
+      required_elements?(import_fields) && file_paths.is_a?(Array)
+    rescue StandardError => e
+      errors.add(:base, e.class.to_s.to_sym, message: e.message)
+      return false
+    end
+
+    def create_collections
+      collections.each_with_index do |collection, index|
+        next if collection.blank?
+        metadata = {
+            title: [collection],
+            Bulkrax.system_identifier_field => [collection],
+            visibility: 'open',
+            collection_type_gid: Hyrax::CollectionType.find_or_create_default_collection_type.gid
+        }
+        new_entry = find_or_create_entry(collection_entry_class, collection, 'Bulkrax::Importer', metadata)
+        ImportWorkCollectionJob.perform_now(new_entry.id, current_importer_run.id)
+        increment_counters(index, true)
+      end
+    end
+
+    def create_works
+      records.each_with_index do |record, index|
+        next if record[:source_identifier].blank?
+        break if !limit.nil? && index >= limit
+
+        seen[record[:source_identifier]] = true
+        new_entry = find_or_create_entry(entry_class, record[:source_identifier], 'Bulkrax::Importer', record.to_h.compact)
+        ImportWorkJob.perform_later(new_entry.id, current_importer_run.id)
+        increment_counters(index)
+      end
+    rescue StandardError => e
+      errors.add(:base, e.class.to_s.to_sym, message: e.message)
+    end
+
+    def create_parent_child_relationships
+      super
+    end
+
+    def create_from_importer
+      Bulkrax::Importer.find(importerexporter.export_source).entries.each do |entry|
+        query = "#{ActiveFedora.index_field_mapper.solr_name(Bulkrax.system_identifier_field)}:\"#{entry.identifier}\""
+        work_id = ActiveFedora::SolrService.query(query, fl: 'id', rows: 1).first['id']
+        new_entry = find_or_create_entry(entry_class, work_id, 'Bulkrax::Exporter')
+        Bulkrax::ExportWorkJob.perform_now(new_entry.id, current_exporter_run.id)
+      end
+    end
+
+    def create_from_collection
+      work_ids = ActiveFedora::SolrService.query("member_of_collection_ids_ssim:#{importerexporter.export_source}").map(&:id)
+      work_ids.each do |wid|
+        new_entry = find_or_create_entry(entry_class, wid, 'Bulkrax::Exporter')
+        Bulkrax::ExportWorkJob.perform_now(new_entry.id, current_exporter_run.id)
+      end
+    end
+
+    def entry_class
+      CsvEntry
+    end
+
+    def collection_entry_class
+      CsvCollectionEntry
+    end
+
+    # See https://stackoverflow.com/questions/2650517/count-the-number-of-lines-in-a-file-without-reading-entire-file-into-memory
+    #   Changed to grep as wc -l counts blank lines, and ignores the final unescaped line (which may or may not contain data)
+    def total
+      if importer?
+        # @total ||= `wc -l #{parser_fields['import_file_path']}`.to_i - 1
+        @total ||= `grep -vc ^$ #{parser_fields['import_file_path']}`.to_i - 1
+      elsif exporter?
+        @total ||= importerexporter.entries.count
+      else
+        @total = 0
+      end
+    rescue StandardError
+      @total = 0
+    end
+
+    # @todo - investigate getting directory structure
+    # @todo - investigate using perform_later, and having the importer check for
+    #   DownloadCloudFileJob before it starts
+    def retrieve_cloud_files(files)
+      files_path = File.join(path_for_import, 'files')
+      FileUtils.mkdir_p(files_path) unless File.exist?(files_path)
+      files.each_pair do |_key, file|
+        # this only works for uniquely named files
+        target_file = File.join(files_path, file['file_name'].tr(' ', '_'))
+        # Now because we want the files in place before the importer runs
+        # Problematic for a large upload
+        Bulkrax::DownloadCloudFileJob.perform_now(file, target_file)
+      end
+      return nil
+    end
+
+    # export methods
+
+    def write_files
+      file = setup_export_file
+      file.puts(export_headers)
+      importerexporter.entries.each do |e|
+        file.puts(e.parsed_metadata.values.to_csv)
+      end
+      file.close
+    end
+
+    def export_headers
+      headers = ['id']
+      headers << ['model']
+      importerexporter.mapping.keys.each { |key| headers << key unless Bulkrax.reserved_properties.include?(key) && !field_supported?(key) }.sort
+      headers << 'file'
+      headers.to_csv
+    end
+
+    # in the parser as it is specific to the format
+    def setup_export_file
+      File.open(File.join(importerexporter.exporter_export_path, 'export.csv'), 'w')
+    end
+
+    def file_paths
+      @file_paths ||= records.map do |r|
+        next unless r[:file].present?
+        r[:file].split(/\s*[:;|]\s*/).map do |f|
+          file = File.join(files_path, f.tr(' ', '_'))
+          if File.exist?(file)
+            file
+          else
+            raise "File #{file} does not exist"
+          end
+        end
+      end.flatten.compact.uniq
+    end
+
+    def files_path
+      path = self.importerexporter.parser_fields['import_file_path'].split('/')
+      # remove the metadata filename from the end of the import path
+      path.pop
+      File.join(path.join('/'), 'files')
+    end
+  end
+end
