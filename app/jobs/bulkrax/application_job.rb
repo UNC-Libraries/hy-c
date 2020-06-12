3c9e4e93cea683f2792976f73279f7b17c621004
Jennifer Smith <smithj15@email.unc.edu>
2020-06-12 17:18:52 -0400

    Allow bulkrax jobs to be run by sidekiq

--- app/jobs/bulkrax/application_job.rb
+++ app/jobs/bulkrax/application_job.rb
@@ -1,6 +1,8 @@
+# Override to allow bulkrax jobs to be available in sidekiq status ui
 # frozen_string_literal: true
 
 module Bulkrax
   class ApplicationJob < ActiveJob::Base
+    include Sidekiq::Status::Worker
   end
 end
