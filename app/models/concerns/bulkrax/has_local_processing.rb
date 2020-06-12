f78d4b386329d5e30d9c8a2f37a475f206a0e8df
Jennifer Smith <smithj15@email.unc.edu>
2020-06-12 17:31:24 -0400

    Add admin set and rights statement label to parsed metadata for bulkrax imports

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -4,5 +4,14 @@ module Bulkrax::HasLocalProcessing
   # This method is called during build_metadata
   # add any special processing here, for example to reset a metadata property
   # to add a custom property from outside of the import data
-  def add_local; end
+  def add_local
+    # add rights statement label
+    if self.parsed_metadata.key?('rights_statement') && !self.parsed_metadata['rights_statement'].blank? &&
+        !self.parsed_metadata.key?('rights_statement_label')
+      self.parsed_metadata['rights_statement_label'] = CdrRightsStatementsService.label(self.parsed_metadata['rights_statement'].first)
+    end
+
+    # use admin set chosen in form if not already set
+    self.parsed_metadata['admin_set_id'] ||= importerexporter.admin_set_id
+  end
 end
