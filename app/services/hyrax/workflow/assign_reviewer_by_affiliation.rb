f55a28e8451b0e3680710ef17711db04ae7c9384
lfarrell <lfarrell@email.unc.edu>
2021-05-24 11:00:58 -0400

    Replace commas in role group names with underscores

--- app/services/hyrax/workflow/assign_reviewer_by_affiliation.rb
+++ app/services/hyrax/workflow/assign_reviewer_by_affiliation.rb
@@ -3,8 +3,8 @@ module Hyrax::Workflow::AssignReviewerByAffiliation
   def self.call(target:, **)
     target.creators.each do |creator|
       creator['affiliation'].each do |affiliation|
-        # Replace spaces and dashes. PostGres doesn't seem to do well with dashes
-        department = affiliation.strip.to_s.downcase.gsub(/\s|–/, '_')
+        # Replace spaces, dashes and commas. PostGres doesn't seem to do well with dashes and commas
+        department = affiliation.strip.to_s.downcase.gsub(/\s|–|,/, '_')
         reviewer = find_reviewer_for(department: department)
         permission_template_id = Hyrax::PermissionTemplate.find_by_source_id(target.admin_set_id).id
 
