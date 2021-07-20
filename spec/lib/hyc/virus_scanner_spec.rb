ede8e15846392e059751afdaa6bb2cecbb4d5ec4
lfarrell <lfarrell@email.unc.edu>
2021-07-20 13:32:37 -0400

    Check if tests are running from github actions before setting file paths

--- spec/lib/hyc/virus_scanner_spec.rb
+++ spec/lib/hyc/virus_scanner_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Hyc::VirusScanner do
     let(:file) { src_path }
 
     before do
-      if ENV["CI"]
+      if Dir.pwd.include? 'runner'
         file = Tempfile.new.path
         FileUtils.rm(file)
         FileUtils.cp(src_path, file)
@@ -34,7 +34,7 @@ RSpec.describe Hyc::VirusScanner do
     let(:file) { src_path }
 
     before do
-      if ENV["CI"]
+      if Dir.pwd.include? 'runner'
         file = Tempfile.new.path
         FileUtils.rm(file)
         FileUtils.cp(src_path, file)
