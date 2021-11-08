52fb7244da810c994e0ae492edd7dcbd132a0ac6
Max Kadel <max@curationexperts.com>
2021-11-08 13:16:15 -0500

    Fix byebug in test context
    - Send deprecation warnings to log in test, so rewrite of stderr hopefully not needed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # Print deprecation notices to the Rails logger. (can also change to :stderr)
+  config.active_support.deprecation = :log
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
