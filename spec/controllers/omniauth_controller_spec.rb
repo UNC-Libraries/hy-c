aa64b48d516451380717778312582a484b0d2fc1
Max Kadel <max@curationexperts.com>
2021-11-12 16:15:55 -0500

    Ensure we have some string as the origin, even if empty
    Co-Authored-By: Ben Pennell <bbpennel@users.noreply.github.com>

--- spec/controllers/omniauth_controller_spec.rb
+++ spec/controllers/omniauth_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe OmniauthController, type: :request do
 
       it 'is successful' do
         get new_user_session_path
-        expect(response).to redirect_to "/Shibboleth.sso/Login?origin=&target=#{CGI.escape(user_shibboleth_omniauth_callback_path)}"
+        expect(response).to redirect_to "/Shibboleth.sso/Login?target=#{user_shibboleth_omniauth_callback_path}%26origin="
       end
     end
   end
