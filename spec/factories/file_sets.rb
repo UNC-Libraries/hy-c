c6832969da81f8a1072331fec7e25ab17b48d4f3
Max Kadel <mkadel@email.unc.edu>
2021-12-08 12:29:15 -0500

    Update test factories so we can create full works

---
+++ spec/factories/file_sets.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# from https://github.com/samvera/hyrax/blob/b034218b89dde7df534e32d1e5ade9161e129a1d/spec/factories/file_sets.rb#L35
+FactoryBot.define do
+  factory :file_set do
+    transient do
+      user { create(:user) }
+      content { nil }
+    end
+    after(:build) do |fs, evaluator|
+      fs.apply_depositor_metadata evaluator.user.user_key
+    end
+
+    after(:create) do |file, evaluator|
+      Hydra::Works::UploadFileToFileSet.call(file, evaluator.content) if evaluator.content
+    end
+
+    trait :public do
+      read_groups { ["public"] }
+    end
+
+    trait :registered do
+      read_groups { ["registered"] }
+    end
+
+    trait :image do
+      content { File.open(Hyrax::Engine.root + 'spec/fixtures/world.png') }
+    end
+
+    trait :with_original_file do
+      after(:create) do |file_set, _evaluator|
+        Hydra::Works::AddFileToFileSet
+          .call(file_set, File.open(Hyrax::Engine.root + 'spec/fixtures/world.png'), :original_file)
+      end
+    end
+
+    factory :file_with_work do
+      after(:build) do |file, _evaluator|
+        file.title = ['testfile']
+      end
+      after(:create) do |file, evaluator|
+        Hydra::Works::UploadFileToFileSet.call(file, evaluator.content) if evaluator.content
+        create(:work, user: evaluator.user).members << file
+      end
+    end
+  end
+end
