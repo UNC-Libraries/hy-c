d28f47700658fdac5f63c52b188796bf9b17a0bd
Max Kadel <mkadel@email.unc.edu>
2021-12-08 12:55:52 -0500

    Add version footer (#722)
    * Add version footer
    
    - Includes branch or release name, date deployed if in production environment, and title with git sha
    
    * Add Hyrax version to footer, link to Hy-C and Hyrax
    
    - Three sections for version footer
    
    * Adjust html and center all footer menus
    
    Co-authored-by: lfarrell <lfarrell@email.unc.edu>

---
+++ spec/features/footer_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.feature 'custom shared footer' do
+  before do
+    visit "/"
+  end
+
+  it "displays the version footer" do
+    expect(page).to have_css("#unc-version-footer")
+  end
+
+  it "has the Hyrax version from the gemfile" do
+    expect(page).to have_link("Hyrax version", href: "https://hyrax.samvera.org/")
+    expect(page).to have_link("Hy-C")
+  end
+
+  context "in the development environment" do
+    before do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+    end
+
+    it "displays that it's not in a deployed environment" do
+      expect(page).to have_content("Not in deployed environment")
+    end
+  end
+
+  context "in a deployed environment" do
+    before do
+      Rails.logger.debug("in test 'before' block, before 'allow': #{Rails.env}")
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+      Rails.logger.debug("in test 'before' block, after 'allow': #{Rails.env}")
+    end
+
+    it "displays data based on the directory it's in" do
+      pending("Cannot mock production environment in initializer")
+      Rails.logger.debug("in test 'it' block: #{Rails.env}")
+      expect(page).to have_content("Deployed some_date")
+    end
+  end
+end
