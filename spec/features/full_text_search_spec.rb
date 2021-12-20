d2b74906ff6d4803521bfcb2f095de727c9864e5
Max Kadel <mkadel@email.unc.edu>
2021-12-20 10:54:10 -0500

    Test and restore previous fix - test failing for right reason

--- spec/features/full_text_search_spec.rb
+++ spec/features/full_text_search_spec.rb
@@ -1,62 +1,42 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require Rails.root.join('spec/support/full_text.rb')
 include Warden::Test::Helpers
 
 RSpec.describe 'Search the catalog for full text', type: :feature, js: false do
-  before do
-    solr = Blacklight.default_index.connection
-    solr.add(
-      id: "1",
-      has_model_ssim: ['Article'],
-      title_tesim: ["My first title"],
-      visibility_ssi: "open",
-      read_access_group_ssim: ["public"],
-      all_text_timv: "purple monkey dishwasher"
-    )
-    solr.commit
-  end
-
-  it "can go to the advanced search page from the home page" do
-    visit root_path
-    click_link("Advanced search", match: :first)
-    expect(page).to have_content('Select "match all" to require all fields.')
-  end
-
-  it "can perform a regular search against full text" do
-    visit root_path
-    fill_in('q', with: 'monkey')
-    click_button("Go")
-    expect(page).to have_content("My first title")
-  end
-
-  it "can do an advanced search against full text" do
-    visit '/advanced'
-    fill_in('all_fields', with: "monkey")
-    click_button('Search')
-    expect(page).to have_content("My first title")
-  end
-
-  it "can return to the advanced search page after an advanced search" do
-    visit '/advanced'
-    fill_in('all_fields', with: "monkey")
-    click_button('Search')
-    expect(page).to have_content("My first title")
-    click_link("Advanced search", match: :first)
-    expect(page).to have_content('Select "match all" to require all fields.')
-  end
-
-  context "as a logged in user" do
-    let(:user) { FactoryBot.create(:user) }
+  context "with full text on the solr object" do
     before do
-      login_as user
+      solr = Blacklight.default_index.connection
+      solr.add([FULL_TEXT_WORK, FULL_TEXT_FILE_SET])
+      solr.commit
     end
 
-    it "can do an advanced search and return to the advanced search page" do
+    it "can go to the advanced search page from the home page" do
+      visit root_path
+      click_link("Advanced search", match: :first)
+      expect(page).to have_content('Select "match all" to require all fields.')
+    end
+
+    it "can perform a regular search against full text" do
+      visit root_path
+      fill_in('q', with: 'metalloprotease')
+      click_button("Go")
+      expect(page).to have_content("Full text search testing")
+    end
+
+    it "can do an advanced search against full text" do
       visit '/advanced'
-      fill_in('all_fields', with: "monkey")
+      fill_in('all_fields', with: "metalloprotease")
       click_button('Search')
-      expect(page).to have_content("My first title")
+      expect(page).to have_content("Full text search testing")
+    end
+
+    it "can return to the advanced search page after an advanced search" do
+      visit '/advanced'
+      fill_in('all_fields', with: "metalloprotease")
+      click_button('Search')
+      expect(page).to have_content("Full text search testing")
       click_link("Advanced search", match: :first)
       expect(page).to have_content('Select "match all" to require all fields.')
     end
