04fe28e60bcd948f326feca51bb16549047dc3f8
Max Kadel <mkadel@email.unc.edu>
2021-12-20 13:12:41 -0500

    Dry up tests, delete fixtures from solr

--- spec/features/full_text_search_spec.rb
+++ spec/features/full_text_search_spec.rb
@@ -5,40 +5,41 @@ require Rails.root.join('spec/support/full_text.rb')
 include Warden::Test::Helpers
 
 RSpec.describe 'Search the catalog for full text', type: :feature, js: false do
-  context "with full text on the solr object" do
-    before do
-      solr = Blacklight.default_index.connection
-      solr.add([FULL_TEXT_WORK, FULL_TEXT_FILE_SET])
-      solr.commit
-    end
+  let(:solr) { Blacklight.default_index.connection }
+  let(:query_term) { 'metalloprotease' }
+  let(:target_title) { 'Full text search testing' }
 
-    it "can go to the advanced search page from the home page" do
-      visit root_path
-      click_link("Advanced search", match: :first)
-      expect(page).to have_content('Select "match all" to require all fields.')
-    end
+  before do
+    solr.add([FULL_TEXT_WORK, FULL_TEXT_FILE_SET])
+    solr.commit
+  end
 
-    it "can perform a regular search against full text" do
-      visit root_path
-      fill_in('q', with: 'metalloprotease')
-      click_button("Go")
-      expect(page).to have_content("Full text search testing")
-    end
+  after do
+    solr.delete_by_query("id:#{FULL_TEXT_WORK[:id]}")
+    solr.delete_by_query("id:#{FULL_TEXT_FILE_SET[:id]}")
+    solr.commit
+  end
 
-    it "can do an advanced search against full text" do
-      visit '/advanced'
-      fill_in('all_fields', with: "metalloprotease")
-      click_button('Search')
-      expect(page).to have_content("Full text search testing")
-    end
+  it "can perform a regular search against full text" do
+    visit root_path
+    fill_in('q', with: query_term)
+    click_button("Go")
+    expect(page).to have_content(target_title)
+  end
 
-    it "can return to the advanced search page after an advanced search" do
-      visit '/advanced'
-      fill_in('all_fields', with: "metalloprotease")
-      click_button('Search')
-      expect(page).to have_content("Full text search testing")
-      click_link("Advanced search", match: :first)
-      expect(page).to have_content('Select "match all" to require all fields.')
-    end
+  it "can do an advanced search against full text" do
+    visit '/advanced'
+    fill_in('all_fields', with: query_term)
+    click_button('Search')
+    expect(page).to have_content(target_title)
+  end
+
+  it "can return to the advanced search page after an advanced search" do
+    visit '/advanced'
+    fill_in('all_fields', with: query_term)
+    click_button('Search')
+    expect(page).to have_content(target_title)
+    click_link("Advanced search", match: :first)
+    expect(page).to have_content('Select "match all" to require all fields.')
   end
 end
