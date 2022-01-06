4ad7a7aef1ef5b5c5a06f04a0336239932470170
Max Kadel <mkadel@email.unc.edu>
2022-01-06 17:40:17 -0500

    Normalize whitespace - closer to Capybara 2 behavior

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'webmock/rspec'
+
 WebMock.disable_net_connect!(allow_localhost: true, allow: ['fcrepo:8080', 'solr:8983', 'opaquenamespace.org'])
+
+Capybara.default_normalize_ws = true
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
