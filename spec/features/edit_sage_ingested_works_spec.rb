1308e0bb4a651cf6bf6e1cbceb769e23f0810e24
Max Kadel <mkadel@email.unc.edu>
2022-01-26 11:27:02 -0500

    Test trying to edit as a regular user

--- spec/features/edit_sage_ingested_works_spec.rb
+++ spec/features/edit_sage_ingested_works_spec.rb
@@ -43,71 +43,82 @@ RSpec.feature 'Edit works created through the Sage ingest', :sage, js: false do
     ingest_service.process_packages
   end
 
-  it 'can open the edit page' do
-    login_as admin
-    visit "concern/articles/#{first_work_id}"
-    expect(page).to have_content('Inequalities in Cervical Cancer Screening Uptake Between')
-    expect(page).to have_content('Smith, Jennifer S.')
-    expect(page).to have_content('sage admin set')
-    expect(page).to have_content('Attribution-NonCommercial 4.0 International')
-    expect(page).to have_content('February 1, 2021')
-    expect(page).to have_content('In Copyright')
-    click_link('Edit', match: :first)
-    expect(page).to have_link('Work Deposit Form')
+  context 'as a regular user' do
+    let(:user) { FactoryBot.create(:user) }
+    it 'gives an unauthorized message' do
+      login_as user
+      visit "concern/articles/#{first_work_id}/edit"
+      expect(page).to have_content('Unauthorized')
+    end
   end
 
-  # Creator 2 only visible with Javascript on
-  it 'can render the pre-populated edit page', js: true do
-    login_as admin
-    visit "concern/articles/#{first_work_id}/edit"
-    # These values are also tested in the spec/services/tasks/sage_ingest_service_spec.rb
-    # form order
-    expect(page).to have_field('Title', with: 'Inequalities in Cervical Cancer Screening Uptake Between Chinese Migrant Women and Local Women: A Cross-Sectional Study')
-    expect(page).to have_field('Creator #1', with: 'Holt, Hunter K.')
+  context 'as an admin user' do
+    it 'can open the edit page' do
+      login_as admin
+      visit "concern/articles/#{first_work_id}"
+      expect(page).to have_content('Inequalities in Cervical Cancer Screening Uptake Between')
+      expect(page).to have_content('Smith, Jennifer S.')
+      expect(page).to have_content('sage admin set')
+      expect(page).to have_content('Attribution-NonCommercial 4.0 International')
+      expect(page).to have_content('February 1, 2021')
+      expect(page).to have_content('In Copyright')
+      click_link('Edit', match: :first)
+      expect(page).to have_link('Work Deposit Form')
+    end
+
     # Creator 2 only visible with Javascript on
-    expect(page).to have_field('Creator #2', with: 'Zhang, Xi')
-    expect(page).to have_field('Additional affiliation (Creator #1)', with: 'Department of Family and Community Medicine, University of California, San Francisco, CA, USA')
-    expect(page).to have_field('ORCID (Creator #1)', with: 'https://orcid.org/0000-0001-6833-8372')
-    expect(page).to have_field('Abstract', with: /Efforts to increase education opportunities, provide insurance/)
-    click_link('Optional fields')
-    # alpha order below
-    expect(page).to have_field('Copyright date', with: '2021')
-    expect(page).to have_field('Date of publication', with: 'February 1, 2021') # aka date_issued
-    expect(page).to have_select('Dcmi type', with_selected: 'Text')
-    expect(page).to have_field('Funder', with: 'Fogarty International Center')
-    expect(page).to have_field('Identifier', with: 'https://doi.org/10.1177/1073274820985792')
-    expect(page).to have_field('ISSN', with: '1073-2748')
-    expect(page).to have_field('Journal issue', with: '')
-    expect(page).to have_field('Journal title', with: 'Cancer Control')
-    expect(page).to have_field('Journal volume', with: '28')
-    # keywords
-    expected_keywords = ['HPV', 'HPV knowledge and awareness', 'cervical cancer screening', 'migrant women', 'China', '']
-    keyword_fields = page.all(:xpath, '/html/body/div[2]/div[2]/form/div/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/ul/li/input')
-    keywords = keyword_fields.map(&:value)
-    expect(keyword_fields.count).to eq 6
-    expect(keywords).to match_array(expected_keywords)
-    expect(page).to have_select('License', with_selected: 'Attribution-NonCommercial 4.0 International')
-    expect(page).to have_field('Publisher', with: 'SAGE Publications')
-    expect(page).to have_select('Resource type', with_selected: 'Article')
-    expect(page).to have_field('Rights holder', with: /SAGE Publications Inc, unless otherwise noted. Manuscript/)
-    expect(page).to have_select('Rights statement', with_selected: 'In Copyright')
-    expect(page).to have_checked_field('Public')
-  end
+    it 'can render the pre-populated edit page', js: true do
+      login_as admin
+      visit "concern/articles/#{first_work_id}/edit"
+      # These values are also tested in the spec/services/tasks/sage_ingest_service_spec.rb
+      # form order
+      expect(page).to have_field('Title', with: 'Inequalities in Cervical Cancer Screening Uptake Between Chinese Migrant Women and Local Women: A Cross-Sectional Study')
+      expect(page).to have_field('Creator #1', with: 'Holt, Hunter K.')
+      # Creator 2 only visible with Javascript on
+      expect(page).to have_field('Creator #2', with: 'Zhang, Xi')
+      expect(page).to have_field('Additional affiliation (Creator #1)', with: 'Department of Family and Community Medicine, University of California, San Francisco, CA, USA')
+      expect(page).to have_field('ORCID (Creator #1)', with: 'https://orcid.org/0000-0001-6833-8372')
+      expect(page).to have_field('Abstract', with: /Efforts to increase education opportunities, provide insurance/)
+      click_link('Optional fields')
+      # alpha order below
+      expect(page).to have_field('Copyright date', with: '2021')
+      expect(page).to have_field('Date of publication', with: 'February 1, 2021') # aka date_issued
+      expect(page).to have_select('Dcmi type', with_selected: 'Text')
+      expect(page).to have_field('Funder', with: 'Fogarty International Center')
+      expect(page).to have_field('Identifier', with: 'https://doi.org/10.1177/1073274820985792')
+      expect(page).to have_field('ISSN', with: '1073-2748')
+      expect(page).to have_field('Journal issue', with: '')
+      expect(page).to have_field('Journal title', with: 'Cancer Control')
+      expect(page).to have_field('Journal volume', with: '28')
+      # keywords
+      expected_keywords = ['HPV', 'HPV knowledge and awareness', 'cervical cancer screening', 'migrant women', 'China', '']
+      keyword_fields = page.all(:xpath, '/html/body/div[2]/div[2]/form/div/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/ul/li/input')
+      keywords = keyword_fields.map(&:value)
+      expect(keyword_fields.count).to eq 6
+      expect(keywords).to match_array(expected_keywords)
+      expect(page).to have_select('License', with_selected: 'Attribution-NonCommercial 4.0 International')
+      expect(page).to have_field('Publisher', with: 'SAGE Publications')
+      expect(page).to have_select('Resource type', with_selected: 'Article')
+      expect(page).to have_field('Rights holder', with: /SAGE Publications Inc, unless otherwise noted. Manuscript/)
+      expect(page).to have_select('Rights statement', with_selected: 'In Copyright')
+      expect(page).to have_checked_field('Public')
+    end
 
-  it 'can render values only present on the second work' do
-    login_as admin
-    visit "concern/articles/#{third_work_id}/edit"
-    expect(page).to have_field('Title', with: /The Prevalence of Bacterial Infection in Patients Undergoing/)
-    # date that includes only month and year on the edit page
-    expect(page).to have_field('Date of publication', with: 'January 2021') # aka date_issued
-    expect(page).to have_field('Journal issue', with: '1')
-    expect(page).to have_field('Journal volume', with: '11')
-    keyword_fields = page.all(:xpath, '/html/body/div[2]/div[2]/form/div/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/ul/li/input')
-    keywords = keyword_fields.map(&:value)
-    expect(keyword_fields.count).to eq 8
-    expect(keywords).to include('Propionibacterium acnes')
-    expect(page).to have_select('License', with_selected: 'Attribution-NonCommercial-NoDerivatives 4.0 International')
-    expect(page).to have_field('Page end', with: '20')
-    expect(page).to have_field('Page start', with: '13')
+    it 'can render values only present on the second work' do
+      login_as admin
+      visit "concern/articles/#{third_work_id}/edit"
+      expect(page).to have_field('Title', with: /The Prevalence of Bacterial Infection in Patients Undergoing/)
+      # date that includes only month and year on the edit page
+      expect(page).to have_field('Date of publication', with: 'January 2021') # aka date_issued
+      expect(page).to have_field('Journal issue', with: '1')
+      expect(page).to have_field('Journal volume', with: '11')
+      keyword_fields = page.all(:xpath, '/html/body/div[2]/div[2]/form/div/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/ul/li/input')
+      keywords = keyword_fields.map(&:value)
+      expect(keyword_fields.count).to eq 8
+      expect(keywords).to include('Propionibacterium acnes')
+      expect(page).to have_select('License', with_selected: 'Attribution-NonCommercial-NoDerivatives 4.0 International')
+      expect(page).to have_field('Page end', with: '20')
+      expect(page).to have_field('Page start', with: '13')
+    end
   end
 end
