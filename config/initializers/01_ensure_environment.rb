62d6474b133f3c292e890e661812177bb6306c29
Max Kadel <mkadel@email.unc.edu>
2022-02-09 12:50:10 -0500

    Only check for env in development mode

--- config/initializers/01_ensure_environment.rb
+++ config/initializers/01_ensure_environment.rb
@@ -1,15 +1,15 @@
 # Ensure that required environment variables are loaded prior to initializers that use them
 Rails.logger.debug('[ImageProcessing] Ensure environment variables are loaded prior to initializers')
-# if Rails.env.development?
-%w[
-  IMAGE_PROCESSOR
-].each do |env_var|
-  next unless !ENV.key?(env_var) || ENV[env_var].blank?
+if Rails.env.development?
+  %w[
+    IMAGE_PROCESSOR
+  ].each do |env_var|
+    next unless !ENV.key?(env_var) || ENV[env_var].blank?
 
-  raise <<~MESSAGE
-    Missing environment variable: #{env_var}
+    raise <<~MESSAGE
+      Missing environment variable: #{env_var}
 
-    Ask a teammate for the appropriate value.
-  MESSAGE
+      Ask a teammate for the appropriate value.
+    MESSAGE
+  end
 end
-# end
