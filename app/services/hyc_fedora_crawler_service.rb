74129015ea7c3ce526eec6433441da898cc824e4
Max Kadel <mkadel@email.unc.edu>
2022-03-05 11:30:13 -0500

    Map any type of person affiliation

--- app/services/hyc_fedora_crawler_service.rb
+++ app/services/hyc_fedora_crawler_service.rb
@@ -1,14 +1,25 @@
 # Cribbing from https://github.com/samvera/hyrax/blob/v2.9.6/app/services/hyrax/adapters/nesting_index_adapter.rb
 module HycFedoraCrawlerService
-  # include Hyrax::Adapters::NestingIndexAdapter
+  def self.person_fields
+    [:advisors, :arrangers, :composers, :contributors, :creators, :project_directors,
+     :researchers, :reviewers, :translators]
+  end
+
   def self.crawl_for_affiliations
     ActiveFedora::Base.descendant_uris(ActiveFedora.fedora.base_uri, exclude_uri: true).each do |uri|
       id = ActiveFedora::Base.uri_to_id(uri)
       object = ActiveFedora::Base.find(id)
-      creator_affiliations = []
-      creator_affiliations = object.creators.map { |creator| creator.attributes['affiliation'] } if object.try(:creators) && !object.creators.empty?
-
-      yield(id, creator_affiliations) unless creator_affiliations.empty?
+      affiliations = []
+      person_fields.each do |field|
+        affils = person_affiliations(object, field)
+        affiliations << affils unless affils.nil?
+      end
+      yield(id, affiliations.flatten!) unless affiliations.empty?
     end
+  end
+
+  def self.person_affiliations(object, person_type)
+    people_object = object.try(person_type)
+    people_object.map { |person| person.attributes['affiliation'] } if people_object && !people_object.empty?
   end
 end
--- spec/services/hyc_fedora_crawler_service_spec.rb
+++ spec/services/hyc_fedora_crawler_service_spec.rb
@@ -1,26 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe HycFedoraCrawlerService do
+  let(:type_of_person) { :creators }
+
   let(:work_with_people) do
     General.create(title: ['New General Work with people'],
-                   creators_attributes: { '0' => { name: 'creator',
-                                                   affiliation: 'Carolina Center for Genome Sciences',
-                                                   index: 1 },
-                                          '1' => { name: 'creator2',
-                                                   affiliation: 'Department of Chemistry',
-                                                   index: 2 } })
+                   "#{type_of_person}_attributes".to_sym => { '0' => { name: "#{type_of_person}_1",
+                                                                       affiliation: 'Carolina Center for Genome Sciences',
+                                                                       index: 1 },
+                                                              '1' => { name: "#{type_of_person}_2",
+                                                                       affiliation: 'Department of Chemistry',
+                                                                       index: 2 } })
   end
 
   before do
     work_with_people
   end
 
-  it 'can crawl over all the objects in Fedora and return pairs of ids and affiliations' do
-    yielded = []
-    described_class.crawl_for_affiliations do |document_id, affiliations|
-      yielded << { id: document_id, affiliations: affiliations }
+  it 'knows what fields are person fields' do
+    expect(described_class.person_fields).to match_array([:advisors, :arrangers, :composers, :contributors, :creators,
+                                                          :project_directors, :researchers, :reviewers, :translators])
+  end
+
+  context 'with creators' do
+    let(:type_of_person) { :creators }
+
+    it 'can crawl over all the objects in Fedora and return pairs of ids and affiliations' do
+      yielded = []
+      described_class.crawl_for_affiliations do |document_id, affiliations|
+        yielded << { id: document_id, affiliations: affiliations }
+      end
+      target_hash = yielded.find { |x| x[:id] == work_with_people.id }
+      expect(target_hash[:affiliations]).to match_array([['Department of Chemistry'], ['Carolina Center for Genome Sciences']])
     end
-    target_hash = yielded.find { |x| x[:id] == work_with_people.id }
-    expect(target_hash[:affiliations]).to match_array([['Department of Chemistry'], ['Carolina Center for Genome Sciences']])
+
+    it 'can return the affiliations connected to an object' do
+      expect(described_class.person_affiliations(work_with_people, :creators)).to match_array([['Department of Chemistry'], ['Carolina Center for Genome Sciences']])
+    end
+  end
+
+  context 'with reviewers' do
+    let(:type_of_person) { :reviewers }
+
+    it 'can crawl over all the objects in Fedora and return pairs of ids and affiliations' do
+      yielded = []
+      described_class.crawl_for_affiliations do |document_id, affiliations|
+        yielded << { id: document_id, affiliations: affiliations }
+      end
+      target_hash = yielded.find { |x| x[:id] == work_with_people.id }
+      expect(target_hash[:affiliations]).to match_array([['Department of Chemistry'], ['Carolina Center for Genome Sciences']])
+    end
+
+    it 'can return the affiliations connected to an object' do
+      expect(described_class.person_affiliations(work_with_people, type_of_person)).to match_array([['Department of Chemistry'], ['Carolina Center for Genome Sciences']])
+    end
   end
 end
