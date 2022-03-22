bda0536e60b6cb0aa62e990a0f294bd86c62a36d
Max Kadel <mkadel@email.unc.edu>
2022-03-22 12:44:04 -0400

    Rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: ['fedora:8080','solr6:8983', 'fcrepo:8080', 'solr:8983', 'opaquenamespace.org', 
+WebMock.disable_net_connect!(allow_localhost: true, allow: ['fedora:8080', 'solr6:8983', 'fcrepo:8080', 'solr:8983', 'opaquenamespace.org',
                                                             'chromedriver.storage.googleapis.com'])
 
 Capybara.default_normalize_ws = true
