d1197798b47be5d46d744d066170c24ec3e8437c
Max Kadel <mkadel@email.unc.edu>
2022-04-05 10:35:49 -0400

    List file sets without attached files

---
+++ app/services/file_set_remediation_service.rb
@@ -0,0 +1,31 @@
+# This class is to remediate FileSets where the file was not attached because the anti-virus service
+# was either not running or timed out
+class FileSetRemediationService
+  def create_csv_of_file_sets_without_files
+    CSV.open(csv_file_path, 'a+') do |csv|
+      csv << ['file_set_id', 'url']
+      FileSet.search_in_batches('*:*') do |group|
+        group.each do |solr_doc|
+          file_set = FileSet.find(solr_doc['id'])
+          next if has_files?(file_set)
+
+          csv << [file_set.id, Rails.application.routes.url_helpers.url_for(file_set)]
+        end
+      end
+    end
+  end
+
+  # rubocop:disable Naming/PredicateName
+  def has_files?(file_set)
+    !file_set.files.empty?
+  end
+  # rubocop:enable Naming/PredicateName
+
+  def csv_file_path
+    @csv_file_path ||= begin
+      csv_directory = Rails.root.join(ENV['DATA_STORAGE'], 'reports')
+      FileUtils.mkdir_p(csv_directory)
+      Rails.root.join(ENV['DATA_STORAGE'], 'reports', 'file_sets_without_files.csv')
+    end
+  end
+end
---
+++ spec/services/file_set_remediation_service_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+require 'active_fedora/cleaner'
+
+RSpec.describe FileSetRemediationService do
+  let(:file_set_without_file) { FactoryBot.create(:file_set, title: ['Should not have file']) }
+  let(:file_set_with_file) { FactoryBot.create(:file_set, :with_original_file, title: ['Should have file']) }
+  let(:service) { described_class.new }
+
+  before do
+    ActiveFedora::Cleaner.clean!
+    Blacklight.default_index.connection.delete_by_query('*:*')
+    Blacklight.default_index.connection.commit
+    file_set_without_file
+    file_set_with_file
+  end
+
+  it 'can tell if a FileSet has attached files' do
+    expect(service.has_files?(file_set_without_file)).to eq false
+    expect(service.has_files?(file_set_with_file)).to eq true
+  end
+
+  context 'creating a csv' do
+    let(:csv_path) { "#{ENV['DATA_STORAGE']}/reports/file_sets_without_files.csv" }
+    after do
+      FileUtils.remove_entry(csv_path) if File.exist?(csv_path)
+    end
+
+    it 'writes file_sets without files to a csv' do
+      expect(File.exist?(csv_path)).to eq false
+      service.create_csv_of_file_sets_without_files
+      expect(File.exist?(csv_path)).to eq true
+      csv = CSV.parse(File.read(csv_path), headers: true)
+      expect(csv.headers).to match_array(['file_set_id', 'url'])
+      expect(csv.length).to eq(1)
+      expect(csv.first.to_h).to eq({ 'file_set_id' => file_set_without_file.id,
+                                     'url' => "#{ENV['HYRAX_HOST']}/concern/file_sets/#{file_set_without_file.id}" })
+    end
+  end
+end
