83e0d82d4bce1ecaaa6cfd8d6d4b3f5efc0a767c
Max Kadel <mkadel@email.unc.edu>
2022-04-06 11:18:31 -0400

    HYC-1359: List file sets without attached files (#778)
    * List file sets without attached files

---
+++ app/services/file_set_remediation_service.rb
@@ -0,0 +1,48 @@
+# This class is to remediate FileSets where the file was not attached because the anti-virus service
+# was either not running or timed out
+class FileSetRemediationService
+  def create_csv_of_file_sets_without_files
+    CSV.open(csv_file_path, 'a+') do |csv|
+      csv << ['file_set_id', 'file_set_url', 'parent_id', 'parent_url']
+      FileSet.search_in_batches('*:*') do |batch|
+        Rails.logger.info("Finding FileSets without files for FileSets with ids: #{batch.map { |solr_doc| solr_doc['id'] }}")
+        add_file_sets_to_csv(csv, batch)
+      end
+    end
+  end
+
+  def add_file_sets_to_csv(csv, batch)
+    batch.each do |solr_doc|
+      file_set = file_set_by_id(solr_doc['id'])
+      next if file_set.nil? || has_files?(file_set)
+
+      Rails.logger.info("Adding FileSet with url: #{url_for(file_set)}")
+      csv << [file_set.id, url_for(file_set), file_set.parent&.id, url_for(file_set.parent)]
+    end
+  end
+
+  def url_for(object)
+    Rails.application.routes.url_helpers.url_for(object) if object
+  end
+
+  def file_set_by_id(id)
+    FileSet.find(id)
+  rescue ActiveFedora::ObjectNotFoundError
+    Rails.logger.warn("FileSet not found. FileSet identifier: #{id}")
+    nil
+  end
+
+  # rubocop:disable Naming/PredicateName
+  def has_files?(file_set)
+    !file_set.files.empty?
+  end
+  # rubocop:enable Naming/PredicateName
+
+  def csv_file_path
+    @csv_file_path ||= begin
+      csv_directory = Rails.root.join(ENV['DATA_STORAGE'], 'reports')
+      FileUtils.mkdir_p(csv_directory)
+      Rails.root.join(ENV['DATA_STORAGE'], 'reports', 'file_sets_without_files.csv')
+    end
+  end
+end
---
+++ spec/services/file_set_remediation_service_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require 'active_fedora/cleaner'
+
+RSpec.describe FileSetRemediationService do
+  let(:file_set_without_file) { FactoryBot.create(:file_set, title: ['Should not have file']) }
+  let(:file_set_with_file) { FactoryBot.create(:file_set, :with_original_file, title: ['Should have file']) }
+  let(:service) { described_class.new }
+  let(:work_without_file) { FactoryBot.create(:article, title: ['Work without file']) }
+  let(:work_with_file) { FactoryBot.create(:article, title: ['Work with file']) }
+  let(:work_without_file_set) { FactoryBot.create(:article, title: ['Work without file_set']) }
+  let(:file_set_without_file_without_parent) { FactoryBot.create(:file_set, title: ['Should not have file or parent']) }
+
+  before do
+    allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?) { false }
+  end
+
+  it 'can tell if a FileSet has attached files' do
+    expect(service.has_files?(file_set_without_file)).to eq false
+    expect(service.has_files?(file_set_with_file)).to eq true
+  end
+
+  context 'creating a csv' do
+    let(:csv_path) { "#{ENV['DATA_STORAGE']}/reports/file_sets_without_files.csv" }
+    after do
+      FileUtils.remove_entry(csv_path) if File.exist?(csv_path)
+    end
+    before do
+      ActiveFedora::Cleaner.clean!
+      Blacklight.default_index.connection.delete_by_query('*:*')
+      Blacklight.default_index.connection.commit
+      allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?) { false }
+      work_without_file.ordered_members << file_set_without_file
+      work_without_file.save!
+      work_with_file.ordered_members << file_set_with_file
+      work_with_file.save!
+      work_without_file_set
+      file_set_without_file_without_parent
+    end
+
+    it 'writes file_sets without files to a csv' do
+      expect(File.exist?(csv_path)).to eq false
+      service.create_csv_of_file_sets_without_files
+      expect(File.exist?(csv_path)).to eq true
+      csv = CSV.parse(File.read(csv_path), headers: true)
+      expect(csv.headers).to match_array(['file_set_id', 'file_set_url', 'parent_id', 'parent_url'])
+      expect(csv.length).to eq(2)
+      expect(csv.first.to_h).to eq({ 'file_set_id' => file_set_without_file.id,
+                                     'file_set_url' => "#{ENV['HYRAX_HOST']}/concern/file_sets/#{file_set_without_file.id}",
+                                     'parent_id' => work_without_file.id,
+                                     'parent_url' => "#{ENV['HYRAX_HOST']}/concern/articles/#{work_without_file.id}" })
+      expect(csv[1].to_h).to eq({ 'file_set_id' => file_set_without_file_without_parent.id,
+                                  'file_set_url' => "#{ENV['HYRAX_HOST']}/concern/file_sets/#{file_set_without_file_without_parent.id}",
+                                  'parent_id' => nil,
+                                  'parent_url' => nil })
+    end
+  end
+end
