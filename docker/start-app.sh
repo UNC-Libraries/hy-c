aeae1ec50d306df8fdd9f5652ec8d4d2c8740286
Max Kadel <mkadel@email.unc.edu>
2022-04-07 14:30:45 -0400

    Mount hyc-gems and bundle

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -3,8 +3,8 @@
 source /opt/rh/rh-ruby26/enable
 bundle check || bundle install
 # The bundle config and package are needed for the odd way we manage gems in production
-# bundle config --local cache_path /hyc-gems
-# bundle package
+bundle config --local cache_path /hyc-gems
+bundle package
 find . -name *.pid -delete
 bundle exec rake db:create && bundle exec rake db:migrate
 bundle exec rake setup:admin_role
