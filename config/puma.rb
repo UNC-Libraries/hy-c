1f7ce99627c7e8fb752d8ea45d7a62f4b7eae108
Jason Casden <casden@gmail.com>
2022-05-26 19:12:15 -0400

    Calculate available cores for puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
-# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# https://gist.github.com/andrius/7c26a8deef10f3105a136f958b0d582d
+workers Integer(ENV['WEB_CONCURRENCY'] || [1, `grep -c processor /proc/cpuinfo`.to_i].max - 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -9,6 +8,8 @@ preload_app!
 rackup DefaultRackup
 bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
 environment ENV['RACK_ENV'] || 'development'
+
+stdout_redirect(stdout = '/dev/stdout', stderr = '/dev/stderr', append = true)
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
