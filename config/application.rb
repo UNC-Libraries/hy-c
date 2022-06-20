f969ff3482a577b907b2f22cc79f99350cb4d10a
Ben Pennell <bbpennel@email.unc.edu>
2022-06-20 09:24:09 -0400

    Allow for local_env filename to be configured via environment variable

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Hyrax
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      env_file = File.join(Rails.root, 'config', ENV['LOCAL_ENV_FILE'] || 'local_env.yml')
+      puts "Loading env file #{env_file}"
 
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
