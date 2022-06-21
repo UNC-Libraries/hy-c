00e1fa69ce1806fe1197359a73118429c19cc592
Ben Pennell <bbpennel@email.unc.edu>
2022-06-21 09:31:35 -0400

    Implement indexing functionality

--- app/services/tasks/solr_migration_service.rb
+++ app/services/tasks/solr_migration_service.rb
@@ -12,7 +12,7 @@ module Tasks
     AF_TYPES = 'ActiveFedora*'
     OTHER_TYPES = 'DepositRecord FedoraOnlyFile'
     # Types in the order they should be listed
-    QUERY_TYPE_LIST = [ADMINSET_TYPES, COLLECTION_TYPES, WORK_TYPES, FILESET_TYPES, OTHER_TYPES, HYDRA_TYPES, AF_TYPES]
+    QUERY_TYPE_LIST = [HYDRA_TYPES, AF_TYPES, WORK_TYPES, FILESET_TYPES, OTHER_TYPES, COLLECTION_TYPES, ADMINSET_TYPES]
 
     # List all object ids in the repository, ordered by object type.
     # Returns the path to the file containing the list of ids. Its name contains the timestamp when the command was issued.
@@ -23,9 +23,10 @@ module Tasks
       filename = "id_list_#{start_time}.txt"
       file_path = File.join(output_path, filename)
       File.open(file_path, 'w') do |file|
-        QUERY_TYPE_LIST.each do |query_types|
-          record_paged_type_query(file, query_types)
-        end
+        # QUERY_TYPE_LIST.each do |query_types|
+        #   record_paged_type_query(file, query_types)
+        # end
+        record_paged_type_query(file, nil)
       end
       return file_path
     end
@@ -34,26 +35,44 @@ module Tasks
       start_row = 0
       total_count = 0
       begin
-        resp = ActiveFedora::SolrService.get("has_model_ssim:(#{query_types})",
+        # resp = ActiveFedora::SolrService.get("has_model_ssim:(#{query_types})",
+        resp = ActiveFedora::SolrService.get("*:*",
                                              sort: 'system_create_dtsi ASC',
                                              start: start_row,
                                              rows: PAGE_SIZE,
                                              fl: 'id')['response']
         total_count = resp['numFound'].to_i
+        puts "Response: #{resp}"
         resp['docs'].each do |doc|
           file.puts(doc['id'])
         end
         start_row += PAGE_SIZE
-      end while docs.length == PAGE_SIZE && (start_row + PAGE_SIZE) < total_count
+      end while resp['docs'].length == PAGE_SIZE && (start_row + PAGE_SIZE) < total_count
     end
 
     # Trigger indexing of all objects listed in the provided file
     def reindex(id_list_file)
+      progress_logger = Migrate::Services::ProgressTracker.new(progress_log_path(id_list_file))
+      Blacklight.default_index.connection.delete_by_query('*:*')
+      Blacklight.default_index.connection.commit
+      puts "Deleting everything"
+      sleep(10)
       # Read input file
       id_file = File.new(id_list_file)
-      id_file.each_line do |line|
-
+      id_file.each_line do |id_line|
+        id = id_line.chomp
+        puts "Indexing #{id}"
+        object = ActiveFedora::Base.find(id)
+        # Must use update_index instead of going to SolrService.add in order to trigger NestingCollection behaviors
+        object.update_index
+        # ActiveFedora::SolrService.add(object.to_solr, commit: true)
+        # puts "Got object #{object.to_solr}: #{outcome}"
+        progress_logger.add_entry(id)
       end
+    end
+
+    def progress_log_path(id_list_file)
+      "#{id_list_file}-progress.log"
     end
   end
 end
--- lib/tasks/migrate_solr.rake
+++ lib/tasks/migrate_solr.rake
@@ -1,15 +1,19 @@
-# bundle exec rake sage:ingest[path_to_my_config]
+# bundle exec rake migrate_solr:list_ids[/tmp/]
+# bundle exec rake migrate_solr:reindex[/tmp/id_list_2022-06-17T21_15_18Z.txt]
+require 'time'
 namespace :migrate_solr do
   desc 'batch migrate generic files from Sage deposit'
   task :list_ids, [:output_path, :after_timestamp] => :environment do |_t, args|
-    puts "[#{Time.now}] starting listing of ids"
-    file_path = Tasks::SolrMigrationService.new().list_ids(output_path, after_timestamp)
-    puts "[#{Time.now}] stored object list to file #{file_path}"
+    puts "[#{Time.now.utc.iso8601}] starting listing of ids"
+    file_path = Tasks::SolrMigrationService.new().list_object_ids(args.output_path, args.after_timestamp)
+    puts "[#{Time.now.utc.iso8601}] stored object list to file #{file_path}"
   end
 
   task :reindex, [:id_list_file] => :environment do |_t, args|
-    puts "[#{Time.now}] starting indexing of objects from list file #{id_list_file}"
-    file_path = Tasks::SolrMigrationService.new().reindex(id_list_file)
-    puts "[#{Time.now}] finished indexing objects"
+    puts "RAILS_ENV: #{ENV['RAILS_ENV']}"
+    puts "[#{Time.now.utc.iso8601}] starting indexing of objects from list file #{args.id_list_file}"
+    puts "Using solr: #{ENV['SOLR_PRODUCTION_URL']}"
+    file_path = Tasks::SolrMigrationService.new().reindex(args.id_list_file)
+    puts "[#{Time.now.utc.iso8601}] finished indexing objects"
   end
 end
