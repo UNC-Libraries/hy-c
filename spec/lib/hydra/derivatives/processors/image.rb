621d7f039b8de112a3dc7ae23ab1be8bfdfb72e8
krwong <krwong@email.unc.edu>
2022-06-27 15:03:58 -0400

    make code a bit more concise

--- spec/lib/hydra/derivatives/processors/image.rb
+++ spec/lib/hydra/derivatives/processors/image.rb
@@ -142,40 +142,24 @@ RSpec.describe Hydra::Derivatives::Processors::Image do
           subject.process
         end
       end
-    end
-  end
 
-  context 'using GraphicsMagick' do
-    let(:directives) { { size: '100x100>', format: 'png', url: 'file:/tmp/12/34/56/7-thumbnail.jpeg' } }
-    let(:file_name) { File.join(fixture_path, 'derivatives', 'test.tif') }
+      context 'when running the complete command' do
+        let(:file_name) { File.join(fixture_path, 'derivatives', 'test.tif') }
+  
+        it 'calls the GraphicsMagick version of create_resized_image' do
+          expect(subject).to receive(:create_resized_image_with_graphicsmagick)
+          subject.process
+        end
 
-    before do
-      allow(MiniMagick).to receive(:cli).and_return(:graphicsmagick)
-    end
-
-    around do |example|
-      cached_image_processor = ENV['IMAGE_PROCESSOR']
-      ENV['IMAGE_PROCESSOR'] = 'graphicsmagick'
-      example.run
-      ENV['IMAGE_PROCESSOR'] = cached_image_processor
-    end
-
-    it 'calls the GraphicsMagick version of create_resized_image' do
-      expect(subject).to receive(:create_resized_image_with_graphicsmagick)
-      subject.process
-    end
-
-    context 'when running the complete command' do
-      let(:file_name) { File.join(fixture_path, 'derivatives', 'test.tif') }
-
-      it 'converts the image' do
-        expect(Hyrax::PersistDerivatives).to receive(:call).with(kind_of(StringIO), directives)
-        subject.process
-      end
-
-      it 'gets the source data' do
-        expect(subject).to receive(:source_data).and_return(IMAGE_SOURCE_DATA)
-        subject.process
+        it 'converts the image' do
+          expect(Hyrax::PersistDerivatives).to receive(:call).with(kind_of(StringIO), directives)
+          subject.process
+        end
+  
+        it 'gets the source data' do
+          expect(subject).to receive(:source_data).and_return(IMAGE_SOURCE_DATA)
+          subject.process
+        end
       end
     end
   end
