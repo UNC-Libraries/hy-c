147aa800cec0586aa72059db7ff5be25a4b45bea
lfarrell <lfarrell@email.unc.edu>
2022-07-05 09:23:23 -0400

    Allow searching by rights statement text

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -354,6 +354,14 @@ class CatalogController < ApplicationController
       }
     end
 
+    config.add_search_field('rights_statement_label') do |field|
+      solr_name = solr_name('rights_statement_label', :stored_searchable)
+      field.solr_local_parameters = {
+        qf: solr_name,
+        pf: solr_name
+      }
+    end
+
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
