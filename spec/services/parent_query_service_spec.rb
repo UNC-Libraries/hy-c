a33f0ee9c8edafbb8e55109e068f4c7557572f45
krwong <krwong@email.unc.edu>
2022-07-20 13:41:46 -0400

    autocorrect Layout/SpaceAfterColon

--- spec/services/parent_query_service_spec.rb
+++ spec/services/parent_query_service_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe ParentQueryService do
-  let(:parent_doc) { { "response":{ "docs":[{ "id": 'asdf' }] } }.with_indifferent_access }
-  let(:empty_results) { { "response":{ "docs":[] } }.with_indifferent_access }
+  let(:parent_doc) { { "response": { "docs": [{ "id": 'asdf' }] } }.with_indifferent_access }
+  let(:empty_results) { { "response": { "docs": [] } }.with_indifferent_access }
   let(:child_id) { 'child_id' }
   describe '#query_parents_for_id' do
     context 'when a child work exists with a parent' do
