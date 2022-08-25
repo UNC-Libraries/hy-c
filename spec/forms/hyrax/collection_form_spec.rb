d54a90f15b8133a8f7c17f95fcb5a0eafeecc0e6
lfarrell <lfarrell@email.unc.edu>
2022-08-25 08:59:06 -0400

    Remove unneeded test

--- spec/forms/hyrax/collection_form_spec.rb
+++ spec/forms/hyrax/collection_form_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 require Rails.root.join('app/overrides/forms/collection_form_override.rb')
 
 RSpec.describe Hyrax::Forms::CollectionForm do
-  describe '#terms' do
+  describe '.terms' do
     subject { described_class.terms }
 
     it do
@@ -29,11 +29,11 @@ RSpec.describe Hyrax::Forms::CollectionForm do
     end
   end
 
-  let(:collection) { Collection.new }
-  let(:repository) { double }
-  let(:form) { described_class.new(collection, nil, repository) }
-
   describe '#secondary_terms' do
+    let(:collection) { Collection.new }
+    let(:repository) { double }
+    let(:form) { described_class.new(collection, nil, repository) }
+
     subject { form.secondary_terms }
 
     it do
@@ -50,31 +50,6 @@ RSpec.describe Hyrax::Forms::CollectionForm do
                           :related_url,
                           :resource_type
                         ]
-    end
-  end
-
-  describe '.build_permitted_params' do
-    subject { described_class.build_permitted_params }
-
-    it do
-      is_expected.to eq [{ resource_type: [] },
-                         { title: [] },
-                         { creator: [] },
-                         { contributor: [] },
-                         :description,
-                         { keyword: [] },
-                         { license: [] },
-                         { publisher: [] },
-                         :date_created,
-                         { subject: [] },
-                         { language: [] },
-                         :representative_id,
-                         :thumbnail_id,
-                         { identifier: [] },
-                         { related_url: [] },
-                         :visibility,
-                         :collection_type_gid,
-                         { permissions_attributes: [:type, :name, :access, :id, :_destroy] }]
     end
   end
 end
