804c6bc5ea21606c75490969174a2ec6bb39786c
Dean Farrell <lfarrell@email.unc.edu>
2022-08-25 15:20:42 -0400

    Move collection indexing to overrides and add tests (#844)
    * Move collection indexing to overrides and add tests
    
    * Remove all non-overridden code by calling the original_method first

---
+++ app/overrides/indexers/hyrax/collection_indexer_override.rb
@@ -0,0 +1,15 @@
+# [hyc-override] Overriding to add index fields to sort date_created as date_issued (to match works sorting) and title
+# https://github.com/samvera/hyrax/blob/v2.9.6/app/indexers/hyrax/collection_indexer.rb
+Hyrax::CollectionIndexer.class_eval do
+  alias_method :original_generate_solr_document, :generate_solr_document
+
+  # @yield [Hash] calls the yielded block with the solr document
+  # @return [Hash] the solr document WITH all changes
+  def generate_solr_document
+    current_solr_doc = original_generate_solr_document
+    current_solr_doc.tap do |solr_doc|
+      solr_doc['date_issued_sort_ssi'] = Array(object.date_created).first unless object.date_created.blank?
+      solr_doc['title_sort_ssi'] = Array(object.title).first&.downcase unless object.title.blank?
+    end
+  end
+end
