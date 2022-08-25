405f8637485470178dc70f2f40c79b383c405d36
lfarrell <lfarrell@email.unc.edu>
2022-08-25 13:36:37 -0400

    Remove all non-overridden code by calling the original_method first

--- app/overrides/indexers/hyrax/collection_indexer_override.rb
+++ app/overrides/indexers/hyrax/collection_indexer_override.rb
@@ -1,21 +1,16 @@
 # [hyc-override] Overriding to add index fields to sort date_created as date_issued (to match works sorting) and title
 # https://github.com/samvera/hyrax/blob/v2.9.6/app/indexers/hyrax/collection_indexer.rb
 Hyrax::CollectionIndexer.class_eval do
+  alias_method :original_generate_solr_document, :generate_solr_document
+
   # @yield [Hash] calls the yielded block with the solr document
   # @return [Hash] the solr document WITH all changes
   #
   def generate_solr_document
-    super.tap do |solr_doc|
-      # Makes Collections show under the "Collections" tab
-      solr_doc['generic_type_sim'] = ['Collection']
-      solr_doc['visibility_ssi'] = object.visibility
+    current_solr_doc = original_generate_solr_document
+    current_solr_doc.tap do |solr_doc|
       solr_doc['date_issued_sort_ssi'] = Array(object.date_created).first unless object.date_created.blank?
       solr_doc['title_sort_ssi'] = Array(object.title).first&.downcase unless object.title.blank?
-
-      object.in_collections.each do |col|
-        (solr_doc['member_of_collection_ids_ssim'] ||= []) << col.id
-        (solr_doc['member_of_collections_ssim'] ||= []) << col.to_s
-      end
     end
   end
 end
