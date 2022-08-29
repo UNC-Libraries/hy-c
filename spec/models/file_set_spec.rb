dca2ef765367669475f3d433b53587ddebaa7524
Ben Pennell <bbpennel@email.unc.edu>
2022-08-29 16:29:03 -0400

    Use our own FileSetBehavior rather than overriding Hyrax's. Where our behavior includes theirs

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe FileSet do
   describe 'after destroy' do
     it 'calls longleaf deregister hook' do
       expect(DeregisterLongleafJob).to receive(:perform_later).with(subject)
-      
+
       subject.destroy
     end
   end
