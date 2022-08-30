a236750600bf0002188df24687034c6a073fd591
Ben Pennell <ben.pennell@gmail.com>
2022-08-30 13:21:24 -0400

    HYC-1500 - Test coverage and change to override of FileSetBehavior (#849)
    * Add test for file_set_behavior longleaf deregister hook
    
    * Use our own FileSetBehavior rather than overriding Hyrax's. Where our behavior includes theirs

---
+++ app/models/concerns/hyc/file_set_behavior.rb
@@ -0,0 +1,15 @@
+module Hyc
+  module FileSetBehavior
+    extend ActiveSupport::Concern
+    include Hyrax::FileSetBehavior
+
+    included do
+      before_destroy :deregister_longleaf
+    end
+
+    def deregister_longleaf
+      Rails.logger.info("Calling deregistration from longleaf after delete of #{original_file}")
+      DeregisterLongleafJob.perform_later(self)
+    end
+  end
+end
