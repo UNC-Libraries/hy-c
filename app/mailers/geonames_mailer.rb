87396a87c3a6d4ad9c25d3a42a1a3a51cde7251e
lfarrell <lfarrell@email.unc.edu>
2022-08-31 11:28:08 -0400

    Add frozen string

--- app/mailers/geonames_mailer.rb
+++ app/mailers/geonames_mailer.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class GeonamesMailer < ActionMailer::Base
   def send_mail(e)
     mail(to: ENV['EMAIL_GEONAMES_ERRORS_ADDRESS'], subject: 'Unable to index geonames uri to human readable text') do |format|
