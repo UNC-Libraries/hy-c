f80b7690b3178832e2211324826ab50dbbe8cb87
lfarrell <lfarrell@email.unc.edu>
2022-08-31 09:24:55 -0400

    Mover file_set_actor override to its own file and add local tests

---
+++ app/overrides/actors/hyrax/actors/file_set_actor_override.rb
@@ -0,0 +1,11 @@
+# [hyc-override] Overriding to allow updated content jobs to run immediately so file reference isn't lost
+# https://github.com/samvera/hyrax/blob/v2.9.6/app/actors/hyrax/actors/file_set_actor.rb
+Hyrax::Actors::FileSetActor.class_eval do
+  # Spawns synchronous IngestJob with user notification afterward
+  # @param [Hyrax::UploadedFile, File, ActionDigest::HTTP::UploadedFile] file the file uploaded by the user
+  # @param [Symbol, #to_s] relation
+  # @return [IngestJob] the queued job
+  def update_content(file, relation = :original_file)
+    IngestJob.perform_now(wrapper!(file: file, relation: relation), notification: true)
+  end
+end
---
+++ spec/actors/hyrax/actors/file_set_actor_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+# Load the override being tested
+require Rails.root.join('app/overrides/actors/hyrax/actors/file_set_actor_override.rb')
+
+RSpec.describe Hyrax::Actors::FileSetActor do
+  let(:file_set) { FactoryBot.create(:file_set) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:file_path) { File.join(fixture_path, 'files', 'image.png') }
+  let(:file) { File.new(file_path) }
+  let(:relation) { :original_file }
+  let(:actor) { described_class.new(file_set, user) }
+  let(:file_actor)  { Hyrax::Actors::FileActor.new(file_set, relation, user) }
+
+  describe "#update_content" do
+    it 'calls ingest_file and returns ingest job' do
+      expect(IngestJob).to receive(:perform_now).with(any_args).and_return(IngestJob.new)
+      expect(actor.update_content(file)).to be_a(IngestJob)
+    end
+
+    it 'runs callbacks' do
+      # Do not bother ingesting the file -- test only the result of callback
+      allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?) { false }
+      allow(file_actor).to receive(:ingest_file).with(any_args).and_return(double)
+      expect(ContentNewVersionEventJob).to receive(:perform_later).with(file_set, user)
+      actor.update_content(file)
+    end
+  end
+end
