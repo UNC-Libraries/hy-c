d8874b82d119b0368784b7712c45f7537a8f9bba
Sharon Luong <snluong@email.lib.unc.edu>
2022-09-07 15:56:20 -0400

    HYC-1496 add spec file

---
+++ spec/renderers/hyrax/renderers/attribute_renderer_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require Rails.root.join('app/overrides/renderers/hyrax/renderers/attribute_renderer_override.rb')
+
+RSpec.describe Hyrax::Renderers::AttributeRenderer do
+
+  describe "#attribute_to_html" do
+    subject { Nokogiri::HTML(renderer.render) }
+
+    let(:expected) { Nokogiri::HTML(tr_content) }
+
+    context 'with links and < characters' do
+      let(:field) { :language }
+      let(:renderer) { described_class.new(field, ['http://id.loc.gov/vocabulary/iso639-2/eng']) }
+      let(:tr_content) do
+        %(
+          <tr><th>Language</th>
+          <td><ul class='tabular'><li class=\"attribute attribute-language\">
+          <span itemprop=\"language\">English
+          </span></li>
+          </ul></td></tr>
+        )
+      end
+
+      it { expect(subject).to be_equivalent_to(expected) }
+    end
+  end
+end
