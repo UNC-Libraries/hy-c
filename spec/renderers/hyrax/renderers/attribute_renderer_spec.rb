234be0a7ec1e0f9e64bc80cc85d54c2f4b4ea7fb
Sharon Luong <snluong@email.lib.unc.edu>
2022-09-09 13:05:55 -0400

    HYC-1496 adding additional tests

--- spec/renderers/hyrax/renderers/attribute_renderer_spec.rb
+++ spec/renderers/hyrax/renderers/attribute_renderer_spec.rb
@@ -24,5 +24,43 @@ RSpec.describe Hyrax::Renderers::AttributeRenderer do
 
       it { expect(subject).to be_equivalent_to(expected) }
     end
+
+    context 'with language key error' do
+      let(:field) { :language }
+      let(:renderer) { described_class.new(field, ['http://id.loc.gov/vocabulary/iso639-2/bad_code']) }
+      let(:tr_content) do
+        %(
+          <tr>
+          <th>Language</th>
+          <td><ul class="tabular">
+          <li class="attribute attribute-language">
+          <a href="http://id.loc.gov/vocabulary/iso639-2/bad_code">
+          http://id.loc.gov/vocabulary/iso639-2/bad_code
+          </a>
+          </li>
+          </ul></td>
+          </tr>
+        )
+      end
+
+      it { expect(subject).to be_equivalent_to(expected) }
+    end
+
+    context 'with no language key match' do
+      let(:field) { :language }
+      let(:renderer) { described_class.new(field, ['Elvish']) }
+      let(:tr_content) do
+        %(
+          <tr>
+          <th>Language</th>
+          <td><ul class="tabular">
+          <li class="attribute attribute-language">Elvish</li>
+          </ul></td>
+          </tr>
+        )
+      end
+
+      it { expect(subject).to be_equivalent_to(expected) }
+    end
   end
 end
