480653e9adaa6a7c513e591a0d3adb30eaeaae42
sharonl <6546457+sharonluong@users.noreply.github.com>
2022-09-12 15:03:39 -0400

    Hyc 1496 attribute renderer override (#856)
    * HYC-1496 add attribute renderer override file
    
    * HYC-1496 add spec file
    
    * HYC-1496 fixing test assertions
    
    * HYC-1496 delete replicated attribute renderer file
    
    * HYC-1496 switch to single quotes
    
    * HYC-1496 add new override methods and adjust test
    
    * HYC-1496 single quotes
    
    * HYC-1496 adjusting overrides and tests for correct html
    
    * HYC-1496 correct typo in file name
    
    * HYC-1496 adding additional tests
    
    * HYC-1496 proper syntax for allow
    
    * HYC-1496 get rid of whitespace
    
    * HYC-1496 remove obsolete comment
    
    Co-authored-by: Sharon Luong <snluong@email.lib.unc.edu>

---
+++ app/overrides/renderers/hyrax/renderers/attribute_renderer_override.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# [hyc-override] Overriding default. Show the language term instead of the saved value.
+# https://github.com/samvera/hyrax/blob/v2.9.6/app/renderers/hyrax/renderers/attribute_renderer.rb
+Hyrax::Renderers::AttributeRenderer.class_eval do
+  def find_language(language)
+    if !/iso639-2/.match(language).nil?
+      begin
+        LanguagesService.label(language)
+      rescue KeyError
+        language
+      end
+    else
+      language
+    end
+  end
+
+  private
+
+  def li_value(value)
+    field_value = find_language(value) || value
+    auto_link(ERB::Util.h(field_value))
+  end
+end
