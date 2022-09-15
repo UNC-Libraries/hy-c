83bb9068bd93679c6875dc1028eed9a7f4cb58ba
Dean Farrell <lfarrell@email.unc.edu>
2022-09-15 17:12:30 -0400

    HYC-1504: Add tests for publication behavior overrides (#859)
    * Add tests for publication behavior overrides
    
    * Add in additional override
    
    * Update test structure
    
    * Fix sytle
    
    * Add overridden formatting method back in and add tests for it.

---
+++ app/overrides/helpers/hyrax/citations_behaviors/publication_behavior_override.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+# [hyc-override] Overriding helper in order to use date_issued, if present, for publication date
+# [hyc-override] Add check that presenter has place of publication method
+# https://github.com/samvera/hyrax/blob/v2.9.6/app/helpers/hyrax/citations_behaviors/publication_behavior.rb
+Hyrax::CitationsBehaviors::PublicationBehavior.module_eval do
+  def setup_pub_date(work)
+    first_date = ''
+    if work.respond_to?(:date_issued)
+      file_date = work.date_issued
+      if file_date.is_a?(String) || !file_date.respond_to?(:first)
+        first_date = file_date
+      else
+        first_date = file_date.first
+      end
+    end
+
+    date_value = nil
+    if first_date&.present?
+      first_date = CGI.escapeHTML(first_date)
+      date_value = /\d{4}/.match(first_date)
+      return nil if date_value.blank?
+    end
+    clean_end_punctuation(date_value[0]) if date_value
+  end
+
+  def setup_pub_place(work_presenter)
+    work_presenter.place_of_publication&.first if work_presenter.respond_to?(:place_of_publication)
+  end
+
+  def setup_pub_info(work, include_date = false)
+    pub_info = []
+    if (place = setup_pub_place(work))
+      pub_info << CGI.escapeHTML(place)
+    end
+    if (publisher = setup_pub_publisher(work))
+      pub_info << ': ' unless place.to_s == ''
+      pub_info << CGI.escapeHTML(publisher)
+    end
+
+    pub_date = include_date ? setup_pub_date(work) : nil
+    pub_info << ', ' unless pub_info.blank?
+    pub_info << pub_date unless pub_date.nil?
+
+    pub_info = pub_info.join('').strip
+
+    # Remove any trailing commas
+    pub_info = pub_info[0...-1] if pub_info.last == ','
+
+    pub_info&.blank? ? nil : pub_info
+  end
+end
---
+++ spec/helpers/hyrax/citations_behaviors/publication_behavior_spec.rb
@@ -0,0 +1,190 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require Rails.root.join('app/overrides/helpers/hyrax/citations_behaviors/publication_behavior_override.rb')
+
+RSpec.describe Hyrax::CitationsBehaviors::PublicationBehavior, type: :helper do
+  describe '#setup_pub_date' do
+    context 'with a publication date' do
+      let(:work_with_date_issued) {
+        Article.new(title: ['new article title'],
+                    date_issued: '2019-10-11',
+                    abstract: ['Test Abstract'],
+                    creators_attributes: { '0' => { name: 'Test, Person',
+                                                    affiliation: 'University of North Carolina at Chapel Hill. University Libraries',
+                                                    index: 1 } })
+      }
+
+      it 'returns a formatted date from date issued' do
+        expect(helper.setup_pub_date(work_with_date_issued)).to eq '2019'
+      end
+    end
+
+    context 'with multiple publication dates' do
+      let(:work_with_multiple_date_issued) {
+        General.new(title: ['new article title'], date_issued: ['2011-01-01', '2012-04-12'])
+      }
+
+      it 'returns a formatted date for multiple date issued' do
+        expect(helper.setup_pub_date(work_with_multiple_date_issued)).to eq '2011'
+      end
+    end
+
+    context 'without a publication date' do
+      let(:work_without_date_issued) {
+        General.new(title: ['new article title'])
+      }
+
+      it 'does not return a formatted date from date issued if one is not present' do
+        expect(helper.setup_pub_date(work_without_date_issued)).to eq nil
+      end
+    end
+
+    context 'with an invalide publication date' do
+      let(:work_with_bad_date_issued) {
+        Article.new(title: ['new article title'],
+                    date_issued: 'bad date',
+                    abstract: ['Test Abstract'],
+                    creators_attributes: { '0' => { name: 'Test, Person',
+                                                    affiliation: 'University of North Carolina at Chapel Hill. University Libraries',
+                                                    index: 1 } })
+      }
+
+      it 'does not return a formatted date for invalid dates' do
+        expect(helper.setup_pub_date(work_with_bad_date_issued)).to eq nil
+      end
+    end
+  end
+
+  describe '#setup_pub_place' do
+    let(:request) { double(host: 'example.org') }
+    let(:user_key) { 'a_user_key' }
+    let(:ability) { nil }
+
+    context 'with a publication location' do
+      let(:attributes) do
+        { 'id' => '888888',
+          'title_tesim' => ['foo'],
+          'human_readable_type_tesim' => ['Article'],
+          'has_model_ssim' => ['Article'],
+          'depositor_tesim' => user_key,
+          'abstract_tesim' => ['an abstract'],
+          'access_tesim' => 'public',
+          'creator_display_tesim' => ['a creator'],
+          'date_created_tesim' => '2017-01-22',
+          'date_issued_tesim' => '2017-01-22',
+          'place_of_publication_tesim' => ['durham']
+        }
+      end
+      let(:solr_document) { SolrDocument.new(attributes) }
+      let(:presenter) { Hyrax::ArticlePresenter.new(solr_document, ability, request) }
+
+      it 'returns place of publication' do
+        expect(helper.setup_pub_place(presenter)).to eq 'durham'
+      end
+    end
+
+    context 'without a publication location' do
+      let(:attributes_no_pub) do
+        { 'id' => '888888',
+          'title_tesim' => ['foo'],
+          'human_readable_type_tesim' => ['Article'],
+          'has_model_ssim' => ['Article'],
+          'depositor_tesim' => user_key,
+          'abstract_tesim' => ['an abstract'],
+          'access_tesim' => 'public',
+          'creator_display_tesim' => ['a creator'],
+          'date_created_tesim' => '2017-01-22',
+          'date_issued_tesim' => '2017-01-22'
+        }
+      end
+      let(:solr_document_no_pub) { SolrDocument.new(attributes_no_pub) }
+      let(:presenter_no_pub) { Hyrax::ArticlePresenter.new(solr_document_no_pub, ability, request) }
+
+      it 'returns nil if place of publication is absent' do
+        expect(helper.setup_pub_place(presenter_no_pub)).to eq nil
+      end
+    end
+  end
+
+  describe '#setup_pub_info' do
+    context 'with a publication location' do
+      let(:work) {
+        General.new(title: ['new article title'], place_of_publication: ['Chapel Hill, NC'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work)).to eq 'Chapel Hill, NC'
+      end
+    end
+
+    context 'with a publication location and publisher' do
+      let(:work) {
+        General.new(title: ['new article title'], place_of_publication: ['Chapel Hill, NC'], publisher: ['UNC Press'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work)).to eq 'Chapel Hill, NC: UNC Press'
+      end
+    end
+
+    context 'with a publisher' do
+      let(:work) {
+        General.new(title: ['new article title'], publisher: ['UNC Press'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work)).to eq 'UNC Press'
+      end
+    end
+
+    context 'with publisher and publication date' do
+      let(:work) {
+        General.new(title: ['new article title'], publisher: ['UNC Press'], date_issued: ['2012-01-01'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work, true)).to eq 'UNC Press, 2012'
+      end
+    end
+
+    context 'publication date' do
+      let(:work) {
+        General.new(title: ['new article title'], date_issued: ['2012-01-01'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work, true)).to eq '2012'
+      end
+    end
+
+    context 'with location and date' do
+      let(:work) {
+        General.new(title: ['new article title'], place_of_publication: ['Chapel Hill, NC'], date_issued: ['2012-01-01'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work, true)).to eq 'Chapel Hill, NC, 2012'
+      end
+    end
+
+    context 'with publisher, location and date' do
+      let(:work) {
+        General.new(title: ['new article title'], publisher: ['UNC Press'], place_of_publication: ['Chapel Hill, NC'], date_issued: ['2012-01-01'])
+      }
+
+      it 'returns publication info' do
+        expect(helper.setup_pub_info(work, true)).to eq 'Chapel Hill, NC: UNC Press, 2012'
+      end
+    end
+
+    context 'with no publication info' do
+      let(:work) {
+        General.new(title: ['new article title'])
+      }
+
+      it 'returns nil' do
+        expect(helper.setup_pub_info(work)).to eq nil
+      end
+    end
+  end
+end
