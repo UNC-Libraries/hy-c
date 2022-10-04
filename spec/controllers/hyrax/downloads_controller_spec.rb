f413cb89d66d8edcd4fd38bcb7e76ce72be9431b
Sharon Luong <snluong@email.lib.unc.edu>
2022-10-04 10:51:11 -0400

    HYC-1485 add to downloads controller test, delete unneeded files

--- spec/controllers/hyrax/downloads_controller_spec.rb
+++ spec/controllers/hyrax/downloads_controller_spec.rb
@@ -85,47 +85,33 @@ RSpec.describe Hyrax::DownloadsController, type: :controller do
 
   describe '#download_file' do
     before do
-      class ContentHolder < ActiveFedora::Base
-        include Hydra::AccessControls::Permissions
-        has_subresource 'thumbnail'
-      end
       @user = FactoryBot.create(:user)
       sign_in @user
     end
-    let(:obj) do
-      ContentHolder.new.tap do |obj|
-        obj.add_file("It's a stream", path: 'descMetadata', original_name: 'metadata.xml', mime_type: 'text/plain')
-        obj.read_users = [@user.user_key]
-        obj.save!
-      end
-    end
-
-    after do
-      obj.destroy
-      Object.send(:remove_const, :ContentHolder)
-    end
 
     context 'when downloading file' do
-      # mock MimeTypeService to return true
+      let(:file_set) do
+        FactoryBot.create(:file_with_work, user: @user, content: File.open("#{fixture_path}/files/image.png"))
+      end
+
       it 'will add proper mime type extension if valid' do
+        allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?) { false }
         allow(MimeTypeService).to receive(:valid?) { true }
         allow(MimeTypeService).to receive(:label) { 'txt' }
-        allow(controller.request).to receive(:referrer).and_return('http://example.com')
 
-        stub = stub_request(:post, 'http://www.google-analytics.com/collect').to_return(status: 200, body: '', headers: {})
-        get :show, params: { id: obj}
+        get :show, params: { id: file_set}
         expect(response).to be_successful
-        expect(response.headers['Content-Type']).to start_with "text/plain"
-        expect(response.headers["Content-Disposition"]).to start_with "inline; filename=\"metadata.xml\""
+        expect(response.headers["Content-Disposition"]).to include "filename=\"image.png.txt\""
       end
-      # it 'will not add mime type extension if not valid' do
-      #   # mock MimeTypeService to return false
-      #   allow(MimeTypeService).to receive(:valid?) { false }
-      #   get :show, params: { id: obj}
-      #   expect(response).to be_successful
-      #   expect(response.headers['Content-Type']).to start_with "text/plain"
-      #   expect(response.headers["Content-Disposition"]).to start_with "inline; filename=\"metadata.xml\""
-      # end
+
+      it 'will not add mime type extension if not valid' do
+        allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?) { false }
+        allow(MimeTypeService).to receive(:valid?) { false }
+
+        get :show, params: { id: file_set}
+        expect(response).to be_successful
+        expect(response.headers["Content-Disposition"]).to include "filename=\"image.png\""
+      end
     end
   end
 end
