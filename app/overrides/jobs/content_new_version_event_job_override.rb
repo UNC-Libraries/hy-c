676f03d3e0401699480d362ee73b881ef80a7918
Ben Pennell <bbpennel@email.unc.edu>
2022-10-05 17:04:05 -0400

    Switch to override

--- app/overrides/jobs/content_new_version_event_job_override.rb
+++ app/overrides/jobs/content_new_version_event_job_override.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-# [hyc-override] Overriding to make depositor a facet search and not link to user profile
-# Log new version of a file to activity streams
-class ContentNewVersionEventJob < ContentEventJob
+# https://github.com/samvera/hyrax/blob/v2.9.6/app/jobs/content_new_version_event_job.rb
+Hyrax::ContentNewVersionEventJob.class_eval do
+  # [hyc-override] Overriding to make depositor a facet search and not link to user profile
   def action
     @action ||= "User #{link_to depositor, search_catalog_path(f: { depositor_ssim: [depositor] })} has added a new version of #{link_to repo_object.title.first, Rails.application.routes.url_helpers.hyrax_file_set_path(repo_object)}"
   end
--- spec/jobs/content_new_version_event_job_spec.rb
+++ spec/jobs/content_new_version_event_job_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'rails_helper'
-# require Rails.root.join('app/overrides/jobs/content_deposit_event_job_override.rb')
+require Rails.root.join('app/overrides/jobs/content_new_version_event_job_override.rb')
 
 RSpec.describe ContentNewVersionEventJob do
   let(:user) { FactoryBot.create(:user) }
