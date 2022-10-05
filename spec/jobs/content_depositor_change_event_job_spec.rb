e65efb9372f1f9927bb032df3dcc20dab295b4d3
Ben Pennell <bbpennel@email.unc.edu>
2022-10-05 12:23:11 -0400

    Switch to override

--- spec/jobs/content_depositor_change_event_job_spec.rb
+++ spec/jobs/content_depositor_change_event_job_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'rails_helper'
-# require Rails.root.join('app/overrides/jobs/content_deposit_event_job_override.rb')
+require Rails.root.join('app/overrides/jobs/content_depositor_change_event_job_override.rb')
 
 RSpec.describe ContentDepositorChangeEventJob do
   let(:user) { FactoryBot.create(:user) }
