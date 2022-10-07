be2d23f191a34c9816555646e5c2b7bb49ded1ea
Ben Pennell <bbpennel@email.unc.edu>
2022-10-07 11:03:49 -0400

    Switch back to override, use method aliases

--- app/overrides/actors/hyrax/actors/interpret_visibility_actor_override.rb
+++ app/overrides/actors/hyrax/actors/interpret_visibility_actor_override.rb
@@ -1,44 +1,29 @@
 # frozen_string_literal: true
 # [hyc-override] Overriding actor to allow admins to override admin set embargo permissions
-Hyrax::Actors::InterpretVisibilityActor class_eval do
+Hyrax::Actors::InterpretVisibilityActor.class_eval do
   private
 
+  alias_method :original_validate_release_type, :validate_release_type
   # Validate the selected release settings against template, checking for when embargoes/leases are not allowed
   def validate_release_type(env, intention, template)
     # [hyc-override] Overriding actor to allow admins to create embargoes even if not allowed by the admin set policy
     return true if env.current_ability.admin?
-    # It's valid as long as embargo is not specified when a template requires no release delays
-    return true unless intention.wants_embargo? && template.present? && template.release_no_delay?
-
-    env.curation_concern.errors.add(:visibility, 'Visibility specified does not match permission template "no release delay" requirement for selected AdminSet.')
-    false
+    original_validate_release_type(env, intention, template)
   end
 
+  alias_method :original_validate_visibility, :validate_visibility
   # Validate visibility complies with AdminSet template requirements
   def validate_visibility(env, attributes, template)
     # Added this to allow saving of overridden visibility settings in admin set
     return true if env.current_ability.admin? || env.current_ability.can?(:edit, env.curation_concern.id)
-    # NOTE: For embargo/lease, attributes[:visibility] will be nil (see sanitize_params), so visibility will be validated as part of embargo/lease
-    return true if attributes[:visibility].blank?
-
-    # Validate against template's visibility requirements
-    return true if validate_template_visibility(attributes[:visibility], template)
-
-    env.curation_concern.errors.add(:visibility, 'Visibility specified does not match permission template visibility requirement for selected AdminSet.')
-    false
+    original_validate_visibility(env, attributes, template)
   end
 
+  alias_method :original_valid_template_embargo_date?, :valid_template_embargo_date?
   # Validate an embargo date against permission template restrictions
   def valid_template_embargo_date?(env, date, template)
     # Added this to allow admins to override embargo settings in admin set
     return true if env.current_ability.admin?
-
-    return true if template.blank?
-
-    # Validate against template's release_date requirements
-    return true if template.valid_release_date?(date)
-
-    env.curation_concern.errors.add(:embargo_release_date, 'Release date specified does not match permission template release requirements for selected AdminSet.')
-    false
+    original_valid_template_embargo_date?(env, date, template)
   end
 end
--- spec/actors/hyrax/actors/interpret_visibility_actor_spec.rb
+++ spec/actors/hyrax/actors/interpret_visibility_actor_spec.rb
@@ -4,46 +4,90 @@ require 'rails_helper'
 require Rails.root.join('app/overrides/actors/hyrax/actors/interpret_visibility_actor_override.rb')
 
 RSpec.describe Hyrax::Actors::InterpretVisibilityActor do
-  let(:user) { FactoryBot.create(:user) }
-  let(:ability) { ::Ability.new(user) }
+  let(:regular_user) { FactoryBot.create(:user) }
+  let(:admin_user) { FactoryBot.create(:admin) }
   let(:curation_concern) { Article.new }
-  let(:attributes) { { admin_set_id: admin_set.id, permission_template: :permission_template } }
-  let(:admin_set) { AdminSet.create(title: ['an admin set']) }
-  let(:permission_template) do
-    Hyrax::PermissionTemplate.create(
-      source_id: admin_set.id,
-      release_period: Hyrax::PermissionTemplate::RELEASE_TEXT_VALUE_1_YEAR,
-      visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC)
-  end
+  let(:admin_set) { AdminSet.create(
+    title: ['an admin set']) }
+
   let(:terminator) { Hyrax::Actors::Terminator.new }
   let(:env) { Hyrax::Actors::Environment.new(curation_concern, ability, attributes) }
+  let(:one_year_from_today) { Time.zone.today + 1.year }
+  let(:two_years_from_today) { Time.zone.today + 2.year }
 
   subject(:middleware) do
     stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
       middleware.use described_class
-      middleware.use Hyrax::Actors::GenericWorkActor
+      middleware.use Hyrax::Actors::GeneralActor
     end
     stack.build(terminator)
   end
 
-  context "with an admin user" do
-    let(:permission_template) do
-      Hyrax::PermissionTemplate.create(
-             source_id: admin_set.id,
-             release_period: Hyrax::PermissionTemplate::RELEASE_TEXT_VALUE_1_YEAR,
-             visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC)
-    end
+  context 'with 2 year embargo' do
     let(:attributes) do
       { title: ['New embargo'],
         admin_set_id: admin_set.id,
         visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_EMBARGO,
-        embargo_release_date: one_year_from_today.to_s,
-        visibility_after_embargo: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED }
+        visibility_during_embargo: 'authenticated',
+        embargo_release_date: two_years_from_today.to_s,
+        visibility_after_embargo: 'open' }
     end
 
-    it "allows the admin user to override the permission template on the visibility field" do
-      permission_template.reload
-      expect(subject.create(env)).to be true
+    context 'with permission template with no embargo restrictions' do
+      let!(:permission_template) do 
+        Hyrax::PermissionTemplate.create!(
+          source_id: admin_set.id
+        )
+      end
+
+      context "with an admin user" do
+        let(:ability) { ::Ability.new(admin_user) }
+
+        it 'allows admin user to set embargo' do
+          expect(subject.create(env)).to be_truthy
+          expect(curation_concern.visibility_during_embargo).to eq 'authenticated'
+          expect(curation_concern.visibility_after_embargo).to eq 'open'
+          expect(curation_concern.visibility).to eq 'authenticated'
+        end
+      end
+
+      context "with an regular user" do
+        let(:ability) { ::Ability.new(regular_user) }
+
+        it 'allows regular user to set embargo' do
+          expect(subject.create(env)).to be_truthy
+          expect(curation_concern.visibility_during_embargo).to eq 'authenticated'
+          expect(curation_concern.visibility_after_embargo).to eq 'open'
+          expect(curation_concern.visibility).to eq 'authenticated'
+        end
+      end
+    end
+
+    context 'with permission template limiting embargo dates to 1 year in the future' do
+      let!(:permission_template) do 
+        Hyrax::PermissionTemplate.create!(
+          source_id: admin_set.id,
+          release_period: Hyrax::PermissionTemplate::RELEASE_TEXT_VALUE_FIXED,
+          release_date: one_year_from_today
+        )
+      end
+
+      context "with an admin user" do
+        let(:ability) { ::Ability.new(admin_user) }
+
+        it 'allows admin user to set embargo' do
+          expect(subject.create(env)).to be_truthy
+          expect(curation_concern.visibility_during_embargo).to eq 'authenticated'
+        end
+      end
+
+      context "with an regular user" do
+        let(:ability) { ::Ability.new(regular_user) }
+
+        it 'prevents regular user from setting embargo' do
+          expect(subject.create(env)).to be_falsey
+        end
+      end
     end
   end
 end
