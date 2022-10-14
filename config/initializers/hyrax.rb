04ae460e95039370e170669bb41ce02c86b16f14
Ben Pennell <bbpennel@email.unc.edu>
2022-10-14 10:46:38 -0400

    Update hyrax.rb to be aware of HYRAX_ANALYTICS properties, disable analytics in example local_env for dev environments

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -61,7 +61,8 @@ Hyrax.config do |config|
   # Enable displaying usage statistics in the UI
   # Defaults to false
   # Requires a Google Analytics id and OAuth2 keyfile.  See README for more info
-  config.analytics = true
+  config.analytics = ActiveModel::Type::Boolean.new.cast(ENV.fetch('HYRAX_ANALYTICS', 'false'))
+  config.analytics_provider = ENV.fetch('HYRAX_ANALYTICS_PROVIDER', 'google')
 
   # Google Analytics tracking ID to gather usage statistics
   config.google_analytics_id = ENV['GOOGLE_ANALYTICS_ID']
