713709af958ccea84dc2f1e87ace3c49411cfedf
Ben Pennell <bbpennel@email.unc.edu>
2022-10-18 16:32:58 -0400

    Switch to chicago_formatter to override, update to match hyrax 3, adjust doi inclusion to add in space when needed without having to override more methods like before

---
+++ app/overrides/helpers/hyrax/citations_behaviors/formatters/chicago_formatter_override.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+# https://github.com/samvera/hyrax/blob/v3.4.2/app/helpers/hyrax/citations_behaviors/formatters/apa_formatter.rb
+Hyrax::CitationsBehaviors::Formatters::ChicagoFormatter.class_eval do
+  # [hyc-override] Add helper, which is used by some of the behaviors included at a higher level
+  include HycHelper
+
+  # rubocop:disable Rails/OutputSafety
+  def format(work)
+    text = ''
+
+    # setup formatted author list
+    authors_list = all_authors(work)
+    text += format_authors(authors_list)
+    text = "<span class=\"citation-author\">#{text}</span>" if text.present?
+    # Get Pub Date
+    pub_date = setup_pub_date(work)
+    text += " #{whitewash(pub_date)}." unless pub_date.nil?
+
+    text += format_title(work.to_s)
+    pub_info = setup_pub_info(work, false)
+    text += " #{whitewash(pub_info)}." if pub_info.present?
+    # [hyc-override] Add DOI
+    text = "#{text.strip} #{work.doi[0]}" if !work.doi.blank?
+    text.html_safe
+  end
+  #rubocop:enable Rails/OutputSafety
+end
