99a58faec1b8c0fcb7bfd194c681e8824f0029d4
Ben Pennell <bbpennel@email.unc.edu>
2022-10-19 11:21:37 -0400

    Add in alternative_title

--- app/overrides/presenters/hyrax/work_show_presenter_override.rb
+++ app/overrides/presenters/hyrax/work_show_presenter_override.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 # [hyc-override] Overriding helper in order to add doi to citation
-# https://github.com/samvera/hyrax/blob/v2.9.6/app/presenters/hyrax/work_show_presenter.rb
+# https://github.com/samvera/hyrax/blob/v3.4.2/app/presenters/hyrax/work_show_presenter.rb
 Hyrax::WorkShowPresenter.class_eval do
   # delegating just :doi seems to exclude the other fields, so pull all fields in from original file
   delegate :title, :date_created, :date_issued, :description, :doi, :creator, :place_of_publication,
            :creator_display, :contributor, :subject, :publisher, :language, :embargo_release_date,
            :lease_expiration_date, :license, :source, :rights_statement, :thumbnail_id, :representative_id,
-           :rendering_ids, :member_of_collection_ids, to: :solr_document
+           :rendering_ids, :member_of_collection_ids, :alternative_title, to: :solr_document
 end
