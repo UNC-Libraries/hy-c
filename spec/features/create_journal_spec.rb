6fd8d31bb8e1fff06af3bacdb9cbdb5635713fbe
Ben Pennell <bbpennel@email.unc.edu>
2022-10-31 15:42:35 -0400

    Create helper for geoname stubbing, use it everywhere to address test failures

--- spec/features/create_journal_spec.rb
+++ spec/features/create_journal_spec.rb
@@ -2,9 +2,12 @@
 #  `rails generate hyrax:work Journal`
 require 'rails_helper'
 include Warden::Test::Helpers
+require Rails.root.join('spec/support/hyc_geoname_helper.rb')
 
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a Journal', js: false do
+  include HycGeonameHelper
+  
   context 'a logged in user' do
     let(:user) { FactoryBot.create(:user) }
 
@@ -49,26 +52,7 @@ RSpec.feature 'Create a Journal', js: false do
       permission_template.available_workflows.first.update!(active: true)
       DefaultAdminSet.create(work_type_name: 'Journal', admin_set_id: admin_set.id)
 
-      chapel_hill = <<RDFXML.strip_heredoc
-      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-          <rdf:RDF xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:gn="http://www.geonames.org/ontology#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#">
-          <gn:Feature rdf:about="http://sws.geonames.org/4460162/">
-          <gn:name>Chapel Hill</gn:name>
-          </gn:Feature>
-          </rdf:RDF>
-RDFXML
-      stub_request(:get, 'http://sws.geonames.org/4460162/').
-        to_return(status: 200, body: chapel_hill, headers: { 'Content-Type' => 'application/rdf+xml;charset=UTF-8' })
-
-      stub_request(:any, "http://api.geonames.org/getJSON?geonameId=4460162&username=#{ENV['GEONAMES_USER']}").
-        with(headers: {
-               'Accept' => '*/*',
-               'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-               'User-Agent' => 'Ruby'
-             }).to_return(status: 200, body: { asciiName: 'Chapel Hill',
-                                               countryName: 'United States',
-                                               adminName1: 'North Carolina' }.to_json,
-                          headers: { 'Content-Type' => 'application/json' })
+      stub_geo_request()
     end
 
     scenario 'as a non-admin' do
---
+++ spec/support/hyc_geoname_helper.rb
@@ -0,0 +1,21 @@
+module HycGeonameHelper
+  CHAPEL_HILL_RESP = <<RDFXML.strip_heredoc
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <rdf:RDF xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:gn="http://www.geonames.org/ontology#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#">
+        <gn:Feature rdf:about="http://sws.geonames.org/4460162/">
+        <gn:name>Chapel Hill</gn:name>
+        </gn:Feature>
+        </rdf:RDF>
+RDFXML
+
+  def stub_geo_request()
+    stub_request(:get, 'http://sws.geonames.org/4460162/').
+      to_return(status: 200, body: CHAPEL_HILL_RESP, headers: { 'Content-Type' => 'application/rdf+xml;charset=UTF-8' })
+
+    stub_request(:any, "http://www.geonames.org/getJSON?geonameId=4460162&username=#{ENV['GEONAMES_USER']}").
+      to_return(status: 200, body: { name: 'Chapel Hill',
+                                     countryName: 'United States',
+                                     adminName1: 'North Carolina' }.to_json,
+                headers: { 'Content-Type' => 'application/json' })
+  end
+end
