6cd48516519b18e5b3805510a97ac4d7baf5123a
Ben Pennell <bbpennel@email.unc.edu>
2022-11-16 10:41:53 -0500

    Indicate no workflow_restriction if user has read permission and has been granted actions in the current workflow

---
+++ app/overrides/helpers/hyrax/workflows_helper_override.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+Hyrax::WorkflowsHelper.module_eval do
+  def workflow_restriction?(object, ability: current_ability)
+      return false if object.nil? # Yup, we may get nil, and there's no restriction on nil
+      return object.workflow_restriction? if object.respond_to?(:workflow_restriction?)
+      return false if ability.can?(:edit, object)
+      # [hyc-override] permit access when user can read the object and has roles in the workflow.
+      # This handles cases where objects are suppressed, but the workflow grants viewing for some.
+      return false if ability.can?(:read, object) && object.workflow.actions.present?
+      return object.suppressed? if object.respond_to?(:suppressed?)
+      false
+    end
+end
