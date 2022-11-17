fc3891e568e8f38dec8dbc8be6c2a7c97770e647
Ben Pennell <bbpennel@email.unc.edu>
2022-11-17 16:41:19 -0500

    Add test for workflows helper override

---
+++ spec/helpers/hyrax/workflows_helper_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Hyrax::WorkflowsHelper do
+  describe '#workflow_restriction?' do
+    let(:ability) { double }
+    before { allow(controller).to receive(:current_ability).and_return(ability) }
+    subject { helper.workflow_restriction?(object) }
+    let(:workflow) { double(actions: returning_actions) }
+    let(:object) { double(workflow: workflow) }
+
+    context 'with read permissions' do
+      before { expect(ability).to receive(:can?).with(:read, object).and_return(true) }
+
+      context 'with no workflow actions' do
+        let(:returning_actions) { [] }
+        it { is_expected.to be_truthy }
+      end
+      
+      context 'with comment workflow action' do
+        let(:returning_actions) { ['comment_only'] }
+        it { is_expected.to be_falsey }
+      end
+    end
+  end
+end
