799b0f7b58843537039b6cb721229a423067e4f4
lfarrell <lfarrell@email.unc.edu>
2022-11-22 10:31:52 -0500

    Move to local variables

--- app/overrides/services/hyrax/microdata_override.rb
+++ app/overrides/services/hyrax/microdata_override.rb
@@ -2,10 +2,9 @@
 # https://github.com/samvera/hyrax/blob/v3.4.2/app/services/hyrax/microdata.rb
 Hyrax::Microdata.class_eval do
   # [hyc-override] Check for local schema.org file, so our overrides can take effect.
-  local_schema_file = Rails.root.join('config', 'schema_org.yml')
-  LOCAL_FILENAME = File.file?(local_schema_file) ? local_schema_file : FILENAME
-
   def self.load_paths
-    @load_paths ||= [LOCAL_FILENAME]
+    local_schema_file = Rails.root.join('config', 'schema_org.yml')
+    local_filename = File.file?(local_schema_file) ? local_schema_file : FILENAME
+    @load_paths ||= [local_filename]
   end
 end
