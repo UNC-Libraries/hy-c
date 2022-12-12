292783d7738004e93e6032516d0feedef1130079
Ben Pennell <bbpennel@email.unc.edu>
2022-12-12 13:49:37 -0500

    Update override to include 3.5 fix for download restrictions

--- app/overrides/controllers/hyrax/downloads_controller_override.rb
+++ app/overrides/controllers/hyrax/downloads_controller_override.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# https://github.com/samvera/hyrax/blob/v3.4.2/app/controllers/hyrax/downloads_controller.rb
+# https://github.com/samvera/hyrax/blob/hyrax-v3.5.0/app/controllers/hyrax/downloads_controller.rb
 Hyrax::DownloadsController.class_eval do
   # [hyc-override] adding downloads controller and merging hyc:downloadscontroller
   include Hyc::DownloadAnalyticsBehavior
@@ -19,12 +19,27 @@ Hyrax::DownloadsController.class_eval do
 
   private
 
+  # This is unmodified from hyrax
+  def file_set_parent(file_set_id)
+    file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
+    @parent ||=
+      case file_set
+      when Hyrax::Resource
+        Hyrax.query_service.find_parents(resource: file_set).first
+      else
+        file_set.parent
+      end
+  end
+
   # Customize the :read ability in your Ability class, or override this method.
   # Hydra::Ability#download_permissions can't be used in this case because it assumes
   # that files are in a LDP basic container, and thus, included in the asset's uri.
   def authorize_download!
     authorize! :download, params[asset_param_key]
-  rescue CanCan::AccessDenied
+    # Deny access if the work containing this file is restricted by a workflow
+    return unless workflow_restriction?(file_set_parent(params[asset_param_key]), ability: current_ability)
+    raise Hyrax::WorkflowAuthorizationException
+  rescue CanCan::AccessDenied, Hyrax::WorkflowAuthorizationException
     # [hyc-override] Send permission failures to
     render_401
   end
