79484ebe7c443f885124bfc8f64f552190b392b7
Ben Pennell <bbpennel@email.unc.edu>
2022-12-14 09:41:22 -0500

    Set page size to a high number so a reasonable number of results are returned to make the review submissions page usable

--- app/overrides/services/hyrax/solr_query_service_override.rb
+++ app/overrides/services/hyrax/solr_query_service_override.rb
@@ -1,8 +1,8 @@
-# [hyc-override] Hyrax 3.4.1 changes GET request to POST to allow for larger query size
-# https://github.com/samvera/hyrax/blob/v3.4.2/app/services/hyrax/solr_query_service.rb
+# [hyc-override] changes GET request to POST to allow for larger query size, and increase page size from default 10
+# https://github.com/samvera/hyrax/blob/v3.5.0/app/services/hyrax/solr_query_service.rb
 # frozen_string_literal: true
 Hyrax::SolrQueryService.module_eval do
   def get
-    solr_service.post(build)
+    solr_service.post(build, rows: 100000)
   end
 end
