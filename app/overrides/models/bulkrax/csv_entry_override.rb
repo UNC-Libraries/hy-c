fb162bab4c5ea5ce6a135bbe6ad53902027e2d13
Ben Pennell <bbpennel@email.unc.edu>
2022-12-20 14:55:26 -0500

    Shrink amount of overriden code by aliasing method

--- app/overrides/models/bulkrax/csv_entry_override.rb
+++ app/overrides/models/bulkrax/csv_entry_override.rb
@@ -1,31 +1,17 @@
 # frozen_string_literal: true
-
-require 'csv'
-
-# [hyc-override] overriding build_export_metadata method
 # [hyc-override] check model name before building entry
+# https://github.com/samvera-labs/bulkrax/blob/v4.4.0/app/models/bulkrax/csv_entry.rb
 Bulkrax::CsvEntry.class_eval do
+  WORK_TYPES ||= %w[Article Artwork DataSet Dissertation General HonorsThesis Journal MastersPaper Multimed ScholarlyWork]
+
+  alias_method :original_build_metadata, :build_metadata
   def build_metadata
-    raise StandardError, 'Record not found' if record.nil?
-    raise StandardError, "Missing required elements, missing element(s) are: #{importerexporter.parser.missing_elements(keys_without_numbers(record.keys)).join(', ')}" unless importerexporter.parser.required_elements?(keys_without_numbers(record.keys))
-
-    raise StandardError.new "uninitialized constant #{record['model']} (NameError)" unless record['model'].nil? || work_types.include?(record['model'])
-
-    self.parsed_metadata = {}
-    add_identifier
-    add_ingested_metadata
-    # TODO(alishaevn): remove the collections stuff entirely and only reference collections via the new parents code
-    add_collections
-    add_visibility
-    add_metadata_for_model
-    add_rights_statement
-    sanitize_controlled_uri_values!
-    add_local
-
-    self.parsed_metadata
+    raise StandardError.new "uninitialized constant #{record['model']} (NameError)" if invalid_model_type(record)
+    original_build_metadata
   end
 
-  def work_types
-    %w[Article Artwork DataSet Dissertation General HonorsThesis Journal MastersPaper Multimed ScholarlyWork]
+  def invalid_model_type(record)
+    return false if record.nil?
+    record['model'].nil? || !WORK_TYPES.include?(record['model'])
   end
 end
