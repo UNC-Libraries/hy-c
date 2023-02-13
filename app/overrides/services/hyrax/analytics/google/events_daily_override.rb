7a1c0f4a9027810f50a5383d37704108aff1f2b3
Ben Pennell <bbpennel@email.unc.edu>
2023-02-13 10:47:31 -0500

    Override analytics events to use DownloadIR for work downloads

---
+++ app/overrides/services/hyrax/analytics/google/events_daily_override.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+# https://github.com/samvera/hyrax/blob/hyrax-v3.5.0/app/services/hyrax/analytics/google/events_daily.rb
+Hyrax::Analytics::Google::EventsDaily.module_eval do
+  # [hyc-override] use DownloadIR instead of file-set-in-work-download for download count
+  filter(:file_set_in_work_download) { |_event_action| matches(:eventAction, 'DownloadIR') }
+end
