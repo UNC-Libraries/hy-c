c08b626b029e1065f9fee3f30027bdd36a751e07
Ben Pennell <bbpennel@email.unc.edu>
2023-04-04 13:26:53 -0400

    Allow arguments to be passed through get method to solr service

--- app/overrides/services/hyrax/solr_query_service_override.rb
+++ app/overrides/services/hyrax/solr_query_service_override.rb
@@ -1,8 +1,8 @@
-# [hyc-override] changes GET request to POST to allow for larger query size, and increase page size from default 10
+# [hyc-override] changes GET request to POST to allow for larger query size
 # https://github.com/samvera/hyrax/blob/v3.5.0/app/services/hyrax/solr_query_service.rb
 # frozen_string_literal: true
 Hyrax::SolrQueryService.module_eval do
-  def get
-    solr_service.post(build, rows: 1000)
+  def get(*args)
+    solr_service.post(build, *args)
   end
 end
