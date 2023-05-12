50316f69faebbcff9a6ff0cbdc9159b6a7b4bf3a
Ben Pennell <bbpennel@email.unc.edu>
2023-05-12 09:37:56 -0400

    Introduce withdrawl notification
    Send withdrawl notifications to depositor and user instigating the action
    
    Use WithdrawlNotification for withdraw action in all workflows
    
    Add withdrawlnotification test. Remove DeletionApprovalNotification test involving cc's since it is now ignoring configured recipients

---
+++ app/services/hyrax/workflow/withdrawl_notification.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module Hyrax
+  module Workflow
+    # Same behavior as the DeletionApprovalNotification, except that the notification is also sent
+    # to the instigating user
+    class WithdrawlNotification < DeletionApprovalNotification
+      private
+      def users_to_notify
+        users = []
+        user_key = ActiveFedora::Base.find(work_id).depositor
+        users << ::User.find_by(uid: user_key)
+        users << @user
+        users
+      end
+    end
+  end
+end
