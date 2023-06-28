be9cf57017284ab14351cbf38cea6b4b116d9fe4
Ben Pennell <ben.pennell@gmail.com>
2023-06-28 16:48:26 -0400

    HYC-1706 - Hyrax 3.6.0 / Use GA4 (#983)
    * Update to hyrax 3.6.0, remove staccato
    
    * Switch to using GA4 for hyrax analytics tracking and hyc DownloadIR events
    
    * Disable analytics for ga4 in pages where were have overrides that were modified as part of https://github.com/samvera/hyrax/pull/6097
    
    * Updated the case of the name of the admin role expected on the roles page (the matching seems to be case sensitive now, the role is lowercase in all environments I checked)

--- app/controllers/concerns/hyc/download_analytics_behavior.rb
+++ app/controllers/concerns/hyc/download_analytics_behavior.rb
@@ -7,28 +7,52 @@ module Hyc
       after_action :track_download, only: :show
 
       def track_download
-        if Hyrax.config.google_analytics_id.present? && !request.url.match('thumbnail')
-          # Staccato works with Google Analytics v1 api:
-          # https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
-          # Staccato on Github: https://github.com/tpitale/staccato
-          request_referrer = request.referrer
-          medium = request_referrer.present? ? 'referral' : 'direct'
-          tracker = Staccato.tracker(Hyrax.config.google_analytics_id)
-          event = tracker.build_event(
-            action: 'DownloadIR',
-            campaign_medium: medium,
-            category: @admin_set_name,
-            data_source: 'server-side',
-            hostname: request.host,
-            label: params[:id],
-            linkid: request.url,
-            referrer: request_referrer,
-            user_agent: request.headers['User-Agent'],
-            user_ip: request.remote_ip
-          )
-          Rails.logger.debug("DownloadAnalyticsBehavior request_referrer: #{request_referrer}")
-          event.track!
+        if Hyrax::Analytics.config.analytics_id.present? && !request.url.match('thumbnail')
+          Rails.logger.debug("Recording download event for #{params[:id]}")
+          medium = request.referrer.present? ? 'referral' : 'direct'
+          body = {
+              'client_id' => client_id,
+              'events' => [
+                {
+                  'name' => 'DownloadIR',
+                  'params' => {
+                    'category' => @admin_set_name,
+                    'label' => params[:id],
+                    'host_name' => request.host,
+                    'medium' => medium,
+                    'page_referrer' => request.referrer,
+                    'page_location' => request.url
+                  }
+                }
+              ]
+            }.to_json
+
+          ga_id = Hyrax::Analytics.config.analytics_id
+          url = "https://www.google-analytics.com/mp/collect?measurement_id=#{ga_id}&api_secret=#{api_secret}"
+          response = HTTParty.post(url,
+            {
+              body: body
+            })
+          if response.code >= 300
+            Rails.logger.error("DownloadAnalyticsBehavior received an error response #{response.code} for body: #{body}")
+          end
+          Rails.logger.debug("DownloadAnalyticsBehavior request completed #{response.code}")
+          response.code
         end
+      end
+
+      def api_secret
+        @api_secret ||= ENV['ANALYTICS_API_SECRET']
+      end
+
+      def client_id
+        cookie = cookies[:_ga]
+        if cookie.present?
+          parts = cookie.to_s.split('.')
+          return "#{parts[2]}.#{parts[3]}" if parts.length == 4
+        end
+        # fall back to a random id
+        return SecureRandom.uuid if cookie.nil?
       end
     end
   end
