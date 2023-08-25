a008670e02f5c9596886bb8911c3f419e631c221
Ben Pennell <bbpennel@email.unc.edu>
2023-08-25 18:07:54 -0400

    Add service for sidekiq, using the same image as the web app but a different start script. Wait for the web service to start, rather than duplicating the bundle install steps. Allow web app to accept connections as url 'web' so it can be addressed in rails 6 by other docker services

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+echo "#### Running start-app.sh"
 source scl_source enable rh-ruby27
 source scl_source enable devtoolset-8
 
 mkdir -p /opt/hyrax/log/
+echo "#### Ensure rubygems system is up to date before bundle installing"
+gem install rubygems-update -v 3.4.19
+update_rubygems  >> /dev/null
+
+echo "#### Bundle install"
 bundle check || bundle install
+
+echo "#### Performing config steps"
 # The bundle config and package are needed for the odd way we manage gems in production
 bundle config --local cache_path /hyc-gems
 bundle package
@@ -14,6 +21,6 @@ bundle exec rake db:create && bundle exec rake db:migrate
 bundle exec rake setup:admin_role
 bundle exec rails hyrax:default_collection_types:create
 bundle exec rails hyrax:default_admin_set:create
-# sidekiq 6 does not support the daemon flag anymore, so we'll need to start it some other way
-# bundle exec sidekiq --daemon
+echo "#### Starting web application"
 bundle exec puma -C config/puma.rb
+echo "#### Shutdown web application"
---
+++ docker/start-sidekiq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "#### Running start-sidekiq.sh"
+source scl_source enable rh-ruby27
+source scl_source enable devtoolset-8
+
+# Wait for the web container, so that it can handle installation of bundle gems for both
+echo "#### Waiting for web application to become available"
+ATTEMPT_COUNTER=0
+MAX_ATTEMPTS=100
+SERVICE_URL=http://web:3000/
+
+until $(curl --output /dev/null --silent --head --fail $SERVICE_URL); do
+    if [ ${ATTEMPT_COUNTER} -eq ${MAX_ATTEMPTS} ];then
+      echo "Max attempts to connect to web application reached, cannot start sidekiq"
+      exit 1
+    fi
+
+    printf '.'
+    ATTEMPT_COUNTER=$(($ATTEMPT_COUNTER+1))
+    sleep 5
+done
+
+echo "#### Ensure rubygems system is up to date before bundle installing"
+gem install rubygems-update -v 3.4.19
+update_rubygems >> /dev/null
+
+echo "#### Starting sidekiq"
+bundle && bundle exec sidekiq
+echo "#### Shutdown sidekiq"
