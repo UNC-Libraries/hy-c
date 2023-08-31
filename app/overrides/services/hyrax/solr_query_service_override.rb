cdc04430b56fcda4adcb2a19d3a16868b3be4426
Ben Pennell <bbpennel@email.unc.edu>
2023-08-31 14:19:32 -0400

    Update solr_query_service.rb

--- app/overrides/services/hyrax/solr_query_service_override.rb
+++ app/overrides/services/hyrax/solr_query_service_override.rb
@@ -2,7 +2,7 @@
 # https://github.com/samvera/hyrax/blob/v3.5.0/app/services/hyrax/solr_query_service.rb
 # frozen_string_literal: true
 Hyrax::SolrQueryService.module_eval do
-  def get(*args)
-    solr_service.post(build, *args)
+  def get(**args)
+    solr_service.post(build, **args)
   end
 end
