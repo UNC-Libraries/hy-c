c5fcf3376d8796ad13f6e0e0db4b6c129c12d4c2
Ben Pennell <ben.pennell@gmail.com>
2023-11-13 16:25:15 -0500

    HYC-1810 - MissingTemplate error for info.json requests (#1048)
    * Set formats for error pages
    
    * Add render_json_response method from included class which does not appear to be available
    
    * HYC-1813 - Send UnknownFormat errors to 404 page, and specify html format for most errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 class ErrorsController < ApplicationController
   def not_found
-    render nothing: true, status: 404
+    render nothing: true, status: 404, formats: :html
   end
 
   def internal_server_error
-    render nothing: true, status: 500
+    render nothing: true, status: 500, formats: :html
   end
 end
