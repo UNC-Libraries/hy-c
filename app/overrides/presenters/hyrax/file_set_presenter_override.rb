03414d5cd6b95dbfeb96d38d360f7905c5fb7faa
Ben Pennell <bbpennel@email.unc.edu>
2023-11-14 08:51:11 -0500

    Sync file up with version merged into hyrax main

--- app/overrides/presenters/hyrax/file_set_presenter_override.rb
+++ app/overrides/presenters/hyrax/file_set_presenter_override.rb
@@ -2,19 +2,19 @@
 # [hyc-override] https://github.com/samvera/hyrax/blob/hyrax-v4.0.0/app/presenters/hyrax/file_set_presenter.rb
 Hyrax::FileSetPresenter.class_eval do
   def fetch_parent_presenter
-    # [hyc-override] set a rows field to avoid warnings about no rows being explicitly set
-    ids = Hyrax::SolrService.query("{!field f=member_ids_ssim}#{id}", fl: Hyrax.config.id_field, rows: 10)
+    ids = Hyrax::SolrService.query("{!field f=member_ids_ssim}#{id}", fl: Hyrax.config.id_field, rows: 1)
                             .map { |x| x.fetch(Hyrax.config.id_field) }
-    Hyrax.logger.warn("Couldn't find a parent work for FileSet: #{id}.") if ids.empty?
-    ids.each do |id|
-      doc = ::SolrDocument.find(id)
-      next if current_ability.can?(:edit, doc)
-      # [hyc-override] throw exception when suppressed if user CANNOT read the doc, rather than if they can
-      raise Hyrax::WorkflowAuthorizationException if doc.suppressed? && !current_ability.can?(:read, doc)
+    if ids.empty?
+      Hyrax.logger.warn("Couldn't find a parent work for FileSet: #{id}.")
+    else
+      doc = ::SolrDocument.find(ids.first)
+      unless current_ability.can?(:edit, doc)
+        # [hyc-override] throw exception when suppressed if user CANNOT read the doc, rather than if they can
+        raise Hyrax::WorkflowAuthorizationException if doc.suppressed? && !current_ability.can?(:read, doc)
+      end
     end
     Hyrax::PresenterFactory.build_for(ids: ids,
                                       presenter_class: Hyrax::WorkShowPresenter,
                                       presenter_args: current_ability).first
-
   end
 end
