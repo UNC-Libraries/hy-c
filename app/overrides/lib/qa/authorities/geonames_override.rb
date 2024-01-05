c2e10f817de1579c4e17627ddbe8aff82ab20c3e
David Campbell <102170536+davidcam-src@users.noreply.github.com>
2024-01-05 13:52:05 -0500

    HYC-1597 - Eliminating redundant whitespace and preventing duplicate display of countries in Locations autocomplete (#1070)
    * adjustment for entities returned within location autocomplete
    
    including link to geonames codes
    
    rubocop linting changes
    
    * tests for correct label formatting
    
    * rubocop changes

--- app/overrides/lib/qa/authorities/geonames_override.rb
+++ app/overrides/lib/qa/authorities/geonames_override.rb
@@ -6,4 +6,16 @@ Qa::Authorities::Geonames.class_eval do
     query = ERB::Util.url_encode(untaint(q))
     File.join(query_url_host, "searchJSON?q=#{query}&username=#{username}&maxRows=25")
   end
+
+  # [hyc-override] Modification to remove whitespace and only display the name field if the location is a country (fcode == PCLI or PCLS)
+  # https://www.geonames.org/export/codes.html
+  self.label = lambda do |item|
+    if item['fcode'] == 'PCLI' || item['fcode'] == 'PCLS'
+      values = [item['name']]
+    else
+      # Exclude nil, empty string, and whitespace-only values
+      values = [item['name'], item['adminName1'], item['countryName']].reject { |v| v.nil? || v.strip.empty? }
+    end
+    values.join(', ')
+  end
 end
--- spec/lib/qa/authorities/geonames_spec.rb
+++ spec/lib/qa/authorities/geonames_spec.rb
@@ -12,4 +12,31 @@ describe Qa::Authorities::Geonames do
     subject { authority.build_query_url('foo') }
     it { is_expected.to eq 'http://api.geonames.org/searchJSON?q=foo&username=dummy&maxRows=25' }
   end
+
+  describe '.label' do
+    subject { authority.label.call(item) }
+    shared_examples 'correct label formatting' do |name, countryName, adminName1, fcode, expected_label|
+      let(:item) do
+        {
+        'name' => name,
+        'countryName' => countryName,
+        'adminName1' => '',
+        'fcode' => fcode
+        }
+      end
+      it "formats the label correctly when fcode is #{fcode}" do
+        is_expected.to eq expected_label
+      end
+    end
+
+    context 'when fcode is not PCLI or PCLS' do
+      include_examples 'correct label formatting', 'Chapel Rock', 'Falkland Islands', '', 'HLL', 'Chapel Rock, Falkland Islands'
+    end
+    context 'when fcode is PCLI' do
+      include_examples 'correct label formatting', 'Canada', 'Canada', '', 'PCLI', 'Canada'
+    end
+    context 'when fcode is PCLS' do
+      include_examples 'correct label formatting', 'Macao', 'Macao', '', 'PCLS', 'Macao'
+    end
+  end
 end
