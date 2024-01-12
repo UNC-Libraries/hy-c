ef36b6f3fdd62f3868a8d960b4dc444813b21c93
David Campbell <davidcam-src@proton.me>
2024-01-12 13:56:56 -0500

    adding 4.0 licenses to existing tests, including CC BY 4.0 in dataset license options

--- spec/services/cdr_license_service_spec.rb
+++ spec/services/cdr_license_service_spec.rb
@@ -9,28 +9,41 @@ RSpec.describe Hyrax::CdrLicenseService do
   let(:service) { described_class }
 
   describe '#select_options' do
-    it 'returns all options for non dataSet work types' do
-      expect(service.select('hyrax/masters_papers')).to include(
-        ['Attribution 3.0 United States', 'http://creativecommons.org/licenses/by/3.0/us/'],
-        ['Attribution-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-sa/3.0/us/'],
-        ['Attribution-NonCommercial 3.0 United States', 'http://creativecommons.org/licenses/by-nc/3.0/us/'],
-        ['Attribution-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nd/3.0/us/'],
-        ['Attribution-NonCommercial-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/'],
-        ['Attribution-NonCommercial-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/'],
-        ['Public Domain Mark 1.0', 'http://creativecommons.org/publicdomain/mark/1.0/'],
-        ['CC0 1.0 Universal', 'http://creativecommons.org/publicdomain/zero/1.0/'],
-        ['All rights reserved', 'http://www.europeana.eu/portal/rights/rr-r.html'])
-    end
+    context 'as an admin' do
+      it 'returns all options for non dataSet work types' do
+        expect(service.select('hyrax/masters_papers',true)).to include(
+          ['Attribution 4.0 International', 'http://creativecommons.org/licenses/by/4.0/'],
+          ['Attribution-NonCommercial 4.0 International', 'http://creativecommons.org/licenses/by-nc/4.0/'],
+          ['Attribution-NoDerivs 4.0 International', 'http://creativecommons.org/licenses/by-nd/4.0/'],
+          ['Attribution-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-sa/4.0/'],
+          ['Attribution-NonCommercial-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-nc-sa/4.0/'],
+          ['Attribution-NonCommercial-NoDerivs 4.0 International', 'https://creativecommons.org/licenses/by-nc-nd/4.0/'],
+          ['Attribution 3.0 United States', 'http://creativecommons.org/licenses/by/3.0/us/'],
+          ['Attribution-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-sa/3.0/us/'],
+          ['Attribution-NonCommercial 3.0 United States', 'http://creativecommons.org/licenses/by-nc/3.0/us/'],
+          ['Attribution-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nd/3.0/us/'],
+          ['Attribution-NonCommercial-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/'],
+          ['Attribution-NonCommercial-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/'],
+          ['Public Domain Mark 1.0', 'http://creativecommons.org/publicdomain/mark/1.0/'],
+          ['CC0 1.0 Universal', 'http://creativecommons.org/publicdomain/zero/1.0/'],
+          ['All rights reserved', 'http://www.europeana.eu/portal/rights/rr-r.html'])
+      end
 
-    it "returns 'limited' options for dataSet work type" do
-      expect(service.select('hyrax/data_sets')).not_to include(
-        ['Attribution-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-sa/3.0/us/'],
-        ['Attribution-NonCommercial 3.0 United States', 'http://creativecommons.org/licenses/by-nc/3.0/us/'],
-        ['Attribution-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nd/3.0/us/'],
-        ['Attribution-NonCommercial-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/'],
-        ['Attribution-NonCommercial-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/'],
-        ['Public Domain Mark 1.0', 'http://creativecommons.org/publicdomain/mark/1.0/'],
-        ['All rights reserved', 'http://www.europeana.eu/portal/rights/rr-r.html'])
+      it "returns 'limited' options for dataSet work type" do
+        expect(service.select('hyrax/data_sets',true)).not_to include(
+          ['Attribution-NonCommercial 4.0 International', 'http://creativecommons.org/licenses/by-nc/4.0/'],
+          ['Attribution-NoDerivs 4.0 International', 'http://creativecommons.org/licenses/by-nd/4.0/'],
+          ['Attribution-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-sa/4.0/'],
+          ['Attribution-NonCommercial-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-nc-sa/4.0/'],
+          ['Attribution-NonCommercial-NoDerivs 4.0 International', 'https://creativecommons.org/licenses/by-nc-nd/4.0/'],
+          ['Attribution-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-sa/3.0/us/'],
+          ['Attribution-NonCommercial 3.0 United States', 'http://creativecommons.org/licenses/by-nc/3.0/us/'],
+          ['Attribution-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nd/3.0/us/'],
+          ['Attribution-NonCommercial-NoDerivs 3.0 United States', 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/'],
+          ['Attribution-NonCommercial-ShareAlike 3.0 United States', 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/'],
+          ['Public Domain Mark 1.0', 'http://creativecommons.org/publicdomain/mark/1.0/'],
+          ['All rights reserved', 'http://www.europeana.eu/portal/rights/rr-r.html'])
+      end
     end
 
     it "returns a 'selected' option value of 'CC license' for dataSet work types" do
