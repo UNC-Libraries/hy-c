8a2daaf4f955ef70d1af200765d747f1f5f03c8f
David Campbell <davidcam-src@proton.me>
2024-01-12 14:51:56 -0500

    updating archive to archived

--- app/services/cdr_license_service.rb
+++ app/services/cdr_license_service.rb
@@ -7,7 +7,7 @@ module CdrLicenseService
     is_data_set = work_type.match?('data_sets')
     filtered_elements = authority.all
       .reject { |item| is_data_set && item['active'] != 'data'} # hide non-data licenses from data sets
-      .reject { |item| !admin_check && item['archive'] } # hide archive licenses from non-admins
+      .reject { |item| !admin_check && item['archived'] } # hide archived licenses from non-admins
     filtered_elements.map do |element|
       [element[:label], element[:id]]
     end
