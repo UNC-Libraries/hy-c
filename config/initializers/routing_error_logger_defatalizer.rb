fb364e9f4f73dc19ec26dee73c6f2a9f1d1d4931
Ben Pennell <bbpennel@email.unc.edu>
2024-02-15 13:47:58 -0500

    Rubocop

--- config/initializers/routing_error_logger_defatalizer.rb
+++ config/initializers/routing_error_logger_defatalizer.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # [hyc-override] Downgrading certain errors that we do not need to receive as FATAL
 # https://github.com/rails/rails/blob/v6.1.7.6/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb
 module ActionDispatch
