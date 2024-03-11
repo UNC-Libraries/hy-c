4da734e890db2fe5cb6be5b728f2d8826935404f
David Campbell <davidcam-src@proton.me>
2024-03-11 15:02:11 -0400

    deduplication tests for articles with duplicate pmcids and marking articles that may be false positives for review

--- spec/services/tasks/dimensions_query_service_spec.rb
+++ spec/services/tasks/dimensions_query_service_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Tasks::DimensionsQueryService do
             )
             .to_return(status: 200, body: {token: 'test_token'}.to_json, headers: { 'Content-Type' => 'application/json' })
   end
-  
+
   after do
     ActiveFedora::Cleaner.clean!
   end
@@ -83,48 +83,80 @@ RSpec.describe Tasks::DimensionsQueryService do
   end
 
   describe '#deduplicate_publications' do
-      let(:dimensions_publications) { JSON.parse(dimensions_query_response)['publications'] }
-      let(:dimensions_publications_without_dois) { JSON.parse(dimensions_query_response_non_doi)['publications'] }
+    let(:dimensions_publications) { JSON.parse(dimensions_query_response)['publications'] }
+    let(:dimensions_publications_without_dois) { JSON.parse(dimensions_query_response_non_doi)['publications'] }
 
-      it 'removes publications with duplicate dois' do
-        documents =
-          [{ id: '1111',
-          doi_tesim: ["https://doi.org/#{dimensions_publications[0]['doi']}"] },
-          { id: '2222',
-          doi_tesim: ["https://doi.org/#{dimensions_publications[1]['doi']}"]}]
+    it 'removes publications with duplicate dois' do
+      documents =
+        [{ id: '1111',
+        doi_tesim: ["https://doi.org/#{dimensions_publications[0]['doi']}"] },
+        { id: '2222',
+        doi_tesim: ["https://doi.org/#{dimensions_publications[1]['doi']}"]}]
 
-        Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
-        new_publications = service.deduplicate_publications(true, dimensions_publications)
-        expect(new_publications.count).to eq(1)
-        expect(new_publications.first['id']).to eq(dimensions_publications[2]['id'])
-      end
-
-      it 'removes publications with duplicate titles' do
-        documents =
-          [{ id: '1111',
-          title_tesim: [dimensions_publications_without_dois[0]['title']] },
-          { id: '2222',
-          title_tesim: [dimensions_publications_without_dois[1]['title']]}]
-
-        Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
-        new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
-        expect(new_publications.count).to eq(1)
-        expect(new_publications.first['id']).to eq(dimensions_publications_without_dois[2]['id'])
-      end
-
-      it 'removes publications with duplicate pmids' do
-        documents =
-          [{ id: '1111',
-          identifier_tesim: ["PMID: #{dimensions_publications_without_dois[0]['pmid']}"]},
-          { id: '2222',
-          identifier_tesim: ["PMID: #{dimensions_publications_without_dois[1]['pmid']}"]}]
-
-        Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
-        new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
-        expect(new_publications.count).to eq(1)
-        expect(new_publications.first['id']).to eq(dimensions_publications_without_dois[2]['id'])
-      end
-
+      Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
+      new_publications = service.deduplicate_publications(true, dimensions_publications)
+      expect(new_publications.count).to eq(1)
+      expect(new_publications.first['id']).to eq(dimensions_publications[2]['id'])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
     end
+
+    it 'removes publications with duplicate titles' do
+      documents =
+        [{ id: '1111',
+        title_tesim: [dimensions_publications_without_dois[0]['title']] },
+        { id: '2222',
+        title_tesim: [dimensions_publications_without_dois[1]['title']]}]
+
+      Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
+      new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
+      expect(new_publications.count).to eq(1)
+      expect(new_publications.first['id']).to eq(dimensions_publications_without_dois[2]['id'])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'removes publications with duplicate pmids' do
+      documents =
+        [{ id: '1111',
+        identifier_tesim: ["PMID: #{dimensions_publications_without_dois[0]['pmid']}"]},
+        { id: '2222',
+        identifier_tesim: ["PMID: #{dimensions_publications_without_dois[1]['pmid']}"]}]
+
+      Hyrax::SolrService.add([documents[0], documents[1]], commit: true)
+      new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
+      expect(new_publications.count).to eq(1)
+      expect(new_publications.first['id']).to eq(dimensions_publications_without_dois[2]['id'])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'removes publications with duplicate pmcids' do
+      # The only publication in the test fixture with a PMCID is the second one
+      documents =
+        [{ id: '1111',
+        identifier_tesim: ["PMCID: #{dimensions_publications_without_dois[1]['pmcid']}"]}]
+
+      non_pmcid_publication_ids = [dimensions_publications_without_dois[0]['id'], dimensions_publications_without_dois[2]['id']]
+
+      Hyrax::SolrService.add(documents[0], commit: true)
+      new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
+      expect(new_publications.count).to eq(2)
+      expect(new_publications.map { |pub| pub['id'] }).to include(*non_pmcid_publication_ids)
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'marks publications for review if it has a unique title, no pmcid, pmid or doi' do
+      spoofed_dimensions_publications = [{
+        'title' => 'Unique Title', },
+      { 'title' => 'Unique Title 2', },
+      { 'title' => 'Unique Title 3', }]
+      new_publications = service.deduplicate_publications(false, spoofed_dimensions_publications)
+      expect(new_publications.count).to eq(3)
+      expect(new_publications.map { |pub| pub['title'] }).to include(*spoofed_dimensions_publications.map { |pub| pub['title'] })
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_truthy
+    end
+
+  end
 end
- 
