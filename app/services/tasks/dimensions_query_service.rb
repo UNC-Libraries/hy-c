aedc5401dda8193de8613b1a6781b2c740663342
David Campbell <102170536+davidcam-src@users.noreply.github.com>
2024-03-14 11:32:53 -0400

    HYC-1881 - Dimensions Query Service (#1086)
    * added service with functionality for retrieving publications from dimensions that do not currently exist in the cdr
    
    * test suite and fixtures for query service also included

---
+++ app/services/tasks/dimensions_query_service.rb
@@ -0,0 +1,130 @@
+# frozen_string_literal: true
+
+module Tasks
+  class DimensionsQueryService
+    class DimensionsTokenRetrievalError < StandardError
+    end
+    class DimensionsPublicationQueryError < StandardError
+    end
+    DIMENSIONS_URL = 'https://app.dimensions.ai/api'
+
+    def query_dimensions(with_doi: true, page_size: 100)
+      # Initialized as a set to avoid retrieving duplicate publications from Dimensions if the page size exceeds the number of publications on the last page.
+      all_publications = Set.new
+      token = retrieve_token
+      doi_clause = with_doi ? 'where doi is not empty' : 'where doi is empty'
+      cursor = 0
+      # Flag to track if retry has been attempted after token refresh
+      retry_attempted = false
+      wip_count = 0
+
+      loop do
+        begin
+          # Query with paramaters to retrieve publications related to UNC
+          query_string = <<~QUERY
+                        search publications #{doi_clause} in raw_affiliations#{' '}
+                        for """
+                        "University of North Carolina, Chapel Hill" OR "UNC"
+                        """#{'  '}
+                        return publications[basics + extras]
+                        limit #{page_size}
+                        skip #{cursor}
+                      QUERY
+          response = HTTParty.post(
+              "#{DIMENSIONS_URL}/dsl",
+              headers: { 'Content-Type' => 'application/json',
+                        'Authorization' => "JWT #{token}" },
+              body: query_string,
+              format: :json
+          )
+          if response.success?
+            # Merge the new publications with the existing set
+            parsed_body = JSON.parse(response.body)
+            publications = deduplicate_publications(with_doi, parsed_body['publications'])
+            all_publications.merge(publications)
+
+            # End the loop if the cursor exceeds the total count
+            total_count = parsed_body['_stats']['total_count']
+            cursor += page_size
+
+            break if cursor >= total_count
+          elsif response.code == 403
+            if !retry_attempted
+              # If the token has expired, retrieve a new token and try the query again
+              Rails.logger.warn('Received 403 Forbidden error. Retrying after token refresh.')
+              token = retrieve_token
+              retry_attempted = true
+              redo
+            else
+              # If the token has expired and retry has already been attempted, raise a specific error
+              raise DimensionsPublicationQueryError, 'Retry attempted after token refresh failed with 403 Forbidden error'
+            end
+          else
+            raise DimensionsPublicationQueryError, "Failed to retrieve UNC affiliated articles from dimensions. Status code #{response.code}, response body: #{response.body}"
+          end
+        rescue HTTParty::Error, StandardError => e
+          Rails.logger.error("HTTParty error during Dimensions API query: #{e.message}")
+          # Re-raise the error to propagate it up the call stack
+          raise e
+        end
+      end
+      return all_publications.to_a
+    end
+
+    def retrieve_token
+      begin
+        response = HTTParty.post(
+          "#{DIMENSIONS_URL}/auth",
+          headers: { 'Content-Type' => 'application/json' },
+          body: { 'key' => "#{ENV['DIMENSIONS_API_KEY']}" }.to_json
+        )
+        if response.success?
+          return response.parsed_response['token']
+        else
+          raise DimensionsTokenRetrievalError, "Failed to retrieve Dimensions API Token. Status code #{response.code}, response body: #{response.body}"
+        end
+      rescue HTTParty::Error, StandardError => e
+        Rails.logger.error("DimensionsTokenRetrievalError: #{e.message}")
+        # Re-raise the error to propagate it up the call stack
+        raise e
+      end
+    end
+
+    def solr_query_builder(pub)
+      # Build a query string to search Solr for a publication based on pmcid, pmid, or title
+      pmcid_search = pub['pmcid'] ? "identifier_tesim:(\"PMCID: #{pub['pmcid']}\")" : nil
+      pmid_search = pub['pmid'] ? "identifier_tesim:(\"PMID: #{pub['pmid']}\")" : nil
+      title_quote_escaped = pub['title'] ? pub['title'].gsub(/"/, '\\"') : nil
+      title_search = pub['title'] ? "title_tesim:\"#{title_quote_escaped}\"" : nil
+      # Combine the search terms into a single query string excluding nil values
+      publication_data = [pmcid_search, pmid_search, title_search].compact
+      query_string = publication_data.join(' OR ')
+      return query_string
+    end
+
+    def deduplicate_publications(with_doi, publications)
+      if with_doi
+        # Removing publications that have a matching DOI in Solr
+        new_publications = publications.reject do |pub|
+          doi_tesim = "https://doi.org/#{pub['doi']}"
+          result = Hyrax::SolrService.get("doi_tesim:\"#{doi_tesim}\" OR identifier_tesim:\"#{pub['doi']}\"")
+          !result['response']['docs'].empty?
+        end
+        return new_publications
+      else
+        # Removing publications that have a matching PMID, PMCID, or title in Solr
+        new_publications = publications.reject do |pub|
+          query_string = solr_query_builder(pub)
+          result = Hyrax::SolrService.get(query_string)
+          # Mark a publication for review if it has a unique title and no unique identifiers
+          if result['response']['docs'].empty? and pub['pmcid'].nil? and pub['pmid'].nil?
+            pub['marked_for_review'] = true
+          end
+          !result['response']['docs'].empty?
+        end
+        return new_publications
+      end
+
+    end
+end
+end
---
+++ spec/services/tasks/dimensions_query_service_spec.rb
@@ -0,0 +1,304 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Tasks::DimensionsQueryService do
+
+  ERROR_RESPONSE_CODE = 500
+  UNAUTHORIZED_CODE = 403
+  UNAUTHORIZED_MESSAGE = 'Unauthorized'
+
+  let(:dimensions_query_response_fixture) do
+    File.read(File.join(Rails.root, '/spec/fixtures/files/dimensions_query_response.json'))
+  end
+
+  let(:dimensions_query_response_fixture_non_doi) do
+    File.read(File.join(Rails.root, 'spec/fixtures/files/dimensions_query_response_non_doi.json'))
+  end
+
+  let(:service) { described_class.new }
+
+  before do
+    ActiveFedora::Cleaner.clean!
+    @original_dimensions_api_key = ENV['DIMENSIONS_API_KEY']
+    ENV['DIMENSIONS_API_KEY'] = 'test_api_key'
+    stub_request(:post, 'https://app.dimensions.ai/api/auth')
+        .with(
+            body: { 'key' => ENV['DIMENSIONS_API_KEY'] }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+            )
+            .to_return(status: 200, body: {token: 'test_token'}.to_json, headers: { 'Content-Type' => 'application/json' })
+  end
+
+  after do
+    ActiveFedora::Cleaner.clean!
+    ENV['DIMENSIONS_API_KEY'] = @original_dimensions_api_key
+  end
+
+  describe '#retrieve_token' do
+    it 'raises and logs an error if token retrieval returns a status code that is not 200' do
+      allow(Rails.logger).to receive(:error)
+      response_body = 'Internal Server Error'
+      stub_request(:post, 'https://app.dimensions.ai/api/auth')
+        .to_return(status: ERROR_RESPONSE_CODE, body: response_body)
+
+      expect { service.retrieve_token }.to raise_error(Tasks::DimensionsQueryService::DimensionsTokenRetrievalError)
+
+      # Check if the error message has been logged
+      expect(Rails.logger).to have_received(:error).with("DimensionsTokenRetrievalError: Failed to retrieve Dimensions API Token. Status code #{ERROR_RESPONSE_CODE}, response body: #{response_body}")
+    end
+
+    it 'returns a token' do
+      token = service.retrieve_token
+      expect(token).to eq('test_token')
+    end
+  end
+
+  describe '#query_dimensions' do
+
+    with_doi_clause = lambda { |with_doi| with_doi ? 'where doi is not empty' : 'where doi is empty' }
+
+    it 'raises and logs an error if the query returns a status code that is not 403 or 200' do
+      allow(Rails.logger).to receive(:error)
+      response_body = 'Internal Server Error'
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+        .to_return(status: ERROR_RESPONSE_CODE, body: response_body)
+
+      expect { service.query_dimensions }.to raise_error(Tasks::DimensionsQueryService::DimensionsPublicationQueryError)
+
+      # Check if the error message has been logged
+      expect(Rails.logger).to have_received(:error).with("HTTParty error during Dimensions API query: Failed to retrieve UNC affiliated articles from dimensions. Status code #{ERROR_RESPONSE_CODE}, response body: #{response_body}")
+    end
+
+    # Checks that the function only retries once to prevent infinite loops
+    it 'raises and logs an error if the query returns another 403 status code after a token refresh' do
+      allow(Rails.logger).to receive(:error)
+      allow(Rails.logger).to receive(:warn)
+
+      unauthorized_status = UNAUTHORIZED_CODE
+      unauthorized_body = UNAUTHORIZED_MESSAGE
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+        .to_return({status: unauthorized_status, body: unauthorized_body}, {status: unauthorized_status, body: unauthorized_body})
+
+      expect { service.query_dimensions }.to raise_error(Tasks::DimensionsQueryService::DimensionsPublicationQueryError)
+      expect(WebMock).to have_requested(:post, 'https://app.dimensions.ai/api/auth').times(2)
+      expect(Rails.logger).to have_received(:warn).with('Received 403 Forbidden error. Retrying after token refresh.').once
+
+      # Check if the error message has been logged
+      expect(Rails.logger).to have_received(:error).with('HTTParty error during Dimensions API query: Retry attempted after token refresh failed with 403 Forbidden error')
+    end
+
+    # Simulating token reretrieval and retry after expiration during query
+    it 'refreshes the token and retries if query returns a 403' do
+
+      allow(Rails.logger).to receive(:warn)
+      dimensions_pagination_query_responses = [
+        File.read(File.join(Rails.root, 'spec/fixtures/files/dimensions_pagination_query_response_1.json')),
+        File.read(File.join(Rails.root, 'spec/fixtures/files/dimensions_pagination_query_response_2.json'))
+      ]
+
+      # The first request will return a 403 error, the other requests will return a 200 response
+      # Repeating code to stub requests, encountered issues with chaining
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+      .with(
+        body: /skip 0/,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+      .to_return(status: 200, body: dimensions_pagination_query_responses[0], headers: { 'Content-Type' => 'application/json' })
+      .times(1)
+
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+      .with(
+        body: /skip 100/,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+      .to_return({ status: 403, body: 'Unauthorized' },
+                 { status: 200, body: dimensions_pagination_query_responses[1], headers: { 'Content-Type' => 'application/json' }})
+      .times(1)
+
+      publications = service.query_dimensions(with_doi: true)
+      expect(WebMock).to have_requested(:post, 'https://app.dimensions.ai/api/dsl').times(3)
+      expect(WebMock).to have_requested(:post, 'https://app.dimensions.ai/api/auth').times(2)
+      expect(Rails.logger).to have_received(:warn).with('Received 403 Forbidden error. Retrying after token refresh.').once
+
+       # Combine the publications from all pages for comparison
+      expected_publications = dimensions_pagination_query_responses.flat_map { |response| JSON.parse(response)['publications'] }
+
+       # Check if every publication in expected_publications is present in the retrieved publications
+      expected_publications.each do |expected_publication|
+        expect(publications).to include(expected_publication)
+      end
+      # Verifying that the number of publications retrieved is the same as the number of publications in the test fixture
+      expect(publications.count). to eq(expected_publications.count)
+    end
+
+    it 'paginates to retrieve all articles meeting search criteria' do
+      start = 0
+      page_size = 100
+
+      dimensions_pagination_query_responses = [
+        File.read(File.join(Rails.root, 'spec/fixtures/files/dimensions_pagination_query_response_1.json')),
+        File.read(File.join(Rails.root, 'spec/fixtures/files/dimensions_pagination_query_response_2.json'))
+      ]
+
+      # Stub the requests for each page
+      dimensions_pagination_query_responses.each_with_index do |response_body, index|
+        stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+          .with(
+            body: /skip #{(page_size * index)}/,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+          .to_return(status: 200, body: response_body, headers: { 'Content-Type' => 'application/json' })
+      end
+
+      publications = service.query_dimensions(with_doi: true)
+
+      # Combine the publications from all pages for comparison
+      expected_publications = dimensions_pagination_query_responses.flat_map { |response| JSON.parse(response)['publications'] }
+
+      # Check if every publication in expected_publications is present in the retrieved publications
+      expected_publications.each do |expected_publication|
+        expect(publications).to include(expected_publication)
+      end
+      # Verifying that the number of publications retrieved is the same as the number of publications in the test fixture
+      expect(publications.count). to eq(expected_publications.count)
+    end
+
+    it 'returns unc affiliated articles that have dois' do
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+      .with(
+          body: /skip 0/,
+          headers: { 'Content-Type' => 'application/json' })
+          .to_return(status: 200, body: dimensions_query_response_fixture, headers: { 'Content-Type' => 'application/json' })
+
+      publications = service.query_dimensions
+      expected_publications = JSON.parse(dimensions_query_response_fixture)['publications']
+      expect(publications).to eq(expected_publications)
+    end
+
+    it 'returns unc affiliated articles that do not have dois if specified' do
+      stub_request(:post, 'https://app.dimensions.ai/api/dsl')
+      .with(
+          body: /skip 0/,
+          headers: { 'Content-Type' => 'application/json' })
+          .to_return(status: 200, body: dimensions_query_response_fixture_non_doi, headers: { 'Content-Type' => 'application/json' })
+
+      publications = service.query_dimensions(with_doi: false)
+      expected_publications = JSON.parse(dimensions_query_response_fixture_non_doi)['publications']
+      expect(publications).to eq(expected_publications)
+    end
+  end
+
+  describe '#deduplicate_publications' do
+    let(:dimensions_publications) { JSON.parse(dimensions_query_response_fixture)['publications'] }
+    let(:dimensions_publications_without_dois) { JSON.parse(dimensions_query_response_fixture_non_doi)['publications'] }
+
+    it 'removes publications with duplicate dois' do
+      # Create two documents with dois that are the same as the first two publications in the test fixture
+      test_fixture_dois = dimensions_publications.map { |pub| pub['doi'] }
+
+      documents =
+        [{ id: '1111',
+        doi_tesim: ["https://doi.org/#{test_fixture_dois[0]}"] },
+        { id: '2222',
+        identifier_tesim: ["PMID: 12345678, https://doi.org/#{test_fixture_dois[1]}"]}]
+      Hyrax::SolrService.add(documents[0..1], commit: true)
+
+      new_publications = service.deduplicate_publications(true, dimensions_publications)
+
+      # Expecting that the two documents with dois that are the same as the first two publications in the test fixture have been removed
+      expect(new_publications.map { |pub| pub['doi'] }) .not_to include([test_fixture_dois[0..1]])
+      expect(new_publications.count).to eq(1)
+      expect(new_publications.first['doi']).to eq(test_fixture_dois[2])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'removes publications with duplicate titles' do
+
+      # Modifying the test fixture to include publications with the same title, concatenated with a string in double quotes. (to test for escaping double quotes in solr queries)
+      double_quote_test_string = '"test string in double quotes"'
+      dimensions_publications_without_modified_titles = dimensions_publications_without_dois
+      dimensions_publications_with_modified_titles = dimensions_publications_without_dois.map { |pub| pub.merge('title' => pub['title'] + double_quote_test_string) }
+      all_publications = dimensions_publications_without_modified_titles.concat(dimensions_publications_with_modified_titles)
+
+      # Creating four documents using the first two publications in the test fixture. Two with modified titles that include the test strings
+      # Using publications without dois since deduplicate will default to using the other unique identifiers if there is no doi
+      test_fixture_titles = dimensions_publications_without_modified_titles.map { |pub| pub['title'] }
+      test_fixture_titles_with_test_string = dimensions_publications_with_modified_titles.map { |pub| pub['title'] }
+
+      documents =
+        [{ id: '1111',
+        title_tesim: [test_fixture_titles[0]] },
+        { id: '2222',
+        title_tesim: [test_fixture_titles[1]]},
+        { id: '3333',
+        title_tesim: [test_fixture_titles_with_test_string[0]] },
+        { id: '4444',
+        title_tesim: [test_fixture_titles_with_test_string[1]]}]
+      Hyrax::SolrService.add(documents, commit: true)
+
+      new_publications = service.deduplicate_publications(false, all_publications)
+
+      # Expecting that the four documents with titles that are the same as the publications in all_publications have been removed
+      expect(new_publications.map { |pub| pub['title'] }) .not_to include(test_fixture_titles[0..1])
+      expect(new_publications.map { |pub| pub['title'] }) .not_to include(test_fixture_titles_with_test_string[0..1])
+
+      # Verifying deduplicate_publications only returns records with unique titles
+      expect(new_publications.count).to eq(2)
+      expect(new_publications.map { |pub| pub['title'] }).to include(test_fixture_titles[2], test_fixture_titles_with_test_string[2])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'removes publications with duplicate pmids' do
+      # Create two documents with pmids that are the same as the first two publications in the test fixture
+      # Using publications without dois since deduplicate will default to using the other unique identifiers if there is no doi
+      test_fixture_pmids = dimensions_publications_without_dois.map { |pub| pub['pmid'] }
+
+      documents =
+        [{ id: '1111',
+        identifier_tesim: ["PMID: #{test_fixture_pmids[0]}"]},
+        { id: '2222',
+        identifier_tesim: ["PMID: #{test_fixture_pmids[1]}"]}]
+      Hyrax::SolrService.add(documents[0..1], commit: true)
+
+      new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
+
+      expect(new_publications.count).to eq(1)
+      expect(new_publications.first['id']).to eq(dimensions_publications_without_dois[2]['id'])
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'removes publications with duplicate pmcids' do
+      # Create one document with pmcids that's the same as the only publication that has one in the test fixture
+      documents =
+        [{ id: '1111',
+        identifier_tesim: ["PMCID: #{dimensions_publications_without_dois[1]['pmcid']}"]}]
+
+      non_pmcid_publication_ids = [dimensions_publications_without_dois[0]['id'], dimensions_publications_without_dois[2]['id']]
+      Hyrax::SolrService.add(documents[0], commit: true)
+
+      new_publications = service.deduplicate_publications(false, dimensions_publications_without_dois)
+
+      expect(new_publications.count).to eq(2)
+      expect(new_publications.map { |pub| pub['id'] }).to include(*non_pmcid_publication_ids)
+      # Expecting that none of the publications have been marked for review
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_falsy
+    end
+
+    it 'marks publications for review if it has a unique title, no pmcid, pmid or doi' do
+      # Spoof publications with no unique identifiers
+      spoofed_dimensions_publications = [{
+        'title' => 'Unique Title', },
+      { 'title' => 'Unique Title 2', },
+      { 'title' => 'Unique Title 3', }]
+      new_publications = service.deduplicate_publications(false, spoofed_dimensions_publications)
+
+      expect(new_publications.count).to eq(3)
+      expect(new_publications.map { |pub| pub['title'] }).to include(*spoofed_dimensions_publications.map { |pub| pub['title'] })
+      expect(new_publications.map { |pub| pub['marked_for_review'] }.all?).to be_truthy
+    end
+
+  end
+end
