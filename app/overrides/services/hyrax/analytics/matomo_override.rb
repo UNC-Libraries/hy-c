2363cf02dd3eed62bfcc7df159d027fb6040a0a9
David Campbell <davidcam-src@proton.me>
2024-03-25 16:07:41 -0400

    fixing syntax errors, logging for daily events

--- app/overrides/services/hyrax/analytics/matomo_override.rb
+++ app/overrides/services/hyrax/analytics/matomo_override.rb
@@ -1,25 +1,29 @@
-Hyrax::Analytics::Matomo.class_eval do
+Hyrax::Analytics::Matomo.module_eval do
     # [hyc-override] https://github.com/samvera/hyrax/blob/hyrax-v3.5.0/app/services/hyrax/analytics/matomo.rb
-  def daily_events_for_id(id, action, date = default_date_range)
-    Rails.logger.debug("DAILY EVENTS OVERRIDE: id=#{id}, action=#{action}, date=#{date}")
-    additional_params = {
-      flat: 1,
-      segment: additional_params_helper(id, action)
-    #   label: "#{id} - #{action}"
-    }
-    response = api_params('Events.getName', 'range', date, additional_params)
-    response&.first ? response.first['nb_events'] : 0
-  end
+  class_methods do
 
-  def additional_params_helper(id, action)
-    segment = ''
-      # Filter by download event or pageview, and the id of the related work
-      # https://developer.matomo.org/api-reference/reporting-api-segmentation
-    if action == 'DownloadIR'
-      segment = "eventAction==DownloadIR;eventName==#{id}"
-    elsif action == 'work-view'
-      segment = "actionType==pageviews;dimension1==#{id}"
+    def daily_events_for_id(id, action, date = default_date_range)
+      additional_params = {
+        flat: 1,
+        segment: additional_params_helper(id, action)
+      #   label: "#{id} - #{action}"
+      }
+      response = api_params('Events.getName', 'day', date, additional_params)
+      Rails.logger.debug("DAILY EVENTS FOR ID OVERRIDE RESPONSE: response=#{response}")
+      results_array(response, 'nb_events')
     end
-    segment
+
+    def additional_params_helper(id, action)
+      segment = ''
+        # Filter by download event or pageview, and the id of the related work
+        # https://developer.matomo.org/api-reference/reporting-api-segmentation
+      if action == 'DownloadIR'
+        segment = "eventAction==DownloadIR;eventName==#{id}"
+      elsif action == 'work-view'
+        segment = "actionType==pageviews;dimension1==#{id}"
+      end
+      segment
+    end
+
   end
 end
