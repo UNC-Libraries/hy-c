2bcad1c429743806baa89e1f1a7638e8d024c8c5
David Campbell <davidcam-src@proton.me>
2024-03-26 14:34:50 -0400

    modifying log statement

--- app/overrides/services/hyrax/analytics/matomo_override.rb
+++ app/overrides/services/hyrax/analytics/matomo_override.rb
@@ -28,7 +28,7 @@ Hyrax::Analytics::Matomo.module_eval do
 
     def get(params)
       response = Faraday.get(config.base_url, params)
-      Rails.logger.debug("GET OVERRIDE: response=#{response}, json=#{JSON.parse(response.body)}")
+      Rails.logger.debug("GET OVERRIDE: response=#{response}, body = #{response.body}")
       return [] if response.status != 200
       JSON.parse(response.body)
     end
