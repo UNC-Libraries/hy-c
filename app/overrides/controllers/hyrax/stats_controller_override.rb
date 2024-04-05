bd76857580d8dd2bcb28e260827be384e1626c9e
David Campbell <davidcam-src@proton.me>
2024-04-05 16:08:09 -0400

    updating stats override

--- app/overrides/controllers/hyrax/stats_controller_override.rb
+++ app/overrides/controllers/hyrax/stats_controller_override.rb
@@ -5,6 +5,23 @@ Hyrax::StatsController.class_eval do
     # [hyc-override] different parameters for daily_events_for_id
     @document = ::SolrDocument.find(params[:id])
     @pageviews = Hyrax::Analytics.daily_events_for_id(@document.id, 'work-view')
-    @downloads = Hyrax::Analytics.daily_events_for_id(@document.id, 'DownloadIR')
+    # @downloads = Hyrax::Analytics.daily_events_for_id(@document.id, 'DownloadIR')
+    # [hyc-override] Pull DownloadIR stats from the first 100 filesets in the work
+    work = ActiveFedora::Base.find(params[:id])
+    fileset_ids = work.members.first(100).map(&:id)
+    combined_results = nil
+    fileset_ids.each do |fileset_id|
+      events = Hyrax::Analytics.daily_events_for_id(fileset_id, 'DownloadIR')
+      if combined_results.nil?
+        combined_results = events
+      else
+        # Merge incoming event counts into the combined result.
+        # Results values are lists containing 2 elements, the date and the event count.
+        events.results.each_with_index do |entry, index|
+          combined_results.results[index][1] += entry[1]
+        end
+      end
+    end
+    @downloads = combined_results
   end
 end
