109ed6d6dc81d8a0af9804afcb9cae982ce2aec2
David Campbell <davidcam-src@proton.me>
2024-04-08 17:02:47 -0400

    article_with_metadata dimensions ingest

--- app/services/tasks/dimensions_ingest_service.rb
+++ app/services/tasks/dimensions_ingest_service.rb
@@ -13,50 +13,55 @@ module Tasks
           ingest_publication(publication)
           ingested_count += 1
           rescue StandardError => e
+            # Wip remove puts later
+            puts "Error ingesting publication: #{e.message}"
             Rails.logger.error("Error ingesting publication: #{e.message}")
         end
       end
-
       ingested_count
     end
 
     def ingest_publication(publication)
     # Extract the publication attributes
-      # puts "Ingesting publication: #{publication['title']}"
-      title = publication['title']
-      creator = publication['authors']
-      funder = publication['funders']
-      date = publication['date']
-      abstract = publication['abstract']
-      version = publication['type'] == 'preprint' ? 'preprint' : nil
-      resource_type = publication['type']
-      identifier_tesim = [
-        publication['id'].present? ? "Dimensions ID: #{publication['id']}" : nil,
-        publication['doi'].present? ? "DOI: https://dx.doi.org/#{publication['doi']}" : nil,
-        publication['pmid'].present? ? "PMID: #{publication['pmid']}" : nil,
-        publication['pmcid'].present? ? "PMCID: #{publication['pmid']}" : nil,
-      ]
-      identifier_tesim.compact!
-      issn = publication['issn']
-      publisher = publication['publisher']
-      journal_title = publication['journal_title_raw']
-      journal_volume = publication['volume']
-      journal_issue = publication['issue']
-      page_start = publication['pages'].present? && publication['pages'].include?('-') ? publication['pages'].split('-').first : nil
-      page_end = publication['pages'].present? && publication['pages'].include?('-') ? publication['pages'].split('-').last : nil
-      rights_statement = CdrRightsStatementsService.label('http://rightsstatements.org/vocab/InC/1.0/')
-      dcmi_type = DcmiTypeService.label('http://purl.org/dc/dcmitype/Text')
+      article_with_metadata(publication)
+      puts "Ingesting publication: #{publication['title']}"
+      puts "Article Inspector: #{article_with_metadata(publication)}"
 
-    # Create or update the publication in the database
-      Publication.find_or_create_by(dimensions_id: dimensions_id) do |pub|
-        pub.title = title
-        pub.doi = doi
-        pub.publication_date = publication_date
-        pub.journal_title = journal_title
-        pub.journal_issn = journal_issn
-        pub.journal_eissn = journal_eissn
-        pub.authors = authors
-      end
+      # work_attributes = {
+      #   # title: publication['title'],
+      #   # creator: publication['authors'],
+      #   # funder: publication['funders'],
+      #   # date: publication['date'],
+      #   # abstract: publication['abstract'],
+      #   version: publication['type'] == 'preprint' ? 'preprint' : nil,
+      #   resource_type: publication['type'],
+      #   identifier_tesim: [
+      #     publication['id'].present? ? "Dimensions ID: #{publication['id']}" : nil,
+      #     publication['doi'].present? ? "DOI: https://dx.doi.org/#{publication['doi']}" : nil,
+      #     publication['pmid'].present? ? "PMID: #{publication['pmid']}" : nil,
+      #     publication['pmcid'].present? ? "PMCID: #{publication['pmid']}" : nil,
+      #   ].compact,
+      #   issn: publication['issn'],
+      #   publisher: publication['publisher'],
+      #   journal_title: publication['journal_title_raw'],
+      #   journal_volume: publication['volume'],
+      #   journal_issue: publication['issue'],
+      #   page_start: publication['pages'].present? && publication['pages'].include?('-') ? publication['pages'].split('-').first : nil,
+      #   page_end: publication['pages'].present? && publication['pages'].include?('-') ? publication['pages'].split('-').last : nil,
+      #   rights_statement: CdrRightsStatementsService.label('http://rightsstatements.org/vocab/InC/1.0/'),
+      #   dcmi_type: DcmiTypeService.label('http://purl.org/dc/dcmitype/Text')
+      # }
+    end
+
+    def article_with_metadata(publication)
+      art = Article.new
+      art.title = [publication['title']]
+      # art.creator = publication['authors']
+      # art.funder = publication['funders'].map { |funder| funder['name'] }
+      # art.date_issued = publication['date']
+      # art.abstract = publication['abstract']
+      art.save!
+      art
     end
   end
     end
