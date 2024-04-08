83dd358356c3fe017d02fdce0e8ea492e9412b80
David Campbell <davidcam-src@proton.me>
2024-04-08 14:56:52 -0400

    ingest service test

--- app/services/tasks/dimensions_ingest_service.rb
+++ app/services/tasks/dimensions_ingest_service.rb
@@ -13,7 +13,7 @@ module Tasks
       publications.each do |publication|
         begin
         # Ingest the publication into the database
-          ingest_publication(publication)
+          puts "Ingesting publication: #{publication['title']}"
           ingested_count += 1
           rescue StandardError => e
             Rails.logger.error("Error ingesting publication: #{e.message}")
@@ -23,27 +23,27 @@ module Tasks
       ingested_count
     end
 
-    def ingest_publication(publication)
-    # Extract the publication attributes
-      title = publication['title']
-      doi = publication['doi']
-      publication_date = publication['publication_date']
-      journal_title = publication['journal_title']
-      journal_issn = publication['journal_issn']
-      journal_eissn = publication['journal_eissn']
-      authors = publication['authors']
-      dimensions_id = publication['id']
+    # def ingest_publication(publication)
+    # # Extract the publication attributes
+    #   title = publication['title']
+    #   doi = publication['doi']
+    #   publication_date = publication['publication_date']
+    #   journal_title = publication['journal_title']
+    #   journal_issn = publication['journal_issn']
+    #   journal_eissn = publication['journal_eissn']
+    #   authors = publication['authors']
+    #   dimensions_id = publication['id']
 
-    # Create or update the publication in the database
-      Publication.find_or_create_by(dimensions_id: dimensions_id) do |pub|
-        pub.title = title
-        pub.doi = doi
-        pub.publication_date = publication_date
-        pub.journal_title = journal_title
-        pub.journal_issn = journal_issn
-        pub.journal_eissn = journal_eissn
-        pub.authors = authors
-      end
-    end
+    # # Create or update the publication in the database
+    #   Publication.find_or_create_by(dimensions_id: dimensions_id) do |pub|
+    #     pub.title = title
+    #     pub.doi = doi
+    #     pub.publication_date = publication_date
+    #     pub.journal_title = journal_title
+    #     pub.journal_issn = journal_issn
+    #     pub.journal_eissn = journal_eissn
+    #     pub.authors = authors
+    #   end
+    # end
   end
     end
---
+++ spec/services/tasks/dimensions_ingest_service_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Tasks::DimensionsIngestService do
+  let(:dimensions_query_response_fixture) do
+    File.read(File.join(Rails.root, '/spec/fixtures/files/dimensions_pagination_query_response_1.json'))
+  end
+
+  # Retrieving fixture publications and randomly assigning the marked_for_review attribute
+  let(:test_input) do
+    fixture_publications = JSON.parse(dimensions_query_response_fixture)['publications']
+    fixture_publications.each do |publication|
+      random_number = rand(1..5)
+      if random_number == 1
+        publication['marked_for_review'] = true
+  end
+    end
+    fixture_publications
+  end
+
+  describe '#ingest_dimensions_publications' do
+    it 'ingests the publications into the database' do
+      described_class.new.ingest_dimensions_publications(test_input)
+    #   expect { described_class.new.ingest_dimensions_publications(publications) }
+    #     .to change { Publication.count }.by(2)
+    end
+  end
+end
