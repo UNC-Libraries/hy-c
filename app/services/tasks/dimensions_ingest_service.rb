9a68c07288ee30cdf21c07fc8ab0781c00df6444
David Campbell <davidcam-src@proton.me>
2024-04-17 14:40:52 -0400

    adding pdfs to works, visibility and permissions attributes

--- app/services/tasks/dimensions_ingest_service.rb
+++ app/services/tasks/dimensions_ingest_service.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 module Tasks
+  require 'tasks/migration_helper'
   class DimensionsIngestService
+    attr_reader :admin_set, :depositor
+
     class DimensionsPublicationIngestError < StandardError
+    end
+
+    def initialize(config)
+      @config = config
+      # Should deposit works into an admin set
+      admin_set_title = @config['admin_set']
+      @admin_set = ::AdminSet.where(title: admin_set_title)&.first
+      raise(ActiveRecord::RecordNotFound, "Could not find AdminSet with title #{admin_set_title}") unless @admin_set.present?
+
+      @depositor = User.find_by(uid: @config['depositor_onyen'])
+      raise(ActiveRecord::RecordNotFound, "Could not find User with onyen #{@config['depositor_onyen']}") unless @depositor.present?
     end
    
     def ingest_publications(publications)
@@ -14,7 +28,13 @@ module Tasks
           if index == 3
             break
           end
-          article_with_metadata(publication)
+          article_with_metadata = article_with_metadata(publication)
+          # create_sipity_workflow(work: article_with_metadata)
+          pdf_path = extract_pdf(publication)
+          pdf_file = attach_pdf_to_work(article_with_metadata, pdf_path)
+
+          pdf_file.update permissions_attributes: group_permissions
+
           ingested_count += 1
           rescue StandardError => e
             raise DimensionsPublicationIngestError, "Error ingesting publication: #{e.message}"
@@ -27,9 +47,29 @@ module Tasks
       article = Article.new
       populate_article_metadata(article, publication)
       puts "Article Inspector: #{article.inspect}"
+      article.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+      article.permissions_attributes = group_permissions
       article.save!
       article
     end
+
+    def group_permissions
+      @group_permissions ||= MigrationHelper.get_permissions_attributes(@admin_set.id)
+    end
+
+        # def create_sipity_workflow(work:)
+    #   # Create sipity record
+    #   join = Sipity::Workflow.joins(:permission_template)
+    #   workflow = join.where(permission_templates: { source_id: work.admin_set_id }, active: true)
+    #   raise(ActiveRecord::RecordNotFound, "Could not find Sipity::Workflow with permissions template with source id #{work.admin_set_id}") unless workflow.present?
+
+    #   workflow_state = Sipity::WorkflowState.where(workflow_id: workflow.first.id, name: 'deposited')
+    #   raise(ActiveRecord::RecordNotFound, "Could not find Sipity::WorkflowState with workflow_id: #{workflow.first.id} and name: 'deposited'") unless workflow_state.present?
+
+    #   Sipity::Entity.create!(proxy_for_global_id: work.to_global_id.to_s,
+    #                          workflow: workflow.first,
+    #                          workflow_state: workflow_state.first)
+    # end
 
     def populate_article_metadata(article, publication)
       set_basic_attributes(article, publication)
@@ -39,6 +79,7 @@ module Tasks
 
     def set_basic_attributes(article, publication)
       article.title = [publication['title']]
+      article.admin_set = @admin_set
       article.creators_attributes = publication['authors'].map.with_index { |author, index| [index,author_to_hash(author, index)] }.to_h
       article.funder = format_funders_data(publication)
       article.date_issued = publication['date']
@@ -120,15 +161,39 @@ module Tasks
       pdf_url = publication['linkout']
       return nil unless pdf_url.present?
       
-      response = HTTParticley.head(pdf_url)
+      response = HTTParty.head(pdf_url)
       return nil unless response.headers['content-type'].include?('application/pdf')
 
       pdf_file = Tempfile.new(['temp_pdf', '.pdf'])
       pdf_file.binmode
-      pdf_file.write(HTTParticley.get(pdf_url).body)
+      pdf_file.write(HTTParty.get(pdf_url).body)
       pdf_file.rewind
-      pdf_file
+      
+      file_path = pdf_file.path
+      pdf_file.close  
+      file_path  # Return the file path
     end
+
+
+    def attach_pdf_to_work(work, file_path)
+      attach_file_set_to_work(work: work, file_path: file_path, user: @depositor, visibility: work.visibility)
+    end
+
+    def attach_file_set_to_work(work:, file_path:, user:, visibility:)
+      file_set_params = { visibility: visibility }
+      @logger.info("Attaching file_set for #{file_path} to DOI: #{work.identifier.first}")
+      file_set = FileSet.create
+      actor = Hyrax::Actors::FileSetActor.new(file_set, user)
+      actor.create_metadata(file_set_params)
+      file = File.open(file_path)
+      actor.create_content(file)
+      actor.attach_to_work(work, file_set_params)
+      file.close
+
+      file_set
+    end
+
+
 
   
 
--- spec/services/tasks/dimensions_ingest_service_spec.rb
+++ spec/services/tasks/dimensions_ingest_service_spec.rb
@@ -2,9 +2,28 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::DimensionsIngestService do
+  let(:config) {
+    {
+      'admin_set' => 'Open_Access_Articles_and_Book_Chapters',
+      'depositor_onyen' => 'admin'
+    }
+  }
   let(:dimensions_ingest_test_fixture) do
     File.read(File.join(Rails.root, '/spec/fixtures/files/dimensions_ingest_test_fixture.json'))
   end
+  let(:admin) { FactoryBot.create(:admin) }
+  let(:service) { described_class.new(config) }
+
+  let(:admin_set) do
+    FactoryBot.create(:admin_set, title: ['Open_Access_Articles_and_Book_Chapters'])
+  end
+  let(:permission_template) do
+    FactoryBot.create(:permission_template, source_id: admin_set.id)
+  end
+  # let(:workflow) do
+  #   FactoryBot.create(:workflow, permission_template_id: permission_template.id, active: true)
+  # end
+
 
   # Retrieving fixture publications and randomly assigning the marked_for_review attribute
   let(:test_input) do
@@ -18,9 +37,22 @@ RSpec.describe Tasks::DimensionsIngestService do
     fixture_publications
   end
 
+  before do
+    ActiveFedora::Cleaner.clean!
+    admin_set
+    permission_template
+    # workflow
+    # workflow_state
+    # return the FactoryBot admin user when searching for uid: admin from config
+    allow(User).to receive(:find_by).with(uid: 'admin').and_return(admin)
+    # return the FactoryBot admin_set when searching for admin set from config
+    allow(AdminSet).to receive(:where).with(title: 'Open_Access_Articles_and_Book_Chapters').and_return([admin_set])
+  end
+
+
   describe '#ingest_dimensions_publications' do
     it 'ingests the publications into the database' do
-      described_class.new.ingest_publications(test_input)
+      service.ingest_publications(test_input)
     #   expect { described_class.new.ingest_dimensions_publications(publications) }
     #     .to change { Publication.count }.by(2)
     end
