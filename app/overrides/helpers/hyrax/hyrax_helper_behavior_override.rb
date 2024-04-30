0b6f5efbb3dd07e1a813c5338bdd106368abcf27
David Campbell <davidcam-src@proton.me>
2024-04-30 15:56:38 -0400

    override for removing groups available to non admins

---
+++ app/overrides/helpers/hyrax/hyrax_helper_behavior_override.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+# https://github.com/samvera/hyrax/blob/hyrax-v4.0.0/app/helpers/hyrax/hyrax_helper_behavior.rb
+
+Hyrax::HyraxHelperBehavior.class_eval do
+      ##
+    # @return [Array<String>] the list of all user groups
+    def available_user_groups(ability:)
+        var = ::User.group_service.role_names
+        return ::User.group_service.role_names if ability.admin?
+    
+        group = ability.user_groups
+        # group << "registered"
+        # group << "public"
+        # Excluding "public" and "registered" groups if non admin user
+        group.delete("public")
+        group.delete("registered")
+        group
+    end
+end
