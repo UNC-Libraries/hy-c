b139bacc99d0f8f2a1e5e9c09e852b6eab4d719c
David Campbell <davidcam-src@proton.me>
2024-04-30 20:33:52 -0400

    wip commit

--- app/overrides/helpers/hyrax/hyrax_helper_behavior_override.rb
+++ app/overrides/helpers/hyrax/hyrax_helper_behavior_override.rb
@@ -5,15 +5,12 @@ Hyrax::HyraxHelperBehavior.class_eval do
       ##
     # @return [Array<String>] the list of all user groups
     def available_user_groups(ability:)
-        var = ::User.group_service.role_names
+        user_groups = ability.user_groups
+        return user_groups if ability.admin?    
         Rails.logger.info "Pasta - Current user groups: #{ability.user_groups}"
-        return var if ability.admin?    
-        group = ability.user_groups
-        # group << "registered"
-        # group << "public"
         # Excluding "public" and "registered" groups if non admin user
-        group.delete("public")
-        group.delete("registered")
-        group
+        user_groups.delete("public")
+        user_groups.delete("registered")
+        user_groups
     end
 end
