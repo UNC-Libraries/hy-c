1c6b720a42307e07a24edd5ccd0f74dfd5e0a589
Ben Pennell <bbpennel@email.unc.edu>
2024-05-16 10:13:54 -0400

    Remove GA overrides and property

--- app/overrides/services/hyrax/analytics/google/events_override.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-# https://github.com/samvera/hyrax/blob/hyrax-v3.5.0/app/services/hyrax/analytics/google/events.rb
-Hyrax::Analytics::Google::Events.module_eval do
-  # [hyc-override] use DownloadIR instead of file-set-in-work-download for download count
-  filter(:file_set_in_work_download) { |_event_action| matches(:eventAction, 'DownloadIR') }
-end
