fbac47275ba5562ec06c07b232880974c3c96b22
David Campbell <davidcam-src@proton.me>
2024-05-29 17:51:38 -0400

    null check for current_user, test added

--- app/overrides/controllers/hyrax/admin/analytics/work_reports_controller_override.rb
+++ app/overrides/controllers/hyrax/admin/analytics/work_reports_controller_override.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
 Hyrax::Admin::Analytics::WorkReportsController.class_eval do
-    private
-        def accessible_works
-          models = Hyrax.config.curation_concerns.map { |m| "\"#{m}\"" }
-          if current_user.ability.admin?
-            ActiveFedora::SolrService.query("has_model_ssim:(#{models.join(' OR ')})",
-              fl: 'title_tesim, id, member_of_collections',
-              rows: 50_000)
-          else
-             ActiveFedora::SolrService.query(
-              "edit_access_person_ssim:#{current_user} AND has_model_ssim:(#{models.join(' OR ')})",
-              fl: 'title_tesim, id, member_of_collections',
-              rows: 50_000
-            )
-          end
-        end
+  def index
+    # [hyc-override] Only show analytics to logged in users
+    return unless Hyrax.config.analytics? && Hyrax.config.analytics_provider != 'ga4' && current_user
+
+    @accessible_works ||= accessible_works
+    @accessible_file_sets ||= accessible_file_sets
+    @works_count = @accessible_works.count
+    @top_works = paginate(top_works_list, rows: 10)
+    @top_file_set_downloads = paginate(top_files_list, rows: 10)
+
+    if current_user.ability.admin?
+      @pageviews = Hyrax::Analytics.daily_events('work-view')
+      @downloads = Hyrax::Analytics.daily_events('file-set-download')
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv { export_data }
+    end
+  end
 end
--- spec/controllers/hyrax/admin/analytics/work_reports_controller_spec.rb
+++ spec/controllers/hyrax/admin/analytics/work_reports_controller_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Hyrax::Admin::Analytics::WorkReportsController, type: :controller
   routes { Hyrax::Engine.routes }
   describe 'GET #index' do
     context 'when user is not logged in' do
-      it 'raises NoMethodError for nil:NilClass' do
+      it 'does not raise an error' do
         expect {
           get :index
-        }.to raise_error(NoMethodError, /undefined method `ability' for nil:NilClass/)
+        }.to_not raise_error
       end
     end
   end
