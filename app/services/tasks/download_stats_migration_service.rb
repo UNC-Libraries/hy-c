6a1b8ed7b288c9ac39d1d3fd3dc6de44d29487cd
David Campbell <davidcam-src@proton.me>
2024-08-21 14:02:50 -0400

    truncate by date, update or create stat function

--- app/services/tasks/download_stats_migration_service.rb
+++ app/services/tasks/download_stats_migration_service.rb
@@ -5,10 +5,13 @@ module Tasks
     def list_record_info(output_path, after_timestamp = nil)
 
       # Build the query
-      query = FileDownloadStat.select(:id, :date, :downloads, :file_id)
+      query = FileDownloadStat.select("file_id, DATE_TRUNC('month', date) AS date, SUM(downloads) AS downloads")
       query = query.where('updated_at > ?', after_timestamp) if after_timestamp.present?
 
-      # Fetch the IDs in batches
+      # Group by file_id and truncated month
+      query = query.group("file_id, DATE_TRUNC('month', date)").order('file_id, month')
+
+      # Fetch the IDs in batches for memory efficiency and performance
       records = []
       query.find_in_batches(batch_size: PAGE_SIZE) do |batch|
         records.concat(batch.map { |record| { id: record.id, date: record.date, downloads: record.downloads, file_id: record.file_id } })
@@ -22,14 +25,25 @@ module Tasks
       end
     end
 
-    def replicate_to_new_table(id_list_file, clean_index)
-      # Read the list of IDs from the file and strip whitespace
-      ids = File.readlines(id_list_file).map(&:strip)
+    def migrate_to_new_table(csv_path)
+      csv_data = CSV.read(csv_path, headers: true)
+      records = csv_data.map { |row| row.to_h.symbolize_keys }
 
-      # Reindex the objects
-      ids.each do |id|
-        hyc_download_stat = HycDownloadStat.find_or_initialize_by(id)
+      # Recreate or update objects in new table
+      records.each do |stat|
+        update_or_create_stat(stat)
       end
+    end
+
+    def update_or_create_stat(stat)
+      hyc_download_stat = HycDownloadStat.find_or_initialize_by(id: stat.id.to_s)
+      hyc_download_stat.assign_attributes(
+        date: stat.date,
+        downloads: stat.downloads,
+        file_id: stat.file_id,
+        user_id: stat.user_id
+      )
+      hyc_download_stat.save
     end
   end
 end
