4f62fb624573e2e9ebe9972c6e5fed74a574b43e
David Campbell <davidcam-src@proton.me>
2024-08-23 14:42:57 -0400

    cleaning up test file

--- spec/services/tasks/download_stats_migration_service_spec.rb
+++ spec/services/tasks/download_stats_migration_service_spec.rb
@@ -5,83 +5,96 @@ RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
   let(:admin_set_title) { 'Open_Access_Articles_and_Book_Chapters' }
   let(:mock_admin_set) { FactoryBot.create(:solr_query_result, :admin_set, title_tesim: [admin_set_title]) }
   let(:output_path) { Rails.root.join('tmp', 'download_migration_test_output.csv') }
-  # let(:csv_data) { CSV.read(output_path, headers: true) }
-  # let(:output_records) { csv_data.map { |row| row.to_h.symbolize_keys } }
   let(:service) { described_class.new }
 
   before do
-    # Ensure the output file is removed before each test
-    File.delete(output_path) if File.exist?(output_path)
     allow(ActiveFedora::SolrService).to receive(:get).with("title_tesim:#{admin_set_title}", rows: 1).and_return('response' => { 'docs' => [mock_admin_set] })
   end
 
-  describe '#list_record_info' do
-    before do
-      @file_download_stats = [
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 1)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 15)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 30)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 1)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 15)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 30)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 1)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 15)),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 30))
-      ]
-      @mock_works = @file_download_stats.map { |stat| FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id]) }
-      @file_download_stats.each_with_index do |stat, index|
-        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [@mock_works[index]] })
-      end
-    end
+  after do
+    # Ensure the output file is removed after each test
+    File.delete(output_path) if File.exist?(output_path)
+  end
 
+  # Smaller groups to allow for easier testing for aggregation of download stats from daily to monthly
+  let(:file_download_stats) { [[
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 15), downloads: 10, file_id: 'file_id_1'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 30), downloads: 10, file_id: 'file_id_1'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 15), downloads: 10, file_id: 'file_id_1'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 30), downloads: 10, file_id: 'file_id_1'),
+ ],
+ [
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 15), downloads: 10, file_id: 'file_id_2'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 30), downloads: 10, file_id: 'file_id_2'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 15), downloads: 10, file_id: 'file_id_2'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 30), downloads: 10, file_id: 'file_id_2'),
+ ],
+ [
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 15), downloads: 10, file_id: 'file_id_3'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 30), downloads: 10, file_id: 'file_id_3'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 15), downloads: 10, file_id: 'file_id_3'),
+   FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 30), downloads: 10, file_id: 'file_id_3'),
+ ]]
+  }
+
+  let(:mock_works) do
+    file_download_stats.flatten.map do |stat|
+      FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id])
+    end
+  end
+
+  describe '#list_record_info' do
     it 'writes all records to the output CSV file' do
+      file_download_stats.flatten.each_with_index do |stat, index|
+        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [mock_works[index]] })
+      end
+
+      expected_records = [
+        { file_id: 'file_id_1', date: '2023-01-01 00:00:00 UTC', downloads: '20' },
+        { file_id: 'file_id_1', date: '2023-03-01 00:00:00 UTC', downloads: '20' },
+        { file_id: 'file_id_2', date: '2023-04-01 00:00:00 UTC', downloads: '20' },
+        { file_id: 'file_id_2', date: '2023-05-01 00:00:00 UTC', downloads: '20' },
+        { file_id: 'file_id_3', date: '2023-06-01 00:00:00 UTC', downloads: '20' },
+        { file_id: 'file_id_3', date: '2023-07-01 00:00:00 UTC', downloads: '20' }
+      ]
       service.list_record_info(output_path, nil)
 
       expect(File).to exist(output_path)
-      expect(csv_to_hash_array(output_path)).to match_array(expected_records_for(@file_download_stats))
+      expect(csv_to_hash_array(output_path)).to match_array(expected_records)
     end
 
     context 'with an after_timestamp' do
-      let(:after_timestamp) { 1.day.ago }
-      let!(:recent_stats) { FactoryBot.create_list(:file_download_stat, 3, updated_at: 1.hour.ago) }
-      let!(:old_stat) { FactoryBot.create(:file_download_stat, updated_at: 2.days.ago) }
+      let(:recent_stats) { FactoryBot.create_list(:file_download_stat, 3, updated_at: '2023-05-05 00:00:00 UTC') }
+      let(:old_stats) { FactoryBot.create_list(:file_download_stat, 3, updated_at: '2023-04-05 00:00:00 UTC') }
+      let(:recent_stat_file_ids) { recent_stats.map(&:file_id) }
+      let(:old_stat_file_ids) { old_stats.map(&:file_id) }
+
+      before do
+        all_stats = recent_stats + old_stats
+        all_works = all_stats.map do |stat|
+          FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id])
+        end
+        all_stats.each_with_index do |stat, index|
+          allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [all_works[index]] })
+        end
+      end
 
       it 'filters records by the given timestamp' do
-        service.list_record_info(output_path, after_timestamp)
+        # Retrieve records created after 'updated_at' date for old stats
+        service.list_record_info(output_path, '2023-04-06 00:00:00 UTC')
+        puts "CSV data: #{csv_to_hash_array(output_path).inspect}"
 
         expect(File).to exist(output_path)
-        expect(csv_to_hash_array(output_path)).not_to include(expected_records_for([old_stat]).first)
-        expect(csv_to_hash_array(output_path)).to match_array(expected_records_for(recent_stats))
+        expect(csv_to_hash_array(output_path).map { |record| record[:file_id] }).to match_array(recent_stat_file_ids)
+        expect(csv_to_hash_array(output_path).map { |record| record[:file_id] }).not_to include(*old_stat_file_ids)
       end
     end
   end
 
   describe '#migrate_to_new_table' do
     before do
-        # Smaller groups to enable easier testing for aggregation of download stats from daily to monthly
-      @file_download_stats = [[
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 15), downloads: 10, file_id: 'file_id_1'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 30), downloads: 10, file_id: 'file_id_1'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 15), downloads: 10, file_id: 'file_id_1'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 30), downloads: 10, file_id: 'file_id_1'),
-      ],
-      [
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 15), downloads: 10, file_id: 'file_id_2'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 30), downloads: 10, file_id: 'file_id_2'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 15), downloads: 10, file_id: 'file_id_2'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 30), downloads: 10, file_id: 'file_id_2'),
-      ],
-      [
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 15), downloads: 10, file_id: 'file_id_3'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 30), downloads: 10, file_id: 'file_id_3'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 15), downloads: 10, file_id: 'file_id_3'),
-        FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 30), downloads: 10, file_id: 'file_id_3'),
-      ]]
-      @mock_works = @file_download_stats.flatten.map do |stat|
-        FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id])
-      end
-      @file_download_stats.flatten.each_with_index do |stat, index|
-        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [@mock_works[index]] })
+      file_download_stats.flatten.each_with_index do |stat, index|
+        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [mock_works[index]] })
       end
       service.list_record_info(output_path, nil)
       service.migrate_to_new_table(output_path)
@@ -89,15 +102,9 @@ RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
 
     it 'creates new HycDownloadStat records from the CSV file' do
       csv_to_hash_array(output_path).each_with_index do |record, index|
-        # puts 'All HycDownloadStat records:'
-        # HycDownloadStat.all.each do |stat|
-        #   puts "ID: #{stat.id}, File ID: #{stat.fileset_id}, Date: #{stat.date}, Downloads: #{stat.download_count}"
-        # end
-        # puts "Record: #{record.inspect}"
-        # hyc_download_stat = HycDownloadStat.find(record[:id])
         work_data = WorkUtilsHelper.fetch_work_data_by_fileset_id(record[:file_id])
         hyc_download_stat = HycDownloadStat.find_by(fileset_id: record[:file_id], date: record[:date].to_date.beginning_of_month)
-        
+
         expect(hyc_download_stat).to be_present
         expect(hyc_download_stat.fileset_id).to eq(record[:file_id])
         expect(hyc_download_stat.work_id).to eq(work_data[:work_id])
