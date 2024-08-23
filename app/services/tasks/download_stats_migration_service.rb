b891812099eb8591e3771f54e9e94485ac27b83b
David Campbell <davidcam-src@proton.me>
2024-08-23 13:29:11 -0400

    helper functions for test class, handling truncation outside of query to get around group by errors

--- app/services/tasks/download_stats_migration_service.rb
+++ app/services/tasks/download_stats_migration_service.rb
@@ -4,22 +4,37 @@ module Tasks
     PAGE_SIZE = 1000
     def list_record_info(output_path, after_timestamp = nil)
 
-      # Build the query
-      query = FileDownloadStat.select("id, file_id, DATE_TRUNC('month', date) AS date, SUM(downloads) AS downloads")
+    #   # Build the query
+    #   query = FileDownloadStat.select("file_id, DATE_TRUNC('month', date) AS date, SUM(downloads) AS downloads")
+    #   query = query.where('updated_at > ?', after_timestamp) if after_timestamp.present?
+
+    #   # Group by file_id and truncated month
+    #   query = query.group('file_id, DATE_TRUNC(\'month\', date)').order('file_id, date')
+      query = FileDownloadStat.all
       query = query.where('updated_at > ?', after_timestamp) if after_timestamp.present?
 
-      # Group by file_id and truncated month
-      query = query.group('id, file_id, date, downloads').order('file_id, date')
+    #   # Fetch the IDs in batches for memory efficiency and performance
+    #   records = []
+    #   query.in_batches(of: PAGE_SIZE, load: true) do |relation|
+    #     records.concat(relation.map { |record| { file_id: record.file_id, date: record.date, downloads: record.downloads } })
+    #   end
 
-      # Fetch the IDs in batches for memory efficiency and performance
+    # Fetch the records in batches to handle large datasets
       records = []
-      query.in_batches(of: PAGE_SIZE, load: true) do |relation|
-        records.concat(relation.map { |record| { file_id: record.file_id, date: record.date, downloads: record.downloads } })
+      query.find_each(batch_size: PAGE_SIZE) do |record|
+        records << {
+          file_id: record.file_id,
+          date: record.date,
+          downloads: record.downloads
+        }
       end
+
+    # Perform aggregation in Ruby
+      aggregated_records = aggregate_downloads(records)
 
       # Write the records to the specified CSV file
       CSV.open(output_path, 'w', write_headers: true, headers: ['file_id', 'date', 'downloads']) do |csv|
-        records.each do |record|
+        aggregated_records.each do |record|
           csv << [record[:file_id], record[:date], record[:downloads]]
         end
       end
@@ -38,7 +53,10 @@ module Tasks
     def create_hyc_download_stat(stat)
       puts "Inspect stat: #{stat.inspect}"
       puts "Stat file_id: #{stat[:file_id]}"
-      hyc_download_stat = HycDownloadStat.find_or_initialize_by(fileset_id: stat[:file_id].to_s)
+      hyc_download_stat = HycDownloadStat.find_or_initialize_by(
+        fileset_id: stat[:file_id].to_s,
+        date: stat[:date]
+    )
       work_data = work_data_from_stat(stat)
       hyc_download_stat.assign_attributes(
         fileset_id: stat[:file_id],
@@ -55,6 +73,24 @@ module Tasks
     # Memoization is not necessary here since this method is called per stat
     def work_data_from_stat(stat)
       WorkUtilsHelper.fetch_work_data_by_fileset_id(stat[:file_id])
+    end
+
+    def aggregate_downloads(records)
+      aggregated_data = {}
+
+      records.each do |record|
+        file_id = record[:file_id]
+        truncated_date = record[:date].beginning_of_month
+        downloads = record[:downloads]
+
+        # Group the file_id and truncated date to be used as a key
+        key = [file_id, truncated_date]
+        # Initialize the hash for the key if it doesn't exist
+        aggregated_data[key] ||= { file_id: file_id, date: truncated_date, downloads: 0 }
+        # Sum the downloads for each key
+        aggregated_data[key][:downloads] += downloads
+      end
+      aggregated_data.values
     end
   end
 end
--- spec/services/tasks/download_stats_migration_service_spec.rb
+++ spec/services/tasks/download_stats_migration_service_spec.rb
@@ -2,8 +2,22 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
-  let!(:file_download_stats) do
-    [
+  let(:admin_set_title) { 'Open_Access_Articles_and_Book_Chapters' }
+  let(:mock_admin_set) { FactoryBot.create(:solr_query_result, :admin_set, title_tesim: [admin_set_title]) }
+  let(:output_path) { Rails.root.join('tmp', 'download_migration_test_output.csv') }
+  # let(:csv_data) { CSV.read(output_path, headers: true) }
+  # let(:output_records) { csv_data.map { |row| row.to_h.symbolize_keys } }
+  let(:service) { described_class.new }
+
+  before do
+    # Ensure the output file is removed before each test
+    File.delete(output_path) if File.exist?(output_path)
+    allow(ActiveFedora::SolrService).to receive(:get).with("title_tesim:#{admin_set_title}", rows: 1).and_return('response' => { 'docs' => [mock_admin_set] })
+  end
+
+  describe '#list_record_info' do
+    before do
+      @file_download_stats = [
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 1)),
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 15)),
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 30)),
@@ -13,39 +27,10 @@ RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 1)),
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 15)),
         FactoryBot.create(:file_download_stat, date: Date.new(2023, 12, 30))
-    ]
-  end
-  let(:admin_set_title) { 'Open_Access_Articles_and_Book_Chapters' }
-  let!(:mock_admin_set) { FactoryBot.create(:solr_query_result) }
-  # Generate works for the each file_download_stat
-  let!(:mock_works) { file_download_stats.map { |stat| FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id]) } }
-  let(:output_path) { Rails.root.join('tmp', 'download_migration_test_output.csv') }
-  let(:csv_data) { CSV.read(output_path, headers: true) }
-  let(:output_records) { csv_data.map { |row| row.to_h.symbolize_keys } }
-  let(:service) { described_class.new }
-
-
-  before do
-    # Ensure the output file is removed before each test
-    File.delete(output_path) if File.exist?(output_path)
-    allow(ActiveFedora::SolrService).to receive(:get).with("title_tesim:#{admin_set_title}", rows: 1).and_return('response' => { 'docs' => [mock_admin_set] })
-    file_download_stats.each_with_index do |stat, index|
-      # Assign a random number of downloads to each stat
-      puts "Stat file_id: #{stat.file_id}"
-      allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [mock_works[index]] })
-    end
-  end
-
-  describe '#list_record_info' do
-    # Helper method to convert an array of FileDownloadStat objects to an array of hashes
-    # Checks for truncated date to the beginning of the month
-    def expected_records_for(stats)
-      stats.map do |stat|
-        {
-          file_id: stat.file_id,
-          date: stat.date.beginning_of_month.to_s,
-          downloads: stat.downloads.to_s,
-        }
+      ]
+      @mock_works = @file_download_stats.map { |stat| FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id]) }
+      @file_download_stats.each_with_index do |stat, index|
+        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [@mock_works[index]] })
       end
     end
 
@@ -53,7 +38,7 @@ RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
       service.list_record_info(output_path, nil)
 
       expect(File).to exist(output_path)
-      expect(output_records).to match_array(expected_records_for(file_download_stats))
+      expect(csv_to_hash_array(output_path)).to match_array(expected_records_for(@file_download_stats))
     end
 
     context 'with an after_timestamp' do
@@ -65,30 +50,78 @@ RSpec.describe Tasks::DownloadStatsMigrationService, type: :service do
         service.list_record_info(output_path, after_timestamp)
 
         expect(File).to exist(output_path)
-        expect(output_records).not_to include(expected_records_for([old_stat]).first)
-        expect(output_records).to match_array(expected_records_for(recent_stats))
+        expect(csv_to_hash_array(output_path)).not_to include(expected_records_for([old_stat]).first)
+        expect(csv_to_hash_array(output_path)).to match_array(expected_records_for(recent_stats))
       end
     end
   end
 
   describe '#migrate_to_new_table' do
     before do
+        # Smaller groups to enable easier testing for aggregation of download stats from daily to monthly
+      @file_download_stats = [[
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 15), downloads: 10, file_id: 'file_id_1'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 1, 30), downloads: 10, file_id: 'file_id_1'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 15), downloads: 10, file_id: 'file_id_1'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 3, 30), downloads: 10, file_id: 'file_id_1'),
+      ],
+      [
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 15), downloads: 10, file_id: 'file_id_2'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 4, 30), downloads: 10, file_id: 'file_id_2'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 15), downloads: 10, file_id: 'file_id_2'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 5, 30), downloads: 10, file_id: 'file_id_2'),
+      ],
+      [
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 15), downloads: 10, file_id: 'file_id_3'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 6, 30), downloads: 10, file_id: 'file_id_3'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 15), downloads: 10, file_id: 'file_id_3'),
+        FactoryBot.create(:file_download_stat, date: Date.new(2023, 7, 30), downloads: 10, file_id: 'file_id_3'),
+      ]]
+      @mock_works = @file_download_stats.flatten.map do |stat|
+        FactoryBot.create(:solr_query_result, :work, file_set_ids_ssim: [stat.file_id])
+      end
+      @file_download_stats.flatten.each_with_index do |stat, index|
+        allow(ActiveFedora::SolrService).to receive(:get).with("file_set_ids_ssim:#{stat.file_id}", rows: 1).and_return('response' => { 'docs' => [@mock_works[index]] })
+      end
       service.list_record_info(output_path, nil)
       service.migrate_to_new_table(output_path)
     end
 
     it 'creates new HycDownloadStat records from the CSV file' do
-      output_records.each_with_index do |record,index|
-        # WIP: Break at index 5
-        if index == 5
-          break
-        end
-        puts "Record: #{record.inspect}"
-        hyc_download_stat = HycDownloadStat.find(record[:id])
-        expect(hyc_download_stat.date.to_s).to eq(record[:date])
-        expect(hyc_download_stat.downloads).to eq(record[:downloads].to_i)
-        expect(hyc_download_stat.file_id).to eq(record[:file_id])
+      csv_to_hash_array(output_path).each_with_index do |record, index|
+        # puts 'All HycDownloadStat records:'
+        # HycDownloadStat.all.each do |stat|
+        #   puts "ID: #{stat.id}, File ID: #{stat.fileset_id}, Date: #{stat.date}, Downloads: #{stat.download_count}"
+        # end
+        # puts "Record: #{record.inspect}"
+        # hyc_download_stat = HycDownloadStat.find(record[:id])
+        work_data = WorkUtilsHelper.fetch_work_data_by_fileset_id(record[:file_id])
+        hyc_download_stat = HycDownloadStat.find_by(fileset_id: record[:file_id], date: record[:date].to_date.beginning_of_month)
+        
+        expect(hyc_download_stat).to be_present
+        expect(hyc_download_stat.fileset_id).to eq(record[:file_id])
+        expect(hyc_download_stat.work_id).to eq(work_data[:work_id])
+        expect(hyc_download_stat.date).to eq(record[:date].to_date)
+        # Each mocked record has 10 downloads per month, so the download count should be 20
+        expect(hyc_download_stat.download_count).to eq(20)
       end
+    end
+  end
+
+  private
+  def csv_to_hash_array(file_path)
+    CSV.read(file_path, headers: true).map { |row| row.to_h.symbolize_keys }
+  end
+
+  # Helper method to convert an array of FileDownloadStat objects to an array of hashes
+  # Checks for truncated date to the beginning of the month
+  def expected_records_for(stats)
+    stats.map do |stat|
+      {
+        file_id: stat.file_id,
+        date: stat.date.beginning_of_month.to_s,
+        downloads: stat.downloads.to_s,
+      }
     end
   end
 end
