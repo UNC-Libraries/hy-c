dd3e82485daa537eb50f5e28a525b6dcb97cf455
David Campbell <davidcam-src@proton.me>
2024-10-07 14:17:13 -0400

    allow html rendering for text pasted in submission fields

--- app/overrides/renderers/hyrax/renderers/attribute_renderer_override.rb
+++ app/overrides/renderers/hyrax/renderers/attribute_renderer_override.rb
@@ -17,6 +17,22 @@ Hyrax::Renderers::AttributeRenderer.class_eval do
 
   private
 
+  def attribute_value_to_html(value)
+    # Define allowed tags and attributes
+    allowed_tags = %w[strong em b i u p br small mark sub sup a ul ol li dl dt dd div span h1 h2 h3 h4 h5 h6 img]
+    allowed_attributes = %w[href]
+
+    # Sanitize the value, allowing only safe HTML tags and attributes
+    # Allow for rendering of text as html for the sanitized value
+    safe_value = sanitize(value, tags: allowed_tags, attributes: allowed_attributes).html_safe
+
+    if microdata_value_attributes(field).present?
+      "<span#{html_attributes(microdata_value_attributes(field))}>#{safe_value}</span>"
+    else
+      li_value(value)
+    end
+  end
+
   def li_value(value)
     field_value = find_language(value) || value
     auto_link(ERB::Util.h(field_value))
