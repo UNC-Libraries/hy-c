a1e46ae494408a49d5d4f5eba24b7b316af6f12e
Ben Pennell <bbpennel@email.unc.edu>
2024-10-11 16:25:29 -0400

    Update docker compose and startup scripts so that web and sidekiq don't share gems anymore (symlinking didn't work across containers), get solr working again

--- docker/start-app.sh
+++ docker/start-app.sh
@@ -5,16 +5,23 @@ source scl_source enable devtoolset-8
 
 mkdir -p /opt/hyrax/log/
 echo "#### Ensure rubygems system is up to date before bundle installing"
-# gem install rubygems-update
-# update_rubygems  >> /dev/null
-
-echo "#### Bundle install"
-bundle check || bundle install
+gem install rubygems-update -v 3.4.20
+update_rubygems >> /dev/null
 
 echo "#### Performing config steps"
 # The bundle config and package are needed for the odd way we manage gems in production
 bundle config --local cache_path /hyc-gems
-bundle package
+
+if ! bundle check; then
+  echo "#### Bundle install required"
+  bundle install
+  bundle package
+  echo "#### Creating symlink for libsass otherwise bundle cannot find it"
+  [ ! -L /opt/rh/rh-ruby30/root/usr/share/gems/gems/sassc-2.4.0/ext/libsass.so ] && ln -s /opt/rh/rh-ruby30/root/usr/lib64/gems/ruby/sassc-2.4.0/sassc/libsass.so /opt/rh/rh-ruby30/root/usr/share/gems/gems/sassc-2.4.0/ext/libsass.so
+else
+  echo "#### Gems already installed, skipping bundle install"
+fi
+
 find . -name *.pid -delete
 bundle exec rake tmp:cache:clear
 bundle exec rake db:create && bundle exec rake db:migrate
--- docker/start-sidekiq.sh
+++ docker/start-sidekiq.sh
@@ -3,27 +3,22 @@ echo "#### Running start-sidekiq.sh"
 source scl_source enable rh-ruby30
 source scl_source enable devtoolset-8
 
-# Wait for the web container, so that it can handle installation of bundle gems for both
-echo "#### Waiting for web application to become available"
-ATTEMPT_COUNTER=0
-MAX_ATTEMPTS=100
-SERVICE_URL=http://web:3000/
-
-until $(curl --output /dev/null --silent --head --fail $SERVICE_URL); do
-    if [ ${ATTEMPT_COUNTER} -eq ${MAX_ATTEMPTS} ];then
-      echo "Max attempts to connect to web application reached, cannot start sidekiq"
-      exit 1
-    fi
-
-    printf '.'
-    ATTEMPT_COUNTER=$(($ATTEMPT_COUNTER+1))
-    sleep 5
-done
-
 echo "#### Ensure rubygems system is up to date before bundle installing"
 gem install rubygems-update -v 3.4.20
 update_rubygems >> /dev/null
 
+bundle config --local cache_path /hyc-gems
+
+if ! bundle check; then
+  echo "#### Bundle install required"
+  bundle install
+  bundle package
+  echo "#### Creating symlink for libsass otherwise bundle cannot find it"
+  [ ! -L /opt/rh/rh-ruby30/root/usr/share/gems/gems/sassc-2.4.0/ext/libsass.so ] && ln -s /opt/rh/rh-ruby30/root/usr/lib64/gems/ruby/sassc-2.4.0/sassc/libsass.so /opt/rh/rh-ruby30/root/usr/share/gems/gems/sassc-2.4.0/ext/libsass.so
+else
+  echo "#### Gems already installed, skipping bundle install"
+fi
+
 echo "#### Starting sidekiq"
-bundle && bundle exec sidekiq
+bundle exec sidekiq
 echo "#### Shutdown sidekiq"
