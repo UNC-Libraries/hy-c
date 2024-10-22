5219bbd745ccc1ec61c4f8d988f706cfb2bff70f
David Campbell <102170536+davidcam-src@users.noreply.github.com>
2024-10-22 12:56:16 -0400

    HYC-1003 - Text Formatting for Abstracts (#1123)
    * save tinymce content to textarea
    
    * tinymce saves on change
    
    * validate metadata on change for rich text fields
    
    * assert tinymce content in test

---
+++ app/renderers/hyrax/renderers/formatted_text_renderer.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+module Hyrax
+  module Renderers
+    class FormattedTextRenderer < AttributeRenderer
+      private
+      def attribute_value_to_html(value)
+        sanitized_value = get_sanitized_string(value)
+        if microdata_value_attributes(field).present?
+          "<span#{html_attributes(microdata_value_attributes(field))}>#{sanitized_value}</span>"
+        else
+          li_value(sanitized_value)
+        end
+      end
+
+      # Sanitize the value, allowing only safe HTML tags and attributes
+      def get_sanitized_string(string)
+         # Define allowed tags and attributes
+        allowed_tags = %w[strong em b i u p br small mark sub sup a ul ol li dl dt dd div span h1 h2 h3 h4 h5 h6]
+        allowed_attributes = %w[href]
+        sanitize(string, tags: allowed_tags, attributes: allowed_attributes)
+      end
+
+    # Same as attribute renderer override, but without escaping the value
+      def li_value(value)
+        field_value = find_language(value) || value
+        auto_link((field_value))
+      end
+    end
+  end
+end
