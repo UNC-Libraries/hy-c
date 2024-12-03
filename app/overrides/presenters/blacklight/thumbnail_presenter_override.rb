b7a021913f3e1db7f92548ad36bf26bdb3465fa9
David Campbell <davidcam-src@proton.me>
2024-12-03 14:31:13 -0500

    removing unnecessary  nil checks

--- app/overrides/presenters/blacklight/thumbnail_presenter_override.rb
+++ app/overrides/presenters/blacklight/thumbnail_presenter_override.rb
@@ -4,21 +4,20 @@ module Blacklight
     private
     # [hyc-override] Retrieve the thumbnail of a document's first file_set instead of using the default if it exists
     def retrieve_values(field_config)
-      # Extract the SolrDocument from the document if it's nested
-      solr_doc = extract_solr_document(document)
-      unless solr_doc
-        Rails.logger.warn("Could not extract SolrDocument for document with id #{document.id}")
+      # Return the default thumbnail if the solr document is nil
+      unless document
         return FieldRetriever.new(document, field_config, view_context).fetch
       end
 
-     # Convert SolrDocument to a mutable hash
+      # Extract the SolrDocument from the document object if it's nested and convert it to a mutable hash
+      solr_doc = extract_solr_document(document)
       document_hash = solr_doc.to_h
 
       # Update the `thumbnail_path_ss` dynamically if needed
       if needs_thumbnail_path_update?(document_hash)
         file_set_id = document_hash['file_set_ids_ssim']&.first
         document_hash['thumbnail_path_ss'] = "/downloads/#{file_set_id}?file=thumbnail"
-        Rails.logger.info("Updated thumbnail_path_ss: #{document_hash['thumbnail_path_ss']} for document with id #{document.id}")
+        Rails.logger.info("Updated thumbnail_path_ss: #{document_hash['thumbnail_path_ss']} for work with id #{document.id}")
         # Create a temporary SolrDocument from the updated hash
         updated_document = SolrDocument.new(document_hash)
         FieldRetriever.new(updated_document, field_config, view_context).fetch
@@ -30,18 +29,10 @@ module Blacklight
     # Extract the SolrDocument from the document if it's nested
     # Prevents errors when the document is a presenter on work show pages
     def extract_solr_document(doc)
-      unless doc
-        Rails.logger.warn("Attempted to extract SolrDocument but document is nil for document with id #{doc.id}")
-        return nil
-      end
-
       if doc.is_a?(SolrDocument)
         doc
       elsif doc.respond_to?(:solr_document) && doc.solr_document.is_a?(SolrDocument)
         doc.solr_document
-      else
-        Rails.logger.warn("Unrecognized document type: #{doc.class} for document with id #{doc.id}")
-        nil
       end
     end
 
