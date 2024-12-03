757e287626cc9d090840767642ebf81dfe9880a3
David Campbell <davidcam-src@proton.me>
2024-12-03 16:38:36 -0500

    update thumbnail test

--- spec/presenters/blacklight/thumbnail_presenter_spec.rb
+++ spec/presenters/blacklight/thumbnail_presenter_spec.rb
@@ -20,5 +20,30 @@ RSpec.describe Blacklight::ThumbnailPresenter do
       # Presenter should not process the document if it's nil
       expect(presenter).not_to have_received(:extract_solr_document)
     end
+
+    it 'updates the thumbnail_path_ss if it needs an update' do
+      field_config = double('FieldConfig')
+      view_context = double('ViewContext')
+      retriever_instance = Blacklight::FieldRetriever.new(nil, nil, nil)
+      document_hash = {
+        'thumbnail_path_ss' => '/assets/work-default.png',
+        'file_set_ids_ssim' => ['file_set_1'],
+        'id' => '1'
+      }
+      solr_doc = SolrDocument.new(document_hash)
+      presenter = Blacklight::ThumbnailPresenter.new(solr_doc, view_context, field_config)
+
+      allow(presenter).to receive(:needs_thumbnail_path_update?).and_return(true)
+      allow(SolrDocument).to receive(:new).and_return(instance_double(SolrDocument))
+      allow(Blacklight::FieldRetriever).to receive(:new).and_return(retriever_instance)
+      allow(retriever_instance).to receive(:fetch).and_return('updated_thumbnail')
+      allow(Rails.logger).to receive(:info)
+
+      presenter.send(:retrieve_values, field_config)
+      expect(Rails.logger).to have_received(:info).with('Updated thumbnail_path_ss: /downloads/file_set_1?file=thumbnail for work with id 1')
+      expect(SolrDocument).to have_received(:new).with(
+        hash_including('thumbnail_path_ss' => '/downloads/file_set_1?file=thumbnail')
+      )
+    end
   end
 end
