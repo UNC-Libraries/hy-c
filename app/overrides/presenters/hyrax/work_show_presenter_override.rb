12249263585271913373b5aed418c0ca22915c59
David Campbell <davidcam-src@proton.me>
2024-12-05 10:33:34 -0500

    shared delegates module

--- app/overrides/presenters/hyrax/work_show_presenter_override.rb
+++ app/overrides/presenters/hyrax/work_show_presenter_override.rb
@@ -2,11 +2,7 @@
 # [hyc-override] Overriding helper in order to add doi to citation
 # https://github.com/samvera/hyrax/blob/hyrax-v4.0.0/app/presenters/hyrax/work_show_presenter.rb
 Hyrax::WorkShowPresenter.class_eval do
-  # delegating just :doi seems to exclude the other fields, so pull all fields in from original file
-  delegate :title, :date_created, :date_issued, :description, :doi, :creator, :place_of_publication,
-           :creator_display, :contributor, :subject, :publisher, :language, :embargo_release_date,
-           :lease_expiration_date, :license, :source, :rights_statement, :thumbnail_id, :representative_id,
-           :rendering_ids, :member_of_collection_ids, :bibliographic_citation, :alternative_title, :member_ids, to: :solr_document
+  include Hyrax::SharedDelegates
 
   # [hyc-override] Add default scholarly? method
   # Indicates if the work is considered scholarly according to google scholar
---
+++ app/presenters/hyrax/shared_delegates.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+module Hyrax
+  module SharedDelegates
+    extend ActiveSupport::Concern
+    included do
+      delegate :abstract, :admin_note, :alternative_title, :bibliographic_citation, :contributor, :contributor_display, :copyright_date,
+             :creator, :creator_display, :date_created, :date_issued, :dcmi_type, :deposit_record, :description, :doi, :embargo_release_date,
+             :extent, :funder, :human_readable_type, :identifier, :itemtype, :kind_of_data, :language, :language_label, :last_modified_date,
+             :lease_expiration_date, :license, :license_label, :member_ids, :member_of_collection_ids, :methodology, :note, :orcid_label,
+             :other_affiliation_label, :place_of_publication, :project_director_display, :publisher, :related_url, :rendering_ids, :representative_id, :researcher_display,
+             :resource_type, :rights_holder, :rights_notes, :rights_statement, :rights_statement_label, :sponsor, :source, :subject,
+             :thumbnail_id, :title, to: :solr_document
+    end
+  end
+  end
