a43dd79058debba96865062cff4eb8bd0a388f3a
David Campbell <davidcam-src@proton.me>
2025-01-30 10:54:49 -0500

    notifications

--- app/services/hyrax/workflow/dataset_deposited_notification.rb
+++ app/services/hyrax/workflow/dataset_deposited_notification.rb
@@ -1,29 +1,23 @@
-# app/services/hyrax/workflow/dataset_deposited_notification.rb
+# frozen_string_literal: true
 module Hyrax
     module Workflow
-        class DatasetDepositedNotification < AbstractNotification
-         private
-        
-         def subject
+      class DatasetDepositedNotification < AbstractNotification
+        private
+  
+        def subject
             I18n.t('hyrax.notifications.workflow.dataset_deposited.subject')
-         end
-
-         def message
-            I18n.t('hyrax.notifications.workflow.dataset_deposited.message', title: title, link: (link_to work_id, document_path))
-          end
-
-        # WIP: Depositor + Super
-        # Notify users when a dataset is deposited
-          def users_to_notify
-            # return [] unless dataset?
-
-            user_key = ActiveFedora::Base.find(work_id).depositor
-            super << ::User.find_by(uid: user_key)
-          end
-
-          def dataset?
-            entity.resource_type&.include?(I18n.t('activefedora.models.data_set'))
-          end
         end
+  
+        def message
+            I18n.t('hyrax.notifications.workflow.dataset_deposited.message', title: title, link: (link_to work_id, document_path))
+        end
+  
+        def users_to_notify
+          user_key = ActiveFedora::Base.find(work_id).depositor
+          # Only returning the depositor, excluding the default users provided by parent class
+          [::User.find_by(uid: user_key)]
+        end
+      end
     end
-end
+  end
+  
