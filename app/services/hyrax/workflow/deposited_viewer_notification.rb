4b2b9da29d7294262d854e6ebfd1b981c7ab699b
David Campbell <davidcam-src@proton.me>
2025-02-04 11:28:24 -0500

    viewer notifications

---
+++ app/services/hyrax/workflow/deposited_viewer_notification.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module Hyrax
+    module Workflow
+      # This notification service was created to allow some admin set managers to get notifications.
+      # Using the default DepositedNotificaiton class for this would send deposit notifications
+      # to all managers in all workflows instead of just in the manager-specific workflow.
+      class DepositedViewerNotification < AbstractNotification
+        private
+  
+        def subject
+          I18n.t('hyrax.notifications.workflow.deposited_viewer.subject')
+        end
+  
+        def message
+          I18n.t('hyrax.notifications.workflow.deposited_viewer.message', title: title, link: (link_to work_id, document_path))
+        end
+  
+        def users_to_notify
+          super
+        end
+      end
+    end
+  end
+  
