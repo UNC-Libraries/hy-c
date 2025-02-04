1248387c7e2525c2e2a76de92a8c5215655adfed
David Campbell <davidcam-src@proton.me>
2025-02-04 15:50:18 -0500

    deposited viewer notif test

---
+++ spec/services/hyrax/workflow/deposited_viewer_notification_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Hyrax::Workflow::DepositedViewerNotification do
+  let(:approver) { FactoryBot.create(:admin) }
+  let(:depositor) { FactoryBot.create(:user) }
+  let(:viewer) { FactoryBot.create(:user) }
+
+  let(:work) { Article.create(title: ['New Article'], depositor: depositor.user_key) }
+  let(:admin_set) do
+    AdminSet.create(title: ['article admin set'],
+                    description: ['some description'],
+                    edit_users: [depositor.user_key])
+  end
+  let(:permission_template) do
+    Hyrax::PermissionTemplate.create!(source_id: admin_set.id)
+  end
+  let(:workflow) do
+    Sipity::Workflow.create(name: 'test', allows_access_grant: true, active: true,
+                            permission_template_id: permission_template.id)
+  end
+  let(:entity) { Sipity::Entity.create(proxy_for_global_id: work.to_global_id.to_s, workflow_id: workflow.id) }
+  let(:comment) { double('comment', comment: 'A pleasant read') }
+
+  describe '.send_notification' do
+    it 'sends a message to depositor and viewer' do
+      recipients = {}
+      expect(approver).to receive(:send_message)
+      .with(anything, I18n.t('hyrax.notifications.workflow.deposited_viewer.message', title: work.title[0],
+                                                                                        link: "<a href=\"#{ENV['HYRAX_HOST']}/concern/articles/#{work.id}\">#{work.id}</a>",
+                                                                                        user: approver, comment: comment.comment),
+              anything).exactly(2).times.and_call_original
+
+      expect { described_class.send_notification(entity: entity, user: approver, comment: comment, recipients: recipients) }
+        .to change { depositor.mailbox.inbox.count }.by(1)
+        .and change { viewer.mailbox.inbox.count }.by(1)
+    end
+  end
+end
