e08e18e383c7c9f62a50931b5cb3b19f7d42f873
David Campbell <davidcam-src@proton.me>
2025-02-17 10:24:25 -0500

    users and roles query

--- app/services/hyrax/workflow/deposited_viewer_notification.rb
+++ app/services/hyrax/workflow/deposited_viewer_notification.rb
@@ -23,7 +23,7 @@ module Hyrax
         # WIP: Users and groups has to be changed to a query that fetches info related to users and groups in an admin set instead of a workflow
         # Rails.logger.info("NOTIF 2 - Admin Set Name: #{admin_set_name}, Admin Set ID: #{admin_set_id}")
 
-        users_and_roles_query = ActiveRecord::Base.connection.execute("SELECT u.id AS user_id, u.email, r.name AS group_name, pta.access AS admin_set_role
+        groups_and_roles_query = ActiveRecord::Base.connection.execute("SELECT u.id AS user_id, u.email, r.name AS group_name, pta.access AS admin_set_role
                     FROM users u  
                     JOIN roles_users ru ON u.id = ru.user_id
                     JOIN roles r ON ru.role_id = r.id
@@ -32,26 +32,51 @@ module Hyrax
                         SELECT id FROM permission_templates WHERE source_id = '#{admin_set_id}'
                     )")
 
+        users_and_roles_query = ActiveRecord::Base.connection.execute("SELECT u.id, u.email, pta.access AS admin_set_role
+                    FROM users u
+                    JOIN permission_template_accesses pta ON u.uid = pta.agent_id
+                    WHERE pta.permission_template_id = (
+                        SELECT id FROM permission_templates WHERE source_id = '#{admin_set_id}'
+                    ) 
+                    AND pta.agent_type = 'user';"
+        )
+        
+
 
         Rails.logger.info("NOTIF 2 - QUERY INSPECT 1")
+        groups_and_roles_query.each do |query_result|
+          Rails.logger.info("RESULT INSPECT #{query_result.inspect}")
+        end
+
         users_and_roles_query.each do |query_result|
           Rails.logger.info("RESULT INSPECT #{query_result.inspect}")
         end
 
+        groups_and_roles = groups_and_roles_query.map { |row| row.symbolize_keys }
         users_and_roles = users_and_roles_query.map { |row| row.symbolize_keys }
-        Rails.logger.info("NOTIF 2 - QUERY INSPECT: #{users_and_roles.inspect}")
+        # Rails.logger.info("NOTIF 2 - QUERY INSPECT: #{groups_and_roles.inspect}")
 
-        Rails.logger.info("NOTIF 2 - QUERY RESULTS")
-        users_and_roles.each do |query_result|
+        Rails.logger.info("NOTIF 2 - GROUP QUERY RESULTS")
+        groups_and_roles.each do |query_result|
           puts "User ID: #{query_result[:user_id]}, Email: #{query_result[:email]}, Group: #{query_result[:group_name]}, Admin Set Role: #{query_result[:admin_set_role]}"
+        end
+
+        Rails.logger.info("NOTIF 2 - USER QUERY RESULTS")
+        users_and_roles.each do |query_result|
+          puts "User ID: #{query_result[:id]}, Email: #{query_result[:email]}, Admin Set Role: #{query_result[:admin_set_role]}"
         end
         
         # Map [user_id, group_name, admin_set_role] -> [user_id, {role_name => count}]
-        user_role_map = users_and_roles.each_with_object({}) do |query_result, h|
+        user_role_map = groups_and_roles.each_with_object({}) do |query_result, h|
           user_id = query_result[:user_id].to_i
           h[user_id] ||= { 'view' => 0, 'manage' => 0 }
           h[user_id][query_result[:admin_set_role]] += 1
+        end
 
+        users_and_roles.each do |query_result| 
+          user_id = query_result[:id].to_i
+          user_role_map[user_id] ||= { 'view' => 0, 'manage' => 0 }
+          user_role_map[user_id][query_result[:admin_set_role]] += 1
         end
         
       
@@ -71,7 +96,7 @@ module Hyrax
           managing_count = count['manage']
           send_notification = viewing_count > managing_count
           
-          Rails.logger.info "User: #{k}, Roles: #{count.to_a}"
+          Rails.logger.info "User: #{id}, Roles: #{count.to_a}"
           Rails.logger.info "LOG VIEWING/MANAGING COUNT - User: #{id}, Viewing Count: #{viewing_count}, Managing Count: #{managing_count}, send_notification: #{send_notification}"
         end
 
