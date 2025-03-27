907595764cda8070b1307440312b8ec08b053e89
David Campbell <davidcam-src@proton.me>
2025-03-27 15:43:46 -0400

    access work id from hash

--- app/services/tasks/pubmed_ingest_service.rb
+++ app/services/tasks/pubmed_ingest_service.rb
@@ -6,7 +6,7 @@ module Tasks
         puts "PubmedIngestService attach_pubmed_pdf with work_type: #{work_hash[:work_type]} and work_id: #{work_hash[:work_id]}"
         # WIP: A bit unsure if I'm using this properly
         model_class = work_hash[:work_type].constantize
-        work = model_class.find(work_id)
+        work = model_class.find(work_hash[:work_id])
         puts "Article inspect #{work.inspect}"
         # attach_file_set_to_work(work: work, file_path: file_path, user: depositor, visibility: visibility)
     end
