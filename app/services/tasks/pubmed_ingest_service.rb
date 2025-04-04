fac4df7f2a1e94f66f549b208bcb756700166065
David Campbell <davidcam-src@proton.me>
2025-04-04 11:49:48 -0400

    depositor onyen instead of depositor

--- app/services/tasks/pubmed_ingest_service.rb
+++ app/services/tasks/pubmed_ingest_service.rb
@@ -2,9 +2,10 @@
 module Tasks
     class PubmedIngestService
     include Tasks::IngestHelper
-    def attach_pubmed_pdf(work_hash, file_path, depositor, visibility)
+    def attach_pubmed_pdf(work_hash, file_path, depositor_onyen, visibility)
         model_class = work_hash[:work_type].constantize
         work = model_class.find(work_hash[:work_id])
+        depositor =  User.find_by(uid: depositor_onyen)
         pdf_file = attach_pdf_to_work(work, file_path, depositor, visibility)
         admin_set = ::AdminSet.where(id: work_hash[:admin_set_id]).first
         pdf_file.update(permissions_attributes: group_permissions(admin_set))
