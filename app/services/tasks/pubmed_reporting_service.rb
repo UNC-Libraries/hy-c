8bed45df5d0677a76cf32e4dab42b08c23600e34
David Campbell <davidcam-src@proton.me>
2025-04-22 16:46:08 -0400

    time formatting

--- app/services/tasks/pubmed_reporting_service.rb
+++ app/services/tasks/pubmed_reporting_service.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 module Tasks
-    class PubmedReportingService
-        def  self.generate_report(ingest_output)
-           { 
-                subject: "Pubmed Ingest Report for #{ingest_output['time']}",
-                headers: {
-                    reporting_message: "Reporting publications from Pubmed Ingest on #{ingest_output['time']}",
-                    depositor: "#{ingest_output['depositor']}",
-                    total_unique_files: "#{ingest_output['counts']['total_unique_files']}",
-                    target_directory: "#{ingest_output['directory_or_csv']}",
-                    successfully_attached: "#{ingest_output['counts']['successfully_attached']}"
-                }, 
-                records: {
-                    successfully_attached: ingest_output['successfully_attached'], 
-                    successfully_ingested: ingest_output['successfully_ingested'],
-                    skipped: ingest_output['skipped'], 
-                    failed: ingest_output['failed']
-                },
-            }
-        end
+  class PubmedReportingService
+    def  self.generate_report(ingest_output)
+      formatted_time = Time.parse(ingest_output['time']).strftime('%B %d, %Y at %I:%M %p %Z')
+      {
+           subject: "Pubmed Ingest Report for #{formatted_time}",
+           headers: {
+               reporting_message: "Reporting publications from Pubmed Ingest on #{formatted_time}",
+               depositor: "#{ingest_output['depositor']}",
+               total_unique_files: "#{ingest_output['counts']['total_unique_files']}",
+               target_directory: "#{ingest_output['directory_or_csv']}",
+               successfully_attached: "#{ingest_output['counts']['successfully_attached']}"
+           },
+           records: {
+               successfully_attached: ingest_output['successfully_attached'],
+               successfully_ingested: ingest_output['successfully_ingested'],
+               skipped: ingest_output['skipped'],
+               failed: ingest_output['failed']
+           },
+       }
     end
+  end
 end
