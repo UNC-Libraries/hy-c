71c0cd585206df579cdf9206bcd3fd4a99fba887
Ben Pennell <bbpennel@email.unc.edu>
2025-05-08 10:14:25 -0400

    Remove turnstile from downloads (#1168)

--- app/controllers/bot_detect_controller.rb
+++ app/controllers/bot_detect_controller.rb
@@ -145,7 +145,6 @@ class BotDetectController < ApplicationController
     query_parameters = controller.request.query_parameters
     user_agent = controller.request.user_agent.to_s.downcase
     controller.is_a?(Hyrax::StatsController) \
-        || (controller.is_a?(Hyrax::DownloadsController) && query_parameters['file'] != 'thumbnail' && !user_agent.include?('googlebot')) \
         || query_parameters.key?('f') || query_parameters.key?('f_inclusive') || query_parameters.key?('clause') \
         || query_parameters.key?('range') || query_parameters.key?('page')
   end
