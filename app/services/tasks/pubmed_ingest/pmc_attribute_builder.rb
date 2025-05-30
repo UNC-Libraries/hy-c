331576dc26945c167ac20c1169a9e5973921cb6f
David Campbell <davidcam-src@proton.me>
2025-05-30 11:38:19 -0400

    update pmc attribute builder tests

--- app/services/tasks/pubmed_ingest/pmc_attribute_builder.rb
+++ app/services/tasks/pubmed_ingest/pmc_attribute_builder.rb
@@ -9,6 +9,8 @@ module Tasks
         new_pubmed_works.find { |row| row['pmid'] == pmid || row['pmcid'] == pmcid }
       end
 
+      private
+
       def generate_authors
         metadata.xpath('front/article-meta/contrib-group/contrib[@contrib-type="author"]').map.with_index do |author, i|
           res = {
@@ -19,38 +21,6 @@ module Tasks
           retrieve_author_affiliations(res, author)
           res
         end
-      end
-
-      def set_identifiers
-        article.identifier = format_publication_identifiers
-        article.issn = [metadata.xpath('front/journal-meta/issn[@pub-type="epub"]').text]
-      end
-
-      def set_journal_attributes
-        article.journal_title = metadata.at_xpath('front/journal-meta/journal-title-group/journal-title')&.text.presence
-        article.journal_volume = metadata.at_xpath('front/article-meta/volume')&.text.presence
-        article.journal_issue = metadata.at_xpath('front/article-meta/issue-id')&.text.presence
-        article.page_start     = metadata.at_xpath('front/article-meta/fpage')&.text.presence
-        article.page_end       = metadata.at_xpath('front/article-meta/lpage')&.text.presence
-      end
-
-      def apply_additional_basic_attributes
-        article.title = [metadata.xpath('front/article-meta/title-group/article-title').text]
-        article.abstract = [metadata.xpath('front/article-meta/abstract').text]
-        article.date_issued = get_date_issued
-        article.publisher = [metadata.at_xpath('front/journal-meta/publisher/publisher-name')&.text].compact.presence
-        article.keyword = metadata.xpath('//kwd-group/kwd').map(&:text)
-        article.funder = metadata.xpath('//funding-source/institution-wrap/institution').map(&:text)
-      end
-
-      private
-
-      def get_date_issued
-        pubdate = metadata.at_xpath('front/article-meta/pub-date[@pub-type="epub"]')
-        year  = pubdate&.at_xpath('year')&.text
-        month = pubdate&.at_xpath('month')&.text || 1
-        day   = pubdate&.at_xpath('day')&.text || 1
-        DateTime.new(year.to_i, month.to_i, day.to_i).strftime('%Y-%m-%d')
       end
 
       def retrieve_author_affiliations(hash, author)
@@ -75,6 +45,28 @@ module Tasks
         hash['other_affiliation'] = unc_affiliation.presence || affiliations[0].presence || ''
       end
 
+      def apply_additional_basic_attributes
+        article.title = [metadata.xpath('front/article-meta/title-group/article-title').text]
+        article.abstract = [metadata.xpath('front/article-meta/abstract').text]
+        article.date_issued = get_date_issued
+        article.publisher = [metadata.at_xpath('front/journal-meta/publisher/publisher-name')&.text].compact.presence
+        article.keyword = metadata.xpath('//kwd-group/kwd').map(&:text)
+        article.funder = metadata.xpath('//funding-source/institution-wrap/institution').map(&:text)
+      end
+
+      def get_date_issued
+        pubdate = metadata.at_xpath('front/article-meta/pub-date[@pub-type="epub"]')
+        year  = pubdate&.at_xpath('year')&.text
+        month = pubdate&.at_xpath('month')&.text || 1
+        day   = pubdate&.at_xpath('day')&.text || 1
+        DateTime.new(year.to_i, month.to_i, day.to_i).strftime('%Y-%m-%d')
+      end
+
+      def set_identifiers
+        article.identifier = format_publication_identifiers
+        article.issn = [metadata.xpath('front/journal-meta/issn[@pub-type="epub"]').text]
+      end
+
       def format_publication_identifiers
         article_meta = metadata.at_xpath('front/article-meta')
         [
@@ -82,6 +74,14 @@ module Tasks
           (pmcid = article_meta.at_xpath('article-id[@pub-id-type="pmcid"]')) ? "PMCID: #{pmcid.text}" : nil,
           (doi   = article_meta.at_xpath('article-id[@pub-id-type="doi"]'))   ? "DOI: https://dx.doi.org/#{doi.text}" : nil
         ].compact
+      end
+
+      def set_journal_attributes
+        article.journal_title = metadata.at_xpath('front/journal-meta/journal-title-group/journal-title')&.text.presence
+        article.journal_volume = metadata.at_xpath('front/article-meta/volume')&.text.presence
+        article.journal_issue = metadata.at_xpath('front/article-meta/issue-id')&.text.presence
+        article.page_start     = metadata.at_xpath('front/article-meta/fpage')&.text.presence
+        article.page_end       = metadata.at_xpath('front/article-meta/lpage')&.text.presence
       end
     end
   end
--- spec/services/tasks/pubmed_ingest/pmc_attribute_builder_spec.rb
+++ spec/services/tasks/pubmed_ingest/pmc_attribute_builder_spec.rb
@@ -2,62 +2,71 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::PubmedIngest::PmcAttributeBuilder, type: :model do
-  let(:builder) { described_class.new }
+  let(:admin_set) { FactoryBot.create(:admin_set) }
+  let(:depositor) { FactoryBot.create(:admin, uid: 'admin') }
+  let(:skipped_rows) do
+    [
+      { 'pmid' => '12345678', 'pmcid' => 'PMC11111111' },
+      { 'pmid' => '99999999', 'pmcid' => 'PMC87654321' }
+    ]
+  end
   let(:metadata) { Nokogiri::XML(File.read(Rails.root.join('spec/fixtures/files/pmc_api_response_multi.xml'))) }
   let(:article_node) { metadata.xpath('//article').first }
+  let(:article) { Article.new }
+  let(:builder) { described_class.new(article_node, article, admin_set, depositor.uid, skipped_rows) }
 
   describe '#find_skipped_row' do
-    let(:pmid) { '12345678' }
-    let(:pmcid) { 'PMC87654321' }
-    let(:skipped_works) do
-      [
-        { 'pmid' => pmid, 'pmcid' => 'PMC11111111' },
-        { 'pmid' => '99999999', 'pmcid' => pmcid }
-      ]
+    it 'matches by pmid' do
+      allow(article_node).to receive(:at_xpath)
+        .with('.//article-id[@pub-id-type="pmid"]')
+        .and_return(double(text: '12345678'))
+      allow(article_node).to receive(:at_xpath)
+        .with('.//article-id[@pub-id-type="pmcid"]')
+        .and_return(nil)
+
+      result = builder.find_skipped_row
+      expect(result['pmid']).to eq('12345678')
     end
 
-    it 'finds by pmid' do
-      allow(metadata).to receive(:at_xpath).with('.//article-id[@pub-id-type="pmid"]').and_return(double(text: pmid))
-      allow(metadata).to receive(:at_xpath).with('.//article-id[@pub-id-type="pmcid"]').and_return(nil)
-      row = builder.find_skipped_row(metadata, skipped_works)
-      expect(row['pmid']).to eq(pmid)
-    end
+    it 'matches by pmcid' do
+      allow(article_node).to receive(:at_xpath)
+        .with('.//article-id[@pub-id-type="pmid"]')
+        .and_return(nil)
+      allow(article_node).to receive(:at_xpath)
+        .with('.//article-id[@pub-id-type="pmcid"]')
+        .and_return(double(text: 'PMC87654321'))
 
-    it 'finds by pmcid' do
-      allow(metadata).to receive(:at_xpath).with('.//article-id[@pub-id-type="pmid"]').and_return(nil)
-      allow(metadata).to receive(:at_xpath).with('.//article-id[@pub-id-type="pmcid"]').and_return(double(text: pmcid))
-      row = builder.find_skipped_row(metadata, skipped_works)
-      expect(row['pmcid']).to eq(pmcid)
+      result = builder.find_skipped_row
+      expect(result['pmcid']).to eq('PMC87654321')
     end
   end
 
   describe '#generate_authors' do
     it 'returns authors with name, orcid, and affiliation' do
-      authors = builder.generate_authors(article_node)
+      authors = builder.send(:generate_authors)
       expect(authors).to all(include('name', 'orcid', 'index', 'other_affiliation'))
       expect(authors.first['name']).to match(/.+, .+/)
     end
   end
 
   describe '#set_identifiers' do
-    let(:article) { Article.new }
-
     it 'sets article.identifier and issn' do
-      builder.set_identifiers(article, article_node)
-      expect(article.identifier).to include(a_string_matching(/^PMID:/), a_string_matching(/^PMCID:/), a_string_matching(/^DOI:/))
+      builder.send(:set_identifiers)
+      expect(article.identifier).to include(a_string_matching(/^PMID:/))
+      expect(article.identifier).to include(a_string_matching(/^PMCID:/))
+      expect(article.identifier).to include(a_string_matching(/^DOI:/))
       expect(article.issn).to all(be_a(String))
     end
   end
 
   describe '#set_journal_attributes' do
-    let(:article) { Article.new }
-
     it 'sets journal title, volume, issue, and pages' do
-    # Mocking fpage and lpage since they are not present in the fixture
+      # Mocking missing values for pagination
       allow(article_node).to receive(:at_xpath).and_call_original
       allow(article_node).to receive(:at_xpath).with('front/article-meta/fpage').and_return(double('Nokogiri::XML::Node', text: '1'))
       allow(article_node).to receive(:at_xpath).with('front/article-meta/lpage').and_return(double('Nokogiri::XML::Node', text: '10'))
-      builder.set_journal_attributes(article, article_node)
+
+      builder.send(:set_journal_attributes)
       expect(article.journal_title).to be_present
       expect(article.journal_volume).to be_present
       expect(article.journal_issue).to be_present
@@ -67,14 +76,12 @@ RSpec.describe Tasks::PubmedIngest::PmcAttributeBuilder, type: :model do
   end
 
   describe '#apply_additional_basic_attributes' do
-    let(:article) { Article.new }
-
-    it 'sets title, abstract, date_issued, publisher, keyword, funder' do
-      builder.apply_additional_basic_attributes(article, article_node)
+    it 'sets title, abstract, date_issued, publisher, keyword, and funder' do
+      builder.send(:apply_additional_basic_attributes)
       expect(article.title).to be_present
       expect(article.abstract).to be_present
       expect(article.date_issued).to match(/\d{4}-\d{2}-\d{2}/)
-      expect(article.publisher).to be_present
+      expect(article.publisher).to all(be_a(String)).or be_empty
       expect(article.keyword).to all(be_a(String))
       expect(article.funder).to all(be_a(String))
     end
