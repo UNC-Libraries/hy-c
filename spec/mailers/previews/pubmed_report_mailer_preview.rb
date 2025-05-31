aeb0b4fac7d198579108501a1dfa48ab8bb18403
David Campbell <davidcam-src@proton.me>
2025-05-30 21:24:04 -0400

    streamlining report mailer

--- spec/mailers/previews/pubmed_report_mailer_preview.rb
+++ spec/mailers/previews/pubmed_report_mailer_preview.rb
@@ -7,7 +7,7 @@ class PubmedReportMailerPreview < ActionMailer::Preview
       # Create Fixture (WIP)
     pubmed_ingest_output_fixture = File.read(File.join(Rails.root, '/spec/fixtures/files/pubmed_ingest_test_fixture.json'))
       # Generate Report
-    report = Tasks::PubmedReportingService.generate_report(JSON.parse(pubmed_ingest_output_fixture))
-    PubmedReportMailer.pubmed_report_email(report)
+    report = Tasks::PubmedReportingService.generate_report(JSON.parse(pubmed_ingest_output_fixture, symbolize_names: true))
+    PubmedReportMailer.pubmed_report_email(report.symbolize_keys)
   end
 end
--- spec/services/tasks/pubmed_reporting_service_spec.rb
+++ spec/services/tasks/pubmed_reporting_service_spec.rb
@@ -9,21 +9,20 @@ RSpec.describe Tasks::PubmedReportingService do
     end
 
     let(:ingest_output) do
-      JSON.parse(File.read(fixture_path))
+      JSON.parse(File.read(fixture_path),  symbolize_names: true)
     end
 
     it 'returns a report hash with expected structure and values' do
-      report = described_class.generate_report(ingest_output)
-
-      expect(report[:headers][:depositor]).to eq(ingest_output['depositor'])
-      expect(report[:formatted_time]).to eq(Time.parse(ingest_output['time']).strftime('%B %d, %Y at %I:%M %p %Z'))
-      expect(report[:file_retrieval_directory]).to eq(ingest_output['file_retrieval_directory'])
-      expect(report[:headers][:depositor]).to eq(ingest_output['depositor'])
-      expect(report[:headers][:total_unique_files]).to eq(ingest_output['counts']['total_unique_files'].to_s)
-      expect(report[:records][:successfully_attached]).to eq(ingest_output['successfully_attached'])
-      expect(report[:records][:successfully_ingested]).to eq(ingest_output['successfully_ingested'])
-      expect(report[:records][:skipped]).to eq(ingest_output['skipped'])
-      expect(report[:records][:failed]).to eq(ingest_output['failed'])
+      report = described_class.generate_report(ingest_output.symbolize_keys)
+      expect(report[:headers][:depositor]).to eq(ingest_output[:depositor])
+      expect(report[:formatted_time]).to eq(Time.parse(ingest_output[:time]).strftime('%B %d, %Y at %I:%M %p %Z'))
+      expect(report[:file_retrieval_directory]).to eq(ingest_output[:file_retrieval_directory])
+      expect(report[:headers][:depositor]).to eq(ingest_output[:depositor])
+      expect(report[:headers][:total_unique_files]).to eq(ingest_output[:counts][:total_unique_files])
+      expect(report[:records][:successfully_attached]).to eq(ingest_output[:successfully_attached])
+      expect(report[:records][:successfully_ingested]).to eq(ingest_output[:successfully_ingested])
+      expect(report[:records][:skipped]).to eq(ingest_output[:skipped])
+      expect(report[:records][:failed]).to eq(ingest_output[:failed])
     end
   end
 end
