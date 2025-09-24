7a029f3465ec720b73060ff9c1f538f29ce0a4e8
David Campbell <davidcam-src@proton.me>
2025-09-24 13:56:43 -0400

    temp test changes

--- app/mailers/pubmed_report_mailer.rb
+++ app/mailers/pubmed_report_mailer.rb
@@ -16,6 +16,7 @@ class PubmedReportMailer < ApplicationMailer
     end
 
     @report = report
-    mail(to: 'cdr@unc.edu', subject: report[:subject], template_name: 'pubmed_report_email')
+    # WIP: Remove hardcoded email
+    mail(to: 'dcam@@unc.edu', subject: report[:subject], template_name: 'pubmed_report_email')
   end
 end
