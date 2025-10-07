9eaa4e9a7d5a6743c7b39e7065299ac0aad8d94f
David Campbell <davidcam-src@proton.me>
2025-10-07 15:41:19 -0400

    new ingest tracker + rubocop

--- app/services/tasks/base_ingest_tracker.rb
+++ app/services/tasks/base_ingest_tracker.rb
@@ -7,28 +7,20 @@ class Tasks::BaseIngestTracker
     output_dir = config['output_dir']
     path = File.join(output_dir, 'ingest_tracker.json')
 
-    if resume
-      LogUtilsHelper.double_log('Resume flag is set. Attempting to resume PubMed ingest from previous state.', :info, tag: 'Ingest Tracker')
-      instance = new(output_dir, config)
-      instance['restart_time'] = config['restart_time'].strftime('%Y-%m-%d %H:%M:%S')
-      LogUtilsHelper.double_log("Resuming from existing state: #{instance.data}", :info, tag: 'Ingest Tracker')
-      return instance
-    end
-
-    if File.exist?(path) &&
-      LogUtilsHelper.double_log("Tracker file already exists at #{path}.", :error, tag: 'Ingest Tracker')
-      exit(1)
-    end
-
-    LogUtilsHelper.double_log('Resume flag is not set. Initializing new ingest tracker.', :info, tag: 'Ingest Tracker')
     instance = new(output_dir, config)
+
+    if resume && File.exist?(path)
+      instance.resume!(config)
+    else
+      instance.initialize_new!(config)
+    end
     instance.save
     instance
   end
 
   def initialize(output_dir, config)
-    @path = File.join(output_dir, 'ingest_tracker.json')
-    @data = File.exist?(@path) ? load_tracker_file : build_initial_tracker(config)
+    @path = File.join(output_dir, TRACKER_FILENAME)
+    @data = {}
   end
 
   def [](key)
@@ -45,7 +37,19 @@ class Tasks::BaseIngestTracker
     LogUtilsHelper.double_log("Failed to save ingest tracker: #{e.message}", :error, tag: 'Ingest Tracker')
   end
 
-        private
+    private
+
+  def resume!(config)
+    LogUtilsHelper.double_log('Resuming ingest from previous tracker state.', :info, tag: self.class.name)
+    @data = load_tracker_file
+    @data['restart_time'] = config['restart_time']&.strftime('%Y-%m-%d %H:%M:%S')
+    @data
+  end
+
+  def initialize_new!(config)
+    LogUtilsHelper.double_log('Initializing new ingest tracker.', :info, tag: self.class.name)
+    @data = build_base_tracker(config)
+  end
 
   def load_tracker_file
     LogUtilsHelper.double_log("Found existing ingest tracker at #{@path}. Loading data.", :info, tag: 'Ingest Tracker')
@@ -69,46 +73,6 @@ class Tasks::BaseIngestTracker
         'output_dir' => config['output_dir'],
         'full_text_dir' => config['full_text_dir'],
         'progress' => {}
-    }
- end
-
-
-  def build_initial_tracker(config)
-    LogUtilsHelper.double_log("Building initial ingest tracker with config: #{config}", :info, tag: 'Ingest Tracker')
-    {
-      'start_time' => config['time'].strftime('%Y-%m-%d %H:%M:%S'),
-      'restart_time' => nil,
-      'date_range' => {
-        'start' => config['start_date'].strftime('%Y-%m-%d'),
-        'end' => config['end_date'].strftime('%Y-%m-%d')
-      },
-      'admin_set_title' => config['admin_set_title'],
-      'depositor_onyen' => config['depositor_onyen'],
-      'output_dir' => config['output_dir'],
-      'full_text_dir' => config['full_text_dir'],
-      'progress' => {
-        'retrieve_ids_within_date_range' => {
-            'pubmed' => { 'cursor' => 0, 'completed' => false },
-            'pmc' => { 'cursor' => 0, 'completed' => false }
-        },
-        'stream_and_write_alternate_ids' => {
-            'pubmed' => { 'cursor' => 0, 'completed' => false },
-            'pmc' => { 'cursor' => 0, 'completed' => false }
-        },
-        'adjust_id_lists' => {
-            'completed' => false,
-            'pubmed' => { 'original_size' => 0, 'adjusted_size' => 0 },
-            'pmc' => { 'original_size' => 0, 'adjusted_size' => 0 }
-        },
-        'metadata_ingest' => {
-            'pubmed' => { 'completed' => false },
-            'pmc' => { 'completed' => false }
-        },
-        'attach_files_to_works' => {
-            'completed' => false,
-        },
-        'send_summary_email' => { 'completed' => false }
-      }
     }
   end
 end
--- lib/tasks/nsf_ingest.rake
+++ lib/tasks/nsf_ingest.rake
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 desc 'Ingest new PDFs from the NSF backlog and attach them to Hyrax works if matched'
 task :nsf_backlog_ingest, [:file_info_csv, :file_retrieval_directory, :output_dir, :admin_set_title, :depositor_onyen] => :environment do |task, args|
-    return unless valid_args('nsf_ingest', args[:file_retrieval_directory], args[:output_dir], args[:admin_set_title], args[:depositor_onyen], args[:file_info_csv])
-    file_retrieval_directory = Pathname.new(args[:file_retrieval_directory]).absolute? ?
-                                 args[:file_retrieval_directory] :
-                                 Rails.root.join(args[:file_retrieval_directory])
-    coordinator = Tasks::NsfIngest::Backlog::NsfIngestCoordinatorService.new({
-        'admin_set_title' => args[:admin_set_title],
-        'depositor_onyen' => args[:depositor_onyen],
-        'file_retrieval_directory' => file_retrieval_directory,
-        'output_dir' => args[:output_dir],
-        'file_info_csv' => args[:file_info_csv]
-    })
-    res = coordinator.run
-    end
+  return unless valid_args('nsf_ingest', args[:file_retrieval_directory], args[:output_dir], args[:admin_set_title], args[:depositor_onyen], args[:file_info_csv])
+  file_retrieval_directory = Pathname.new(args[:file_retrieval_directory]).absolute? ?
+                               args[:file_retrieval_directory] :
+                               Rails.root.join(args[:file_retrieval_directory])
+  coordinator = Tasks::NsfIngest::Backlog::NsfIngestCoordinatorService.new({
+      'admin_set_title' => args[:admin_set_title],
+      'depositor_onyen' => args[:depositor_onyen],
+      'file_retrieval_directory' => file_retrieval_directory,
+      'output_dir' => args[:output_dir],
+      'file_info_csv' => args[:file_info_csv]
+  })
+  res = coordinator.run
+end
