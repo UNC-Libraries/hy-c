91cd5123a8ec39122a0f3aa84c47fa0875b91b6e
David Campbell <davidcam-src@proton.me>
2025-11-11 16:39:56 -0500

    set identifiers

--- app/services/tasks/eric_ingest/backlog/utilities/attribute_builders/eric_attribute_builder.rb
+++ app/services/tasks/eric_ingest/backlog/utilities/attribute_builders/eric_attribute_builder.rb
@@ -16,3 +16,27 @@ module Tasks::EricIngest::Backlog::Utilities::AttributeBuilders
         }
       end
     end
+
+    def apply_additional_basic_attributes(article)
+        article.title = [metadata['title']]
+        article.abstract = [metadata['description']] if metadata['abstract'].present?
+        article.date_issued = get_date_issued.strftime('%Y-%m-%d')
+        article.keyword = metadata['subject'] if metadata['subject'].present?
+        article.publisher = [metadata['publisher']] if metadata['publisher'].present?
+    end
+
+    def set_identifiers(article)
+        identifiers = []
+        if metadata['eric_id'].present?
+            identifiers << "ERIC: #{metadata['eric_id']}"
+        end
+
+         if metadata['issn'].present?
+            metadata['issn'].each do |issn|
+            # remove the "ISSN-" prefix if present, and strip whitespace
+            article.issn = [issn.sub(/^ISSN[-:\s]*/i, '').strip]
+            end
+        end
+        article.identifier = identifiers
+    end
+end
