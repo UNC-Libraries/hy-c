df0f9cb81b8a5ab373f78a5156881f1fb778f6b8
David Campbell <davidcam-src@proton.me>
2025-11-12 15:04:46 -0500

    syntax

--- app/services/tasks/eric_ingest/backlog/utilities/attribute_builders/eric_attribute_builder.rb
+++ app/services/tasks/eric_ingest/backlog/utilities/attribute_builders/eric_attribute_builder.rb
@@ -3,14 +3,14 @@ module Tasks::EricIngest::Backlog::Utilities::AttributeBuilders
   class EricAttributeBuilder < Tasks::IngestHelperUtils::BaseAttributeBuilder
 
     def get_date_issued
-      pub_year = metadata['PublicationYear']
+      pub_year = metadata['publicationdateyear']
       DateTime.new(pub_year.to_i) if pub_year.present?
     end
 
     private
 
     def generate_authors
-      metadata['Author'].map.with_index do |full_name, i|
+      metadata['author'].map.with_index do |full_name, i|
         {
           'name' => full_name,
           'index' => i.to_s
