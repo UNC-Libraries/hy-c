8c9e3a4247d42fd16a865f44fbceceaf792533bf
David Campbell <davidcam-src@proton.me>
2025-11-13 15:48:21 -0500

    pdf count

--- app/services/tasks/eric_ingest/backlog/utilities/notification_service.rb
+++ app/services/tasks/eric_ingest/backlog/utilities/notification_service.rb
@@ -8,9 +8,15 @@ class Tasks::EricIngest::Backlog::Utilities::NotificationService < Tasks::Ingest
 
   def populate_headers!(report)
     report[:headers][:depositor] = @tracker['depositor_onyen']
-    report[:headers][:total_files] = report[:headers][:total_files]
+    report[:headers][:total_files] = eric_pdf_count
     all_eric_ids = report[:records].values.flatten.map { |r| r.dig('ids', 'eric_id') }.compact.uniq
     report[:headers][:total_files] = all_eric_ids.size
+  end
+
+  def eric_pdf_count
+    # Count of ERIC PDFs in the full text directory
+    full_text_dir = @config['full_text_dir']
+    Dir.glob(File.join(full_text_dir, '**', '*.pdf')).count
   end
 
   def send_mail(report, zip_path)
