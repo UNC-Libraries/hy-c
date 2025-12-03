872a412117609003ef442118cab86496649b6fc3
David Campbell <davidcam-src@proton.me>
2025-12-03 14:56:29 -0500

    md ingest service tests

--- spec/services/tasks/rosap_ingest/backlog/utilities/metadata_ingest_service_spec.rb
+++ spec/services/tasks/rosap_ingest/backlog/utilities/metadata_ingest_service_spec.rb
@@ -50,10 +50,13 @@ RSpec.describe Tasks::RosapIngest::Backlog::Utilities::MetadataIngestService do
       allow(service).to receive(:fetch_metadata_for_rosap_id).with('R654321').and_return({ 'title' => 'Test Title 2' })
       allow(Tasks::RosapIngest::Backlog::Utilities::AttributeBuilders::RosapAttributeBuilder).to receive(:new).and_call_original
       allow(service).to receive(:new_article).and_return(FactoryBot.build(:article))
+      allow(Dir).to receive(:glob).and_return([])
+      allow(service).to receive(:sleep)
       allow(service).to receive(:record_result)
       allow(service).to receive(:flush_buffer_if_needed)
       allow(service).to receive(:flush_buffer_to_file)
       allow(Rails.logger).to receive(:info)
+      allow(LogUtilsHelper).to receive(:double_log)
     end
 
     it 'processes remaining ROSAP IDs and records results' do
@@ -64,7 +67,7 @@ RSpec.describe Tasks::RosapIngest::Backlog::Utilities::MetadataIngestService do
       expect(Tasks::RosapIngest::Backlog::Utilities::AttributeBuilders::RosapAttributeBuilder).to have_received(:new).twice
       expect(service).to have_received(:new_article).twice
       expect(service).to have_received(:record_result).twice
-      expect(service).to have_received(:flush_buffer_to_file)
+      expect(service).to have_received(:flush_buffer_if_needed).at_least(:twice)
     end
 
     it 'skips already seen ROSAP IDs' do
@@ -74,6 +77,21 @@ RSpec.describe Tasks::RosapIngest::Backlog::Utilities::MetadataIngestService do
 
       expect(service).to have_received(:fetch_metadata_for_rosap_id).once.with('R654321')
       expect(service).to have_received(:record_result).once
+    end
+
+    it 'skips existing works with the same ROSAP ID' do
+      existing_work = FactoryBot.create(:article)
+      allow(WorkUtilsHelper).to receive(:fetch_work_data_by_alternate_identifier)
+        .with('R123456', admin_set_title: config['admin_set_title'])
+        .and_return({ work_id: existing_work.id.to_s })
+      allow(WorkUtilsHelper).to receive(:fetch_work_data_by_alternate_identifier)
+        .with('R654321', admin_set_title: config['admin_set_title'])
+        .and_return(nil)
+
+      service.process_backlog
+
+      expect(service).to have_received(:record_result).at_least(:once)
+      expect(service).to have_received(:fetch_metadata_for_rosap_id).once.with('R654321')
     end
   end
 end
