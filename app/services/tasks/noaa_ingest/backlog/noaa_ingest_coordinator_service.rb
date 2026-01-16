8294dfd943d3e52d93a4b7652697ac6b15f7ee65
David Campbell <davidcam-src@proton.me>
2026-01-16 14:28:45 -0500

    coordinator service

---
+++ app/services/tasks/noaa_ingest/backlog/noaa_ingest_coordinator_service.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+class Tasks::NoaaIngest::Backlog::NoaaIngestCoordinatorService
+  LOAD_METADATA_OUTPUT_DIR  = '01_load_and_ingest_metadata'
+  ATTACH_FILES_OUTPUT_DIR   = '02_attach_files_to_works'
+  RESULT_CSV_OUTPUT_DIR     = '03_generate_result_csvs'
+  MAX_ROWS = 100
+
+  def initialize(config)
+    # Initialize ingest tracker
+    @config = config
+    @tracker = Tasks::NoaaIngest::Backlog::Utilities::NoaaIngestTracker.build(
+        config: config,
+        resume: config['resume'])
+    @md_ingest_results_path = File.join(@config['output_dir'], LOAD_METADATA_OUTPUT_DIR, 'metadata_ingest_results.jsonl')
+    @file_attachment_results_path = File.join(@config['output_dir'], ATTACH_FILES_OUTPUT_DIR, 'attachment_results.jsonl')
+    @aggregated_file_attachment_results_path = File.join(@config['output_dir'], ATTACH_FILES_OUTPUT_DIR, 'aggregated_attachment_results.jsonl')
+    @generated_results_csv_dir = File.join(@config['output_dir'], RESULT_CSV_OUTPUT_DIR)
+    # Create output directories if they don't exist
+    generate_output_subdirectories
+  end
+
+  def run
+    NotificationUtilsHelper.suppress_emails do
+      load_and_ingest_metadata
+        # attach_files
+    end
+    # format_results_and_notify
+
+    LogUtilsHelper.double_log('NOAA ingest workflow completed successfully', :info, tag: 'NoaaIngestCoordinator')
+    rescue => e
+      LogUtilsHelper.double_log("NOAA ingest workflow failed: #{e.message}", :error, tag: 'NoaaIngestCoordinator')
+      raise e
+  end
+
+  def load_and_ingest_metadata
+    if @tracker['progress']['metadata_ingest']['completed']
+      LogUtilsHelper.double_log('Metadata ingest already completed according to tracker. Skipping this step.', :info, tag: 'NoaaIngestCoordinatorService')
+      return
+    end
+    LogUtilsHelper.double_log('Starting metadata ingest step.', :info, tag: 'NoaaIngestCoordinatorService')
+    md_ingest_service = Tasks::NoaaIngest::Backlog::Utilities::MetadataIngestService.new(
+        config: @config,
+        tracker: @tracker,
+        md_ingest_results_path: @md_ingest_results_path
+    )
+    md_ingest_service.process_backlog
+    @tracker['progress']['metadata_ingest']['completed'] = true
+    @tracker.save
+    LogUtilsHelper.double_log('Metadata ingest step completed.', :info, tag: 'NoaaIngestCoordinatorService')
+  end
+
+  def generate_output_subdirectories
+    [LOAD_METADATA_OUTPUT_DIR, ATTACH_FILES_OUTPUT_DIR, RESULT_CSV_OUTPUT_DIR].each do |dir|
+      full_path = File.join(@config['output_dir'], dir)
+      FileUtils.mkdir_p(full_path) unless Dir.exist?(full_path)
+    end
+  end
+end
