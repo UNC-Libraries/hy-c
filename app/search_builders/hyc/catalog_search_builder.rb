fe0539f38eab4bc8e1b6b6e8237a8feeec76d65f
David Campbell <davidcam-src@proton.me>
2026-03-10 13:15:42 -0400

    remove include

--- app/search_builders/hyc/catalog_search_builder.rb
+++ app/search_builders/hyc/catalog_search_builder.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 module Hyc
   class CatalogSearchBuilder < Hyrax::CatalogSearchBuilder
-    # include BlacklightAdvancedSearch::AdvancedSearchBuilder
     include BlacklightRangeLimit::RangeLimitBuilder
 
     self.default_processor_chain += [
