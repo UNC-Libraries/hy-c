37652276999b09b9262521c737ecfe48b02efd62
David Campbell <davidcam-src@proton.me>
2026-03-11 15:42:47 -0400

    catalog search builder update

--- app/search_builders/hyc/catalog_search_builder.rb
+++ app/search_builders/hyc/catalog_search_builder.rb
@@ -21,13 +21,27 @@ module Hyc
       is_advanced_search = blacklight_params['clause'].present?
 
       if is_advanced_search
-        # Advanced search - remove JSON query and use join query only
-        # (can't easily OR them with JSON Query DSL)
-        solr_parameters.delete(:json)
-        solr_parameters.delete(:qf)
-        solr_parameters.delete(:pf)
-        solr_parameters[:defType] = 'lucene'
-        solr_parameters[:q] = join_query
+        # Build combined query that includes both metadata search (from JSON query) and file text search (from join query)
+        # Retrieve the JSON query for metadata search
+        json_query = solr_parameters.dig(:json, :query, :bool, :must, 0)
+
+        if json_query && json_query[:edismax]
+          # Extract the metadata search parameters
+          qf = json_query[:edismax][:qf]
+          pf = json_query[:edismax][:pf]
+          query_term = json_query[:edismax][:query]
+
+          # Build metadata query using edismax
+          metadata_query = "_query_:\"{!edismax qf='#{qf}' pf='#{pf}'}#{query_term}\""
+
+          # Combine metadata and file text queries with OR
+          combined_query = "(#{metadata_query}) OR (#{join_query})"
+
+          # JSON query and q parameters are AND-ed together by the edismax parser, so we need to remove the JSON query and replace with combined query to achieve OR behavior
+          solr_parameters.delete(:json)
+          solr_parameters[:defType] = 'lucene'
+          solr_parameters[:q] = combined_query
+        end
       else
         # Basic search - skip join query, just let JSON query search metadata
         # File text search only works in advanced search
