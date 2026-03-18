0b6a1e87746105d134a18ed8fc019b868ae2eeb8
David Campbell <davidcam-src@proton.me>
2026-03-18 10:36:25 -0400

    split blacklight-gallery.js

--- app/assets/javascripts/blacklight_gallery.js
+++ app/assets/javascripts/blacklight_gallery.js
@@ -1,1 +1,5 @@
-//= require blacklight_gallery/blacklight-gallery
+// [HYC-2162] blacklight-gallery 4.x split the monolithic blacklight-gallery.js into individual files
+//= require blacklight_gallery/default
+//= require blacklight_gallery/masonry
+//= require blacklight_gallery/osd_viewer
+//= require blacklight_gallery/slideshow
