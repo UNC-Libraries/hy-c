65324677b5cd6e995ad03217cd8f8662c5b6056b
David Campbell <davidcam-src@proton.me>
2026-03-18 23:29:00 -0400

    split blacklight gallery into individual files

--- app/assets/javascripts/blacklight_gallery.js
+++ app/assets/javascripts/blacklight_gallery.js
@@ -1,1 +1,5 @@
-//= require blacklight_gallery/blacklight-gallery
+// blacklight-gallery 4.x split blacklight-gallery.js into individual files
+//= require blacklight_gallery/default
+//= require blacklight_gallery/masonry
+//= require blacklight_gallery/osd_viewer
+//= require blacklight_gallery/slideshow
