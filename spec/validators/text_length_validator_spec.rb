7f27370c698e6a8c6d6a29ddd3fa6ad66def1cfe
lfarrell <lfarrell@email.unc.edu>
2026-07-01 11:54:09 -0400

    Fix rubocop violations

--- spec/validators/text_length_validator_spec.rb
+++ spec/validators/text_length_validator_spec.rb
@@ -1,63 +1,63 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe TextLengthValidator do
   subject(:validator) do
     described_class.new(attributes: [:body], maximum: maximum, message: message)
   end
 
-  let(:record) { instance_double("Record", errors: errors) }
-  let(:errors) { instance_double("Errors", add: true) }
+  let(:record) { instance_double('Record', errors: errors) }
+  let(:errors) { instance_double('Errors', add: true) }
   let(:attribute) { :body }
   let(:maximum) { 10 }
   let(:message) { nil }
 
-  describe "#validate_each" do
-    it "ignores blank values" do
+  describe '#validate_each' do
+    it 'ignores blank values' do
       expect(errors).not_to receive(:add)
 
-      validator.validate_each(record, attribute, ["", nil, " "])
+      validator.validate_each(record, attribute, ['', nil, ' '])
     end
 
-    it "does not add an error for text within the limit" do
+    it 'does not add an error for text within the limit' do
       expect(errors).not_to receive(:add)
 
-      validator.validate_each(record, attribute, "<p>hello</p>")
+      validator.validate_each(record, attribute, '<p>hello</p>')
     end
 
-    it "adds the default error message when text is too long" do
+    it 'adds the default error message when text is too long' do
       validator_without_message = described_class.new(attributes: [:body], maximum: maximum)
 
       expect(errors).to receive(:add).with(
         attribute,
-        "Field text is too long (maximum is 10 characters)"
+        'Field text is too long (maximum is 10 characters)'
       )
 
-      validator_without_message.validate_each(record, attribute, "hello world")
+      validator_without_message.validate_each(record, attribute, 'this is way too long')
     end
 
-    it "adds a custom error message when provided" do
+    it 'adds a custom error message when provided' do
       validator_with_message = described_class.new(
         attributes: [:body],
         maximum: maximum,
-        message: "is too long"
+        message: 'is too long'
       )
 
-      expect(errors).to receive(:add).with(attribute, "is too long")
+      expect(errors).to receive(:add).with(attribute, 'is too long')
 
-      validator_with_message.validate_each(record, attribute, "hello world")
+      validator_with_message.validate_each(record, attribute, 'hello world')
     end
 
-    it "checks cleaned text length rather than raw HTML length" do
+    it 'checks cleaned text length rather than raw HTML length' do
       validator_without_message = described_class.new(attributes: [:body], maximum: 1)
 
       expect(errors).to receive(:add).with(
         attribute,
-        "Field text is too long (maximum is 1 characters)"
+        'Field text is too long (maximum is 1 characters)'
       )
 
-      validator_without_message.validate_each(record, attribute, "<b>hi</b>")
+      validator_without_message.validate_each(record, attribute, '<b>hi</b>')
     end
   end
 end
