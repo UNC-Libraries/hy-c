26d46ab3b169acaf3c68f1f290541ae448a7bb40
Ben Pennell <bbpennel@email.unc.edu>
2026-07-20 09:46:52 -0400

    rubocop

--- app/overrides/controllers/hyrax/downloads_controller_override.rb
+++ app/overrides/controllers/hyrax/downloads_controller_override.rb
@@ -69,7 +69,7 @@ Hyrax::DownloadsController.class_eval do
 
   def file_set_parent(file_set_id)
     file_set = if defined?(Wings) && Hyrax.metadata_adapter.is_a?(Wings::Valkyrie::MetadataAdapter)
-                  Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
+                 Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
                else
                  Hyrax.query_service.find_by(id: file_set_id)
                end
