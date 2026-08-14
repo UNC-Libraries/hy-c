bf0483f4cd3d4d5be75f56750ef95551a40888f3
Dean Farrell <lfarrell@email.unc.edu>
2026-08-14 09:44:28 -0400

    Explicitly send a json request when running a turnstile check (#1278)
    * Explicitly send a json request
    
    * Update tests
    
    * Make the test responses hashes

--- spec/test_support/helper_ruby/webmock_turnstile_helper_methods.rb
+++ spec/test_support/helper_ruby/webmock_turnstile_helper_methods.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 module WebmockTurnstileHelperMethods
   def stub_turnstile_success(turnstile_response: {},
-                             request_body: "secret=#{BotDetectController.cf_turnstile_secret_key}&response=XXXX.DUMMY.TOKEN.XXXX&remoteip=0.0.0.0"
+                             request_body: {
+                               secret: BotDetectController.cf_turnstile_secret_key,
+                               response: 'XXXX.DUMMY.TOKEN.XXXX',
+                               remoteip: '0.0.0.0'
+                             }
   )
 
     turnstile_response.reverse_merge!(
@@ -15,7 +19,8 @@ module WebmockTurnstileHelperMethods
 
     stub_request(:post, BotDetectController.cf_turnstile_validation_url).
       with(
-        body: request_body
+        body: request_body.to_json,
+        headers: { 'Content-Type' => 'application/json' }
       ).to_return(status: 200,
                   body: turnstile_response.to_json,
                   headers: { 'Content-Type' => 'application/json; charset=utf-8' }
@@ -25,7 +30,11 @@ module WebmockTurnstileHelperMethods
   end
 
   def stub_turnstile_failure(turnstile_response: {},
-                             request_body: "secret=#{BotDetectController.cf_turnstile_secret_key}&response=XXXX.DUMMY.TOKEN.XXXX&remoteip=0.0.0.0")
+                             request_body: {
+                               secret: BotDetectController.cf_turnstile_secret_key,
+                               response: 'XXXX.DUMMY.TOKEN.XXXX',
+                               remoteip: '0.0.0.0'
+                             })
 
     turnstile_response.reverse_merge!(
       {'success'=>false, 'error-codes' => ['invalid-input-response'], 'messages' => [], 'metadata' =>
@@ -35,7 +44,8 @@ module WebmockTurnstileHelperMethods
 
     stub_request(:post, BotDetectController.cf_turnstile_validation_url).
       with(
-        body: request_body
+        body: request_body.to_json,
+        headers: { 'Content-Type' => 'application/json' }
       ).to_return(status: 200,
                   body: turnstile_response.to_json,
                   headers: { 'Content-Type' => 'application/json; charset=utf-8' }
