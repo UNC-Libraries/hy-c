391e2e20ad05c92a437211a2db0cbb1ad74ac72b
lfarrell <lfarrell@email.unc.edu>
2026-08-17 10:09:52 -0400

    Files must define a method that matches the file name

--- app/overrides/jobs/file_set_attached_event_job_override.rb
+++ app/overrides/jobs/file_set_attached_event_job_override.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 # [hyc-override] Overriding to not make depositor a clickable link
 # https://github.com/samvera/hyrax/blob/v2.9.6/app/jobs/file_set_attached_event_job.rb
-Hyrax::FileSetAttachedEventJob.class_eval do
-  def action
-    "User #{link_to depositor, search_catalog_path(f: { depositor_ssim: [depositor] })} has attached #{file_link} to #{work_link}"
+module Hyrax
+  module FileSetAttachedEventJobOverride
+    def action
+      "User #{link_to depositor, search_catalog_path(f: { depositor_ssim: [depositor] })} has attached #{file_link} to #{work_link}"
+    end
   end
 end
+::FileSetAttachedEventJob.prepend(Hyrax::FileSetAttachedEventJobOverride)
