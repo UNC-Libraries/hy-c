f140ac87e0dcb35d1a47039d8eca8bd79b7ca59a
lfarrell <lfarrell@email.unc.edu>
2026-08-24 09:38:47 -0400

    Add shim for missing file

---
+++ app/concerns/hyrax/compound_field_behavior.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# [hyc-override] Temporary shim for Hyrax 5.3.0.
+module Hyrax
+  # Temporary shim for Hyrax 5.3.0.
+  #
+  # `Hyrax::Forms::ResourceForm` includes `CompoundFieldBehavior`, but the gem
+  # does not ship a definition for that module. Define a no-op behavior in the
+  # `Hyrax` namespace so Ruby constant lookup from inside
+  # `Hyrax::Forms::ResourceForm` resolves cleanly under Rails 7 / Zeitwerk.
+  #
+  # @TODO: Remove this once upstream Hyrax provides the module or removes the include.
+  module CompoundFieldBehavior
+  end
+end
+
--- app/overrides/forms/hyrax/forms/resource_form_override.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-# [hyc-override] Fix file loading issue. uninitialized constant Hyrax::Forms::ResourceForm::CompoundFieldBehavior (NameError)
-# @TODO: Can remove when this is fixed upstream.
-module ResourceFormCompoundFieldOverride
-  include ::CompoundFieldBehavior
-end
-Hyrax::Forms::ResourceForm.prepend(ResourceFormCompoundFieldOverride)
