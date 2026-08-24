c45207349187960fce5f69f170dee5c831ae3744
lfarrell <lfarrell@email.unc.edu>
2026-08-24 10:46:32 -0400

    Add fix for " NoMethodError (undefined method `+' for an instance of ActiveFedora::Checksum)"

--- app/overrides/lib/hydra/services/characterization_service_override.rb
+++ app/overrides/lib/hydra/services/characterization_service_override.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # [hyc-override] https://github.com/samvera/hydra-works/blob/v2.3.0/lib/hydra/works/services/characterization_service.rb
-# [hyc-override] This override is necessary to normalize scalar and multi-value characterization fields, as some properties (e.g., checksum) are scalar objects,
+# [hyc-override] This override normalizes scalar and multi-value characterization fields, as some properties (e.g., checksum) are scalar objects,
 # which raises an error when attempting to append values to them as if they were arrays.
 require 'hydra/works/services/characterization_service'
 
@@ -9,6 +9,9 @@ Hydra::Works::CharacterizationService.class_eval do
   # Some properties (e.g., checksum) are scalar objects, so `current + [value]`
   # would raise when `current` is not an Array.
   def append_property_value(property, value)
+    setter = "#{property}="
+    return unless object.respond_to?(setter)
+
     current = object.send(property)
 
     # Keep mime_type single-valued by always taking the latest extracted value.
@@ -21,6 +24,6 @@ Hydra::Works::CharacterizationService.class_eval do
       value = Array(value).map(&:to_i).max.to_s
     end
 
-    object.send("#{property}=", value)
+    object.send(setter, value)
   end
 end
