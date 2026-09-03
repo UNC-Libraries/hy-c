08a6ce561075a431865278ecd44a2bebdc9cfbaf
lfarrell <lfarrell@email.unc.edu>
2026-09-03 09:43:16 -0400

    Fix chromedriver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,29 +2,40 @@
 
 Capybara.server = :webrick
 
+def find_executable(commands: [], paths: [])
+  from_path = commands.map { |command| `command -v #{command} 2>/dev/null`.strip }
+  (from_path + paths).reject(&:empty?).uniq.find { |path| File.executable?(path) }
+end
+
 Capybara.register_driver :chrome_headless do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.read_timeout = 120
 
-  chrome_binary = '/snap/bin/chromium'
-  chromedriver_path = '/usr/bin/chromedriver'
+  chrome_binary = find_executable(
+    commands: %w[google-chrome-stable google-chrome chromium chromium-browser],
+    paths: %w[/usr/bin/google-chrome-stable /opt/google/chrome/chrome /usr/bin/chromium /usr/bin/chromium-browser /snap/bin/chromium]
+  )
 
-  raise "Chrome binary does not exist: #{chrome_binary}" unless File.file?(chrome_binary)
-  raise "Chrome binary is not executable: #{chrome_binary}" unless File.executable?(chrome_binary)
-  raise "ChromeDriver does not exist: #{chromedriver_path}" unless File.file?(chromedriver_path)
-  raise "ChromeDriver is not executable: #{chromedriver_path}" unless File.executable?(chromedriver_path)
+  chromedriver_path = find_executable(
+    commands: ['chromedriver'],
+    paths: %w[/usr/bin/chromedriver /usr/local/bin/chromedriver]
+  )
 
   options = Selenium::WebDriver::Chrome::Options.new
-  options.binary = chrome_binary
+  options.binary = chrome_binary if chrome_binary
 
-  options.add_argument('--headless=new')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
+  %w[
+    --headless
+    --no-sandbox
+    --disable-dev-shm-usage
+    --disable-gpu
+    --no-zygote
+    --disable-software-rasterizer
+    --remote-debugging-port=0
+    --window-size=1400,1400
+  ].each { |argument| options.add_argument(argument) }
 
-  service = Selenium::WebDriver::Service.chrome(
-    path: chromedriver_path
-  )
+  service = chromedriver_path ? Selenium::WebDriver::Service.chrome(path: chromedriver_path) : Selenium::WebDriver::Service.chrome
 
   Capybara::Selenium::Driver.new(
     app,
